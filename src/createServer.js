ce0eb2c05e5e80919000475506bcaffa9de40485
Nikita Havryliuk <chinter.games1@gmail.com>
2022-10-18 12:22:00 +0300

    create server

--- src/createServer.js
+++ src/createServer.js
@@ -1,3 +1,32 @@
-// Write code here
-// Also, you can create additional files in the src folder
-// and import (require) them here
+const http = require('http');
+const fs = require('fs');
+
+const createServer = () => {
+  const server = http.createServer(
+    (req, res) => {
+      const normalizedURL = new URL(req.url, `http://${req.headers.host}`);
+
+      if (!normalizedURL.startsWith('/file')) {
+        res.statusCode = 404;
+        res.end('File not found. The file path must starts with "/file/"!');
+      }
+
+      const fileName = normalizedURL.pathname.replace('/file', '')
+        || 'index.html';
+
+      fs.readFile(`./public${fileName}`, (error, data) => {
+        if (!error) {
+          res.statusCode = 200;
+          res.end(data);
+        } else {
+          res.statusCode = 404;
+          res.end('Page not found :(');
+        }
+      });
+    },
+  );
+
+  return server;
+};
+
+module.exports = { createServer };
