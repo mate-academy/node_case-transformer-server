edade188d3095f06be88aa81414923d74082ca7a
Oleksii Pustovit <91pustovit91@gmail.com>
2022-10-18 14:04:41 +0300

    Solution of node_case-transformer-server

--- src/createServer.js
+++ src/createServer.js
@@ -1,3 +1,61 @@
-// Write code here
-// Also, you can create additional files in the src folder
-// and import (require) them here
+const http = require('http');
+const { convertToCase } = require('./convertToCase');
+
+function createServer() {
+  const server = http.createServer((req, res) => {
+    const normalizedUrl = new URL(req.url, `http://${req.headers.host}`);
+    const path = normalizedUrl.pathname.slice(1);
+    const query = Object.fromEntries(normalizedUrl.searchParams.entries());
+    const cases = ['SNAKE', 'KEBAB', 'CAMEL', 'PASCAL', 'UPPER'];
+    const targetCase = query.toCase;
+    const errors = [];
+
+    if (!path) {
+      errors.push({
+        message: 'Text to convert is required. '
+        + 'Correct request is: "/<TEXT_TO_CONVERT>?toCase=<CASE_NAME>".',
+      });
+    }
+
+    if (!targetCase) {
+      errors.push({
+        message: '"toCase" query param is required. '
+        + 'Correct request is: "/<TEXT_TO_CONVERT>?toCase=<CASE_NAME>".',
+      });
+    } else if (!cases.includes(targetCase)) {
+      errors.push({
+        message: 'This case is not supported. '
+        + 'Available cases: SNAKE, KEBAB, CAMEL, PASCAL, UPPER.',
+      });
+    }
+
+    if (errors.length) {
+      res.writeHead(400, {
+        'Content-Type': 'application/json',
+      });
+      res.statusText = 'Bad Request';
+      res.end(JSON.stringify({ errors }));
+
+      return;
+    }
+
+    const convertedText = convertToCase(path, targetCase);
+
+    res.writeHead(200, {
+      'Content-Type': 'application/json',
+    });
+
+    res.end(JSON.stringify({
+      originalCase: convertedText.originalCase,
+      targetCase,
+      originalText: path,
+      convertedText: convertedText.convertedText,
+    }));
+  });
+
+  return server;
+}
+
+module.exports = {
+  createServer,
+};
