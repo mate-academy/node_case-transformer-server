20883658cda9934566ac9dfd2397f0f3be7d914b
Roman Yakymiuk <romeronice93@gmail.com>
2022-10-18 20:42:16 +0300

    Fixes

--- src/createServer.js
+++ src/createServer.js
@@ -9,49 +9,45 @@ const createServer = () => {
     const toCase = normalizedUrl.searchParams.get('toCase');
     let returnedObj = {};
     const returnedListError = [];
-    let isErrors = false;
+    const casesList = ['SNAKE', 'KEBAB', 'CAMEL', 'PASCAL', 'UPPER'];
+    const errorMessageTextReq = 'Text to convert is required. '
+    + 'Correct request is: "/<TEXT_TO_CONVERT>?toCase=<CASE_NAME>".';
+    const errorCaseNotSupp = 'This case is not supported. '
+    + 'Available cases: SNAKE, KEBAB, CAMEL, PASCAL, UPPER.';
+    const errorToCaseReq = '"toCase" query param is required. '
+    + 'Correct request is: "/<TEXT_TO_CONVERT>?toCase=<CASE_NAME>".';
 
-    if (params.length === 0) {
-      isErrors = true;
+    res.setHeader('Content-Type', 'application/json');
 
+    if (!params.length) {
       returnedListError.push({
-        message: 'Text to convert is required. '
-        + 'Correct request is: "/<TEXT_TO_CONVERT>?toCase=<CASE_NAME>".',
+        message: errorMessageTextReq,
       });
     }
 
     if (typeof toCase === 'string' && toCase.length > 0) {
-      if (toCase === 'SNAKE' || toCase === 'KEBAB'
-      || toCase === 'CAMEL' || toCase === 'PASCAL' || toCase === 'UPPER') {
+      if (casesList.includes(toCase)) {
         const caseParams = detectCase(params);
-        const changedText = convertToCase(params, toCase);
+        const changedText = convertToCase(params, toCase).convertedText;
 
         returnedObj = {
           originalCase: caseParams,
-          convertedText: changedText.convertedText,
+          convertedText: changedText,
           targetCase: toCase,
           originalText: params,
         };
       } else {
-        isErrors = true;
-
         returnedListError.push({
-          message: 'This case is not supported. '
-          + 'Available cases: SNAKE, KEBAB, CAMEL, PASCAL, UPPER.',
+          message: errorCaseNotSupp,
         });
       }
     } else {
-      isErrors = true;
-
       returnedListError.push({
-        message: '"toCase" query param is required. '
-        + 'Correct request is: "/<TEXT_TO_CONVERT>?toCase=<CASE_NAME>".',
+        message: errorToCaseReq,
       });
     }
 
-    res.setHeader('Content-Type', 'application/json');
-
-    if (!isErrors) {
+    if (!returnedListError.length) {
       res.statusCode = 200;
     } else {
       res.statusCode = 400;
