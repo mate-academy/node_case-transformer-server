2fe72d23e1d259deb7f162c466da467a159f3c73
Jamal Amoian <leonidagutin09@gmail.com>
2022-10-18 22:40:38 +0300

    Solution

--- src/createServer.js
+++ src/createServer.js
@@ -1,3 +1,60 @@
-// Write code here
-// Also, you can create additional files in the src folder
-// and import (require) them here
+
+const http = require('http');
+const { convertToCase } = require('./convertToCase');
+
+const { ServerError } = require('./types/ServerError');
+
+function createServer() {
+  const server = http.createServer((request, response) => {
+    response.setHeader('Content-Type', 'application/json');
+
+    const url = new URL(request.url, 'http://localhost:3000');
+
+    const convertCases = ['SNAKE', 'KEBAB', 'CAMEL', 'PASCAL', 'UPPER'];
+
+    const { pathname, searchParams } = url;
+
+    const errors = [];
+
+    const text = pathname.slice(1);
+
+    const params = Object.fromEntries(searchParams.entries());
+
+    if (text === '') {
+      errors.push({
+        message: ServerError.EmptyText,
+      });
+    }
+
+    if (Object.keys(params).length === 0) {
+      errors.push({
+        message: ServerError.NoToCase,
+      });
+    } else if (!convertCases.includes(params.toCase)) {
+      errors.push({
+        message: ServerError.InvalidToCase,
+      });
+    }
+
+    if (errors.length) {
+      response.end(JSON.stringify({ errors }));
+
+      return;
+    }
+
+    const { originalCase, convertedText } = convertToCase(text, params.toCase);
+
+    response.end(JSON.stringify({
+      convertedText,
+      originalCase,
+      originalText: text,
+      targetCase: params.toCase,
+    }));
+  });
+
+  return server;
+}
+
+module.exports = {
+  createServer,
+};
---
+++ src/types/ServerError.js
@@ -0,0 +1,12 @@
+const ServerError = {
+  EmptyText: 'Text to convert is required. '
+    + 'Correct request is: "/<TEXT_TO_CONVERT>?toCase=<CASE_NAME>".',
+  NoToCase: '"toCase" query param is required. '
+    + 'Correct request is: "/<TEXT_TO_CONVERT>?toCase=<CASE_NAME>".',
+  InvalidToCase: 'This case is not supported. '
+    + 'Available cases: SNAKE, KEBAB, CAMEL, PASCAL, UPPER.',
+};
+
+module.exports = {
+  ServerError,
+};
