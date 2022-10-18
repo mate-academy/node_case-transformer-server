4f8e9111c64a0a22a94bcf90d96e2b57913796c7
Kostya Kosyuk <80011474+Kostya7337@users.noreply.github.com>
2022-10-18 18:35:14 +0300

    a little fix

--- src/createServer.js
+++ src/createServer.js
@@ -1,6 +1,21 @@
-/* eslint-disable max-len */
 const http = require('http');
 const convertToCase = require('./convertToCase').convertToCase;
+
+const cases = ['SNAKE',
+  'KEBAB',
+  'CAMEL',
+  'PASCAL',
+  'UPPER',
+];
+
+const errorsAnswers = {
+  noText: 'Text to convert is required.'
+    + ' Correct request is: "/<TEXT_TO_CONVERT>?toCase=<CASE_NAME>".',
+  noToCase: '"toCase" query param is required.'
+    + ' Correct request is: "/<TEXT_TO_CONVERT>?toCase=<CASE_NAME>".',
+  invalidToCase: 'This case is not supported.'
+    + ' Available cases: SNAKE, KEBAB, CAMEL, PASCAL, UPPER.',
+};
 
 function createServer() {
   const server = http.createServer();
@@ -8,24 +23,25 @@ function createServer() {
   server.on('request', (req, res) => {
     const normalizedUrl = new URL(req.url, `http://${req.headers.host}`);
 
-    const textToConvert = normalizedUrl.pathname.slice(1);
-    const { toCase } = Object.fromEntries(normalizedUrl.searchParams.entries());
+    const originalText = normalizedUrl.pathname.slice(1);
+    const { toCase: targetCase } = Object.fromEntries(normalizedUrl.searchParams
+      .entries());
 
     const errors = [];
 
-    if (!textToConvert) {
+    if (!originalText) {
       errors.push({
-        message: 'Text to convert is required. Correct request is: "/<TEXT_TO_CONVERT>?toCase=<CASE_NAME>".',
+        message: errorsAnswers.noText,
       });
     }
 
-    if (!toCase) {
+    if (!targetCase) {
       errors.push({
-        message: '"toCase" query param is required. Correct request is: "/<TEXT_TO_CONVERT>?toCase=<CASE_NAME>".',
+        message: errorsAnswers.noToCase,
       });
-    } else if (!['SNAKE', 'KEBAB', 'CAMEL', 'PASCAL', 'UPPER'].includes(toCase)) {
+    } else if (!cases.includes(targetCase)) {
       errors.push({
-        message: 'This case is not supported. Available cases: SNAKE, KEBAB, CAMEL, PASCAL, UPPER.',
+        message: errorsAnswers.invalidToCase,
       });
     }
 
@@ -33,16 +49,20 @@ function createServer() {
 
     if (errors.length) {
       res.statusCode = 400;
+      res.statusText = 'Bad Request';
 
       res.end(JSON.stringify({ errors }));
     } else {
+      res.statusCode = 200;
+      res.statusText = 'OK';
+
       res.end(JSON.stringify({
         ...convertToCase(
-          textToConvert,
-          toCase,
+          originalText,
+          targetCase,
         ),
-        originalText: textToConvert,
-        targetCase: toCase,
+        originalText,
+        targetCase,
       }));
     }
 
