d84e3e8fdf986aa5bd2097272361344f3cb4bc06
Vadls <vadim_tanasienko54541@ukr.net>
2022-10-19 11:55:16 +0300

    Add arrays of messages and types

--- src/createServer.js
+++ src/createServer.js
@@ -4,6 +4,15 @@ const { convertToCase } = require('./convertToCase/convertToCase');
 function createServer() {
   return http.createServer((req, res) => {
     const splited = req.url.split('?');
+    const typeConvent = ['SNAKE', 'KEBAB', 'CAMEL', 'PASCAL', 'UPPER'];
+    const messages = {
+      badCase: 'This case is not supported. '
+      + 'Available cases: SNAKE, KEBAB, CAMEL, PASCAL, UPPER.',
+      noText: 'Text to convert is required. '
+      + 'Correct request is: "/<TEXT_TO_CONVERT>?toCase=<CASE_NAME>".',
+      noCase: '"toCase" query param is required. '
+      + 'Correct request is: "/<TEXT_TO_CONVERT>?toCase=<CASE_NAME>".',
+    };
 
     const errors = [];
 
@@ -15,32 +24,26 @@ function createServer() {
       searchParams = new URLSearchParams(splited[1]);
       wordToConvert = splited[0].slice(1);
 
-      const convertTo = searchParams.get('toCase');
+      const requestCase = searchParams.get('toCase');
 
-      if (convertTo === 'SNAKE'
-      || convertTo === 'KEBAB'
-      || convertTo === 'CAMEL' || convertTo === 'PASCAL'
-      || convertTo === 'UPPER') {
-        body = convertToCase(wordToConvert, convertTo);
+      if (typeConvent.includes(requestCase)) {
+        body = convertToCase(wordToConvert, requestCase);
       } else {
-        errors.push({ message: 'This case is not supported.'
-        + 'Available cases: SNAKE, KEBAB, CAMEL, PASCAL, UPPER.' });
+        errors.push({ message: messages.badCase });
       }
 
-      body.targetCase = convertTo;
+      body.targetCase = requestCase;
       body.originalText = wordToConvert;
     }
 
     res.setHeader('Content-Type', 'application/json');
 
     if (splited[0] === '/') {
-      errors.push({ message: 'Text to convert is required.'
-      + 'Correct request is: "/<TEXT_TO_CONVERT>?toCase=<CASE_NAME>".' });
+      errors.push({ message: messages.noText });
     }
 
     if (splited[1] === undefined) {
-      errors.push({ message: '"toCase" query param is required.'
-      + 'Correct request is: "/<TEXT_TO_CONVERT>?toCase=<CASE_NAME>".' });
+      errors.push({ message: messages.noCase });
     }
 
     if (errors.length === 0) {
