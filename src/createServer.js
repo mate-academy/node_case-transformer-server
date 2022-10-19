f766ed501ef26de48c9be5af221758615aec9a11
Volodymyr Chyuko <vol.chuyko@gmail.com>
2022-10-19 13:21:29 +0300

    Move serverErrors to separate file

--- src/createServer.js
+++ src/createServer.js
@@ -1,17 +1,11 @@
 const http = require('http');
 const { convertToCase } = require('./convertToCase');
+const { serverErrors } = require('./serverErrors');
 
 function createServer() {
   const server = http.createServer((req, res) => {
     const caseNames = ['SNAKE', 'KEBAB', 'CAMEL', 'PASCAL', 'UPPER'];
-    const serverErrors = {
-      noText: 'Text to convert is required. '
-        + 'Correct request is: "/<TEXT_TO_CONVERT>?toCase=<CASE_NAME>".',
-      noTargetCase: '"toCase" query param is required. '
-      + 'Correct request is: "/<TEXT_TO_CONVERT>?toCase=<CASE_NAME>".',
-      invalidTargetCase: 'This case is not supported. '
-      + 'Available cases: SNAKE, KEBAB, CAMEL, PASCAL, UPPER.',
-    };
+
     const [originalText, searchString] = req.url.slice(1).split('?');
     const searchParams = new URLSearchParams(searchString);
     const targetCase = searchParams.get('toCase');
