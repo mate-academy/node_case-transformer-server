5008b0e146c8ced42ec22bb4aa42a9b66bb1769f
Lesya Korniychuk <korniychuklesya@ukr.net>
2022-10-19 13:14:03 +0300

    fixed my cod

--- src/createServer.js
+++ src/createServer.js
@@ -7,34 +7,34 @@ function createServer() {
 
     const normaliseUrl = new URL(req.url, `http://${req.headers.host}`);
     const { pathname, searchParams } = normaliseUrl;
-    const text = pathname.slice(1);
+    const originalText = pathname.slice(1);
     const params = new URLSearchParams(searchParams);
-    const toCase = params.get('toCase');
+    const targetCase = params.get('toCase');
     const cases = ['SNAKE', 'KEBAB', 'CAMEL', 'PASCAL', 'UPPER'];
     const errors = [];
 
-    if (text === '') {
+    if (!originalText) {
       errors.push({
         message: 'Text to convert is required.'
         + ' Correct request is: "/<TEXT_TO_CONVERT>?toCase=<CASE_NAME>".',
       });
     }
 
-    if (!toCase) {
+    if (!targetCase) {
       errors.push({
         message: '"toCase" query param is required.'
         + ' Correct request is: "/<TEXT_TO_CONVERT>?toCase=<CASE_NAME>".',
       });
     }
 
-    if (!cases.includes(toCase) && toCase) {
+    if (!cases.includes(targetCase) && targetCase) {
       errors.push({
         message: 'This case is not supported.'
         + ' Available cases: SNAKE, KEBAB, CAMEL, PASCAL, UPPER.',
       });
     }
 
-    if (errors.length !== 0) {
+    if (errors.length) {
       res.statusCode = 400;
       res.statusMessage = 'Bad request';
 
@@ -45,16 +45,19 @@ function createServer() {
       return;
     }
 
-    const result = convertToCase(text, toCase);
+    const {
+      originalCase,
+      convertedText,
+    } = convertToCase(originalText, targetCase);
 
     res.statusCode = 200;
     res.statusMessage = 'OK';
 
     res.end(JSON.stringify({
-      originalCase: result.originalCase,
-      targetCase: toCase,
-      originalText: text,
-      convertedText: result.convertedText,
+      originalCase,
+      targetCase,
+      originalText,
+      convertedText,
     }));
   });
 
