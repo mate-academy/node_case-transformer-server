1d5075c6d5f7fcf6c7b61587df483813f05033bb
Lesya Korniychuk <korniychuklesya@ukr.net>
2022-10-19 09:39:54 +0300

    solutions

--- src/createServer.js
+++ src/createServer.js
@@ -1,3 +1,64 @@
-// Write code here
-// Also, you can create additional files in the src folder
-// and import (require) them here
+const http = require('http');
+const { convertToCase } = require('./convertToCase');
+
+function createServer() {
+  const server = http.createServer((req, res) => {
+    res.setHeader('Content-Type', 'application/json');
+
+    const normaliseUrl = new URL(req.url, `http://${req.headers.host}`);
+    const { pathname, searchParams } = normaliseUrl;
+    const text = pathname.slice(1);
+    const params = new URLSearchParams(searchParams);
+    const toCase = params.get('toCase');
+    const cases = ['SNAKE', 'KEBAB', 'CAMEL', 'PASCAL', 'UPPER'];
+    const errors = [];
+
+    if (text === '') {
+      errors.push({
+        message: 'Text to convert is required.'
+        + ' Correct request is: "/<TEXT_TO_CONVERT>?toCase=<CASE_NAME>".',
+      });
+    }
+
+    if (!toCase) {
+      errors.push({
+        message: '"toCase" query param is required.'
+        + ' Correct request is: "/<TEXT_TO_CONVERT>?toCase=<CASE_NAME>".',
+      });
+    }
+
+    if (!cases.includes(toCase) && toCase) {
+      errors.push({
+        message: 'This case is not supported.'
+        + ' Available cases: SNAKE, KEBAB, CAMEL, PASCAL, UPPER.',
+      });
+    }
+
+    if (errors.length !== 0) {
+      res.statusCode = 400;
+      res.statusMessage = 'Bad request';
+
+      res.end(JSON.stringify({
+        errors,
+      }));
+
+      return;
+    }
+
+    const result = convertToCase(text, toCase);
+
+    res.statusCode = 200;
+    res.statusMessage = 'OK';
+
+    res.end(JSON.stringify({
+      originalCase: result.originalCase,
+      targetCase: toCase,
+      originalText: text,
+      convertedText: result.convertedText,
+    }));
+  });
+
+  return server;
+}
+
+module.exports = { createServer };
