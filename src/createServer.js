68e638764070d6cd469cca91d95c2bf3655c80a4
Dima Romanenko <demigod177712@gmail.com>
2022-11-25 22:23:56 +0200

    case transformer server was created

--- src/createServer.js
+++ src/createServer.js
@@ -1,3 +1,74 @@
-// Write code here
-// Also, you can create additional files in the src folder
-// and import (require) them here
+const http = require('http');
+const { convertToCase } = require('./convertToCase');
+
+const errorsTypes = {
+  notOriginalText: 'Text to convert is required.'
+  + ' Correct request is: "/<TEXT_TO_CONVERT>?toCase=<CASE_NAME>".',
+  notTargetCase: '"toCase" query param is required.'
+  + ' Correct request is: "/<TEXT_TO_CONVERT>?toCase=<CASE_NAME>".',
+  wrongTargetCase: 'This case is not supported.'
+  + ' Available cases: SNAKE, KEBAB, CAMEL, PASCAL, UPPER.',
+};
+
+function createServer() {
+  const server = http.createServer((req, res) => {
+    res.setHeader('Content-Type', 'application/json');
+
+    const normaliseUrl = new URL(req.url, `http://${req.headers.host}`);
+    const { pathname, searchParams } = normaliseUrl;
+    const originalText = pathname.slice(1);
+    const params = new URLSearchParams(searchParams);
+    const targetCase = params.get('toCase');
+    const cases = ['SNAKE', 'KEBAB', 'CAMEL', 'PASCAL', 'UPPER'];
+    const errors = [];
+    const { notOriginalText, notTargetCase, wrongTargetCase } = errorsTypes;
+
+    if (!originalText) {
+      errors.push({
+        message: notOriginalText,
+      });
+    }
+
+    if (!targetCase) {
+      errors.push({
+        message: notTargetCase,
+      });
+    }
+
+    if (!cases.includes(targetCase) && targetCase) {
+      errors.push({
+        message: wrongTargetCase,
+      });
+    }
+
+    if (errors.length) {
+      res.statusCode = 400;
+      res.statusMessage = 'Bad request';
+
+      res.end(JSON.stringify({
+        errors,
+      }));
+
+      return;
+    }
+
+    const {
+      originalCase,
+      convertedText,
+    } = convertToCase(originalText, targetCase);
+
+    res.statusCode = 200;
+    res.statusMessage = 'OK';
+
+    res.end(JSON.stringify({
+      originalCase,
+      targetCase,
+      originalText,
+      convertedText,
+    }));
+  });
+
+  return server;
+}
+
+module.exports = { createServer }
