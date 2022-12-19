544e3b43312c36a5b6e9dcb059796dbf72f562b3
nazarmatsevych <nazar6192111@gmail.com>
2022-12-19 17:31:30 +0200

    Solution

--- src/createServer.js
+++ src/createServer.js
@@ -1,3 +1,53 @@
-// Write code here
-// Also, you can create additional files in the src folder
-// and import (require) them here
+const http = require('http');
+const { errorMessages } = require("./errorMessages");
+const { convertToCase } = require('./convertToCase');
+const { CASES } = require("./cases");
+
+function createServer() {
+  return http.createServer((req, res) => {
+    const normalizedURL = new URL(req.url, `http://${req.headers.host}`);
+    const originalText = normalizedURL.pathname.slice(1);
+    const targetCase = normalizedURL.searchParams.get('toCase');
+    const errors = [];
+
+    res.setHeader('Content-Type', 'application/json');
+
+    if (!originalText) {
+      errors.push({ message: errorMessages.noOriginalText });
+    }
+
+    if (targetCase && !CASES.includes(targetCase)) {
+      errors.push({ message: errorMessages.invalidCase });
+    }
+
+    if (!targetCase) {
+      errors.push({ message: errorMessages.noOriginalCase })
+    }
+
+    if (errors.length) {
+      res.statusCode = 400;
+      res.statusMessage = 'Bad request';
+      res.end(JSON.stringify({ errors }));
+
+      return;
+    }
+
+    const {
+      originalCase,
+      convertedText
+    } = convertToCase(originalText, targetCase);
+
+    res.statusCode = 200;
+    res.statusMessage = 'OK';
+
+    res.end(JSON.stringify({
+      originalCase,
+      targetCase,
+      convertedText,
+      originalText,
+    }));
+  });
+}
+
+module.exports = { createServer };
+
