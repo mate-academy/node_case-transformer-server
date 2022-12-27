dca7dabfba68f9aa0fd3f30c9fbfc7e1fc66a7cb
Svitlana Yudina <osbka31@gmail.com>
2022-12-27 18:27:04 +0200

    first commit

--- src/createServer.js
+++ src/createServer.js
@@ -1,3 +1,19 @@
+/* eslint-disable no-console */
 // Write code here
 // Also, you can create additional files in the src folder
 // and import (require) them here
+const http = require('http');
+
+function createServer() {
+  const server = http.createServer((req, resp) => {
+    console.log('request - ', req);
+    resp.end('hello');
+  });
+
+  console.log('111');
+
+  return server;
+}
+
+createServer();
+module.exports.createServer = createServer;
