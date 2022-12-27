3ed1d7cc0c1f847fde986c1e3a698ce5b5b0823b
spiritkyiv <baolt.king@gmail.com>
2022-12-28 01:21:07 +0200

    withaout correct error exception

--- src/createServer.js
+++ src/createServer.js
@@ -1,3 +1,48 @@
-// Write code here
-// Also, you can create additional files in the src folder
-// and import (require) them here
+const { detectCase } = require('./convertToCase/detectCase');
+const { convertToCase } = require('./convertToCase/convertToCase');
+
+const http = require('http');
+
+// const PORT = process.env.PORT || 8080;
+
+const createServer = () => {
+  const server = http.createServer((req, res) => {
+    let answerBody = {};
+
+    try {
+      res.statusCode = 200;
+
+      const normalizedURL = new URL(req.url, `http://${req.headers.host}`);
+
+      const toCase = normalizedURL.searchParams.get('toCase');
+
+      const text = normalizedURL.pathname.slice(1);
+      const originalData = convertToCase(text, toCase);
+
+      answerBody = {
+        originalCase: detectCase(text),
+        targetCase: toCase,
+        originalText: text,
+        convertedText: originalData.convertedText,
+      };
+    } catch (error) {
+      res.statusCode = 400;
+
+      answerBody = {
+        errors: [
+          {
+            message: String(error),
+          },
+        ],
+      };
+    }
+
+    res.setHeader('Content-Type', 'application/json');
+
+    res.end(JSON.stringify(answerBody));
+  });
+
+  return server;
+};
+
+module.exports = { createServer };
