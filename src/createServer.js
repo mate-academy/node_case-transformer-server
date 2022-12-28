bf0bf041d1f76c99419cace1c556ecb78d41d8f6
Illia Skladnik <scladnik@gmail.com>
2022-12-28 13:54:42 +0200

    Fixes after review

--- src/createServer.js
+++ src/createServer.js
@@ -1,46 +1,18 @@
+const { getQueryInfo } = require('./modules/getQueryInfo.js');
 const { convertToCase } = require('./convertToCase/convertToCase');
+const { isCodeValid } = require('./modules/isCodeValid');
 const http = require('http');
 
 const createServer = () => {
   const server = http.createServer((req, res) => {
-    const partsUrl = req.url.split('?');
-    const params = new URLSearchParams(partsUrl[1]);
-    const toCase = params.get('toCase');
-    const errors = [];
-    const text = partsUrl[0].slice(1);
-    const cases = ['SNAKE', 'KEBAB', 'CAMEL', 'PASCAL', 'UPPER'];
-
-    if (text.length === 0) {
-      errors.push({
-        // eslint-disable-next-line max-len
-        message: 'Text to convert is required. Correct request is: "/<TEXT_TO_CONVERT>?toCase=<CASE_NAME>".',
-      });
-    }
-
-    if (!toCase) {
-      errors.push({
-        // eslint-disable-next-line max-len
-        message: '"toCase" query param is required. Correct request is: "/<TEXT_TO_CONVERT>?toCase=<CASE_NAME>".',
-      });
-    }
-
-    if (!cases.includes(toCase) && toCase) {
-      errors.push({
-        // eslint-disable-next-line max-len
-        message: 'This case is not supported. Available cases: SNAKE, KEBAB, CAMEL, PASCAL, UPPER.',
-      });
-    }
+    const [text, toCase] = getQueryInfo(req);
+    const errors = isCodeValid(text, toCase);
 
     if (errors.length > 0) {
-      const errorResponse = {
-        errors,
-      };
-
       res.setHeader('Content-Type', 'application/json');
-
       res.statusCode = 400;
       res.statusText = 'Bad request';
-      res.end(JSON.stringify(errorResponse));
+      res.end(JSON.stringify({ errors }));
 
       return;
     }
@@ -55,8 +27,7 @@ const createServer = () => {
 
     res.setHeader('Content-Type', 'application/json');
     res.statusCode = 200;
-    res.write(JSON.stringify(result));
-    res.end();
+    res.end(JSON.stringify(result));
   });
 
   return server;
