d1b393a042e083cb6ba8880d94981e80491fc4ed
Andrii Bassarab <andrijbassarab@gmail.com>
2022-12-28 14:12:39 +0200

    solution

--- src/createServer.js
+++ src/createServer.js
@@ -1,3 +1,45 @@
-// Write code here
-// Also, you can create additional files in the src folder
-// and import (require) them here
+const http = require('http');
+const { convertToCase } = require('./convertToCase/');
+const { writeErrorMessage } = require('./writeErrorMessage');
+
+const createServer = () => {
+  const server = http.createServer((req, res) => {
+    res.setHeader('Content-Type', 'application/json');
+
+    const textToConvert = req.url.split('?')[0].slice(1);
+    const query = req.url.split('?')[1];
+    const params = new URLSearchParams(query);
+    const toCase = params.get('toCase');
+    const errors = writeErrorMessage(textToConvert, toCase);
+
+    if (errors.length > 0) {
+      res.statusCode = 400;
+      res.statusMessage = 'Bad request';
+
+      const responseError = {
+        errors,
+      };
+
+      res.end(JSON.stringify(responseError));
+
+      return;
+    }
+
+    const {
+      originalCase, convertedText,
+    } = convertToCase(textToConvert, toCase);
+
+    const dataText = {
+      originalCase,
+      targetCase: toCase,
+      originalText: textToConvert,
+      convertedText,
+    };
+
+    res.end(JSON.stringify(dataText));
+  });
+
+  return server;
+};
+
+module.exports = { createServer };
