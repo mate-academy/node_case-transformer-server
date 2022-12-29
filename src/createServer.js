c827a6cc7b7ca3774f6bf55c0bfde4e6fb519dcd
Artem66 <melodpi@gmail.com>
2022-12-29 04:54:41 +0000

    implemented create server

--- src/createServer.js
+++ src/createServer.js
@@ -1,3 +1,43 @@
-// Write code here
-// Also, you can create additional files in the src folder
-// and import (require) them here
+'use strict';
+
+const http = require('http');
+const { convertToCase } = require('./convertToCase/convertToCase');
+const { normalizeURL } = require('./normalizeUrl');
+const { validateErrors } = require('./validateErrors');
+
+const createServer = () => {
+  const server = http.createServer((req, res) => {
+    res.setHeader('Content-Type', 'application/json');
+
+    const paramsToConvert = normalizeURL(req.url);
+
+    const errors = validateErrors(...paramsToConvert);
+
+    if (errors.length > 0) {
+      res.statusCode = 400;
+      res.statusText = 'Bad request';
+
+      res.end(JSON.stringify({
+        errors,
+      }));
+
+      return;
+    }
+
+    const convertedText = convertToCase(...paramsToConvert);
+
+    res.statusCode = 200;
+    res.statusText = 'OK';
+
+    res.end(JSON.stringify({
+      originalCase: convertedText.originalCase,
+      targetCase: paramsToConvert[1],
+      originalText: paramsToConvert[0],
+      convertedText: convertedText.convertedText,
+    }));
+  });
+
+  return server;
+};
+
+module.exports = { createServer };
---
+++ src/validateErrors.js
@@ -0,0 +1,32 @@
+/* eslint-disable max-len */
+'use strict';
+
+const errors = [
+  {
+    message: 'Text to convert is required. Correct request is: "/<TEXT_TO_CONVERT>?toCase=<CASE_NAME>".',
+  },
+  {
+    message: '"toCase" query param is required. Correct request is: "/<TEXT_TO_CONVERT>?toCase=<CASE_NAME>".',
+  },
+  {
+    message: 'This case is not supported. Available cases: SNAKE, KEBAB, CAMEL, PASCAL, UPPER.',
+  },
+];
+
+const validateErrors = (url, toCase) => {
+  const errorMassage = [];
+
+  if (!url.length) {
+    errorMassage.push(errors[0]);
+  }
+
+  if (!toCase) {
+    errorMassage.push(errors[1]);
+  } else if (!['SNAKE', 'KEBAB', 'PASCAL', 'CAMEL', 'UPPER'].includes(toCase)) {
+    errorMassage.push(errors[2]);
+  }
+
+  return errorMassage;
+};
+
+module.exports = { validateErrors };
