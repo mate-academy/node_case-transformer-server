563a0a8dedb24c14b42a6bfa9eec5d27e746a069
Alex Tsimba <alexander.tsimba@gmail.com>
2023-01-03 09:43:32 +0200

    add solution

--- src/createServer.js
+++ src/createServer.js
@@ -1,3 +1,75 @@
-// Write code here
-// Also, you can create additional files in the src folder
-// and import (require) them here
+const http = require('http');
+const { convertToCase } = require('./convertToCase');
+
+const cases = ['SNAKE', 'KEBAB', 'CAMEL', 'PASCAL', 'UPPER'];
+const errorTypes = {
+  textIsMissing: 'Text to convert is required.'
+    + ' Correct request is: "/<TEXT_TO_CONVERT>?toCase=<CASE_NAME>".',
+  toCaseIsMissing: '"toCase" query param is required.'
+    + ' Correct request is: "/<TEXT_TO_CONVERT>?toCase=<CASE_NAME>".',
+  toCaseIsWrong: 'This case is not supported.'
+    + ' Available cases: SNAKE, KEBAB, CAMEL, PASCAL, UPPER.',
+};
+
+function createServer() {
+  const server = http.createServer((req, res) => {
+    const normalisedUrl = new URL(
+      req.url,
+      `http://${req.headers.host}`,
+    );
+    res.setHeader('Content-Type', 'application/json');
+
+    const { pathname, searchParams } = normalisedUrl;
+    const originalText = pathname.slice(1);
+    const targetCase = searchParams.get('toCase');
+
+    const errors = [];
+
+    if (!originalText) {
+      errors.push({
+        message: errorTypes.textIsMissing,
+      });
+    }
+
+    if (!targetCase) {
+      errors.push({
+        message: errorTypes.toCaseIsMissing,
+      });
+    } else if (!cases.includes(targetCase)) {
+      errors.push({
+        message: errorTypes.toCaseIsWrong,
+      });
+    }
+
+    if (errors.length) {
+      res.statusCode = 400;
+      res.statusMessage = 'Bad request';
+
+      res.end(JSON.stringify({
+        errors,
+      }));
+
+      return server;
+    }
+
+    const {
+      originalCase,
+      convertedText,
+    } = convertToCase(originalText, targetCase);
+
+    res.statusCode = 200;
+
+    res.end(
+      JSON.stringify({
+        originalCase,
+        targetCase,
+        originalText,
+        convertedText,
+      }),
+    );
+  });
+
+  return server;
+}
+
+module.exports = { createServer };
