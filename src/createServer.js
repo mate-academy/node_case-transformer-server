ada74e8f6ce276b8db65ac7511609bbeabac6715
Oleksandr Filo <filo.sanya@gmail.com>
2023-02-09 00:45:43 +0200

    case transformer server was implemented

--- src/createServer.js
+++ src/createServer.js
@@ -1,3 +1,40 @@
-// Write code here
-// Also, you can create additional files in the src folder
-// and import (require) them here
+const http = require('http');
+const { convertToCase } = require('./convertToCase/convertToCase');
+const { validateURL } = require('./validateURL');
+
+function createServer() {
+  const server = http.createServer((req, res) => {
+    const url = new URL(req.url, `http://${req.headers.host}`);
+    const originalText = url.pathname.slice(1);
+    const targetCase = url.searchParams.get('toCase');
+    const validationResult = validateURL(originalText, targetCase);
+
+    res.setHeader('Content-Type', 'application/json');
+
+    if (validationResult === 'valid URL') {
+      const {
+        originalCase,
+        convertedText,
+      } = convertToCase(originalText, targetCase);
+
+      res.statusCode = 200;
+      res.statusMessage = 'OK';
+
+      res.end(JSON.stringify({
+        originalCase,
+        targetCase,
+        originalText,
+        convertedText,
+      }));
+    } else {
+      res.statusCode = 400;
+      res.statusMessage = 'Bed request';
+
+      res.end(JSON.stringify(validationResult));
+    }
+  });
+
+  return server;
+}
+
+module.exports.createServer = createServer;
