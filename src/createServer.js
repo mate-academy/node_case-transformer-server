c4c737729d2e9121e66fbb4b7911d8da2c6d4088
Danil <dasha8854567@gmail.com>
2023-02-15 21:16:17 +0200

    completed task

--- src/createServer.js
+++ src/createServer.js
@@ -1,3 +1,45 @@
+/* eslint-disable no-console */
 // Write code here
 // Also, you can create additional files in the src folder
 // and import (require) them here
+
+const http = require('http');
+const { convertToCase } = require('./convertToCase');
+const { hendleError } = require('./hendleError');
+
+function createServer() {
+  return http.createServer((req, res) => {
+    res.setHeader('Content-Type', 'application/json');
+
+    const normalizeURL = new URL(req.url, `http://${req.headers.host}`);
+
+    const targetCase = normalizeURL.searchParams.get('toCase');
+    const originalText = normalizeURL.pathname.slice(1);
+
+    const errors = hendleError(originalText, targetCase);
+
+    if (errors.length > 0) {
+      res.statusCode = 400;
+      res.statusMessage = 'Bad request';
+
+      res.end(JSON.stringify({
+        errors,
+      }));
+
+      return;
+    }
+
+    res.statusCode = 200;
+    res.statusMessage = 'OK';
+
+    res.end(JSON.stringify({
+      ...convertToCase(originalText, targetCase),
+      originalText,
+      targetCase,
+    }));
+  });
+}
+
+createServer();
+
+module.exports.createServer = createServer;
