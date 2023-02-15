1209c80265c13666b7aa1f0642a7135bea3a37b1
GabrielDeveloper <89942450+GabrielMaksimovich@users.noreply.github.com>
2023-02-15 18:43:03 +0200

    solution

--- src/createServer.js
+++ src/createServer.js
@@ -1,41 +1,10 @@
 const http = require('http');
 const { convertToCase } = require('./convertToCase');
-
-const possibleCases = ['SNAKE', 'KEBAB', 'CAMEL', 'PASCAL', 'UPPER'];
-
-const verifyData = (text, toCase) => {
-  const errors = [];
-
-  if (!text) {
-    errors.push({
-      message:
-        'Text to convert is required.'
-        + ' Correct request is: "/<TEXT_TO_CONVERT>?toCase=<CASE_NAME>".',
-    });
-  }
-
-  if (!toCase) {
-    errors.push({
-      message:
-        '"toCase" query param is required.'
-        + ' Correct request is: "/<TEXT_TO_CONVERT>?toCase=<CASE_NAME>".',
-    });
-  }
-
-  if (toCase && !possibleCases.includes(toCase)) {
-    errors.push({
-      message:
-        'This case is not supported.'
-        + ' Available cases: SNAKE, KEBAB, CAMEL, PASCAL, UPPER.',
-    });
-  }
-
-  return errors;
-};
+const { verifyData } = require('./verifyData');
 
 const createServer = () => {
   const server = http.createServer((req, res) => {
-    const normalizedUrl = new URL(req.url, `http://${req.headers.host}`);
+    const normalizedUrl = new URL(req.url, `https://${req.headers.host}`);
     const text = normalizedUrl.pathname.slice(1);
     const toCase = normalizedUrl.searchParams.get('toCase');
     const errors = verifyData(text, toCase);
@@ -46,10 +15,12 @@ const createServer = () => {
       res.statusCode = 400;
       res.statusMessage = 'Bad request';
       res.end(JSON.stringify({ errors }));
+
       return;
     }
 
     const convertedText = convertToCase(text, toCase);
+
     res.statusCode = 200;
     res.statusMessage = 'OK';
 
---
+++ src/verifyData.js
@@ -0,0 +1,35 @@
+'use strict';
+
+const possibleCases = ['SNAKE', 'KEBAB', 'CAMEL', 'PASCAL', 'UPPER'];
+
+const verifyData = (text, toCase) => {
+  const errors = [];
+
+  if (!text) {
+    errors.push({
+      message:
+        'Text to convert is required.'
+        + ' Correct request is: "/<TEXT_TO_CONVERT>?toCase=<CASE_NAME>".',
+    });
+  }
+
+  if (!toCase) {
+    errors.push({
+      message:
+        '"toCase" query param is required.'
+        + ' Correct request is: "/<TEXT_TO_CONVERT>?toCase=<CASE_NAME>".',
+    });
+  }
+
+  if (toCase && !possibleCases.includes(toCase)) {
+    errors.push({
+      message:
+        'This case is not supported.'
+        + ' Available cases: SNAKE, KEBAB, CAMEL, PASCAL, UPPER.',
+    });
+  }
+
+  return errors;
+};
+
+module.exports = { verifyData };
