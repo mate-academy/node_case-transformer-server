c6913a3ae24382567e2b8808d03ab79de48dcee8
mr-valentos <nerpavlad@gmail.com>
2023-02-15 16:12:11 +0200

    rm comments

--- src/createServer.js
+++ src/createServer.js
@@ -1,6 +1,3 @@
-// Write code here
-// Also, you can create additional files in the src folder
-// and import (require) them here
 const http = require('http');
 const { convertToCase } = require('./convertToCase');
 const { errorCheck } = require('./errorCheck');
@@ -9,7 +6,7 @@ function createServer() {
   const server = http.createServer((req, res) => {
     const normalizeURL = new URL(req.url, `http://${req.headers.host}`);
 
-    const originalText = normalizeURL.pathname.slice(1); // ????
+    const originalText = normalizeURL.pathname.slice(1);
     const targetCase = normalizeURL.searchParams.get('toCase');
 
     res.setHeader('Content-Type', 'application/json');
