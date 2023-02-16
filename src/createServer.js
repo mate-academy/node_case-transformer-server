113a0d6b823870834a4bc03e0b9dd03aebcbe3d7
Vasya Rymarenko <vrima60@gmail.com>
2023-02-16 20:45:18 +0200

    Server for case transform was created

--- src/createServer.js
+++ src/createServer.js
@@ -1,3 +1,42 @@
-// Write code here
-// Also, you can create additional files in the src folder
-// and import (require) them here
+const http = require('http');
+const { convertToCase } = require('./convertToCase/convertToCase.js');
+const { validateURL } = require('./validateURL.js');
+
+const createServer = () => {
+  const server = http.createServer((req, resp) => {
+    const normalizedURL = new URL(req.url, `http://${req.headers.host}`);
+    const targetCase = normalizedURL.searchParams.get('toCase');
+    const originalText = req.url.split('?')[0].slice(1);
+    const validationResult = validateURL(originalText, targetCase);
+
+    resp.setHeader('Content-Type', 'application/json');
+    // eslint-disable-next-line no-console
+    console.log(JSON.stringify(validationResult));
+
+    if (validationResult === 'valid') {
+      const {
+        originalCase,
+        convertedText,
+      } = convertToCase(originalText, targetCase);
+
+      resp.statusCode = 200;
+      resp.statusMessage = 'OK';
+
+      resp.end(JSON.stringify({
+        originalCase,
+        targetCase,
+        originalText,
+        convertedText,
+      }));
+    } else {
+      resp.statusCode = 400;
+      resp.statusMessage = 'Bad request';
+
+      resp.end(JSON.stringify(validationResult));
+    }
+  });
+
+  return server;
+};
+
+module.exports = { createServer };
