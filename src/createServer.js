595c83f9dd57c3ff6784e991a83866b1f029538f
Vladimir Arkanov <vladimirarkanov@icloud.com>
2023-02-17 14:00:25 +0200

    first commit

--- src/createServer.js
+++ src/createServer.js
@@ -1,3 +1,48 @@
-// Write code here
-// Also, you can create additional files in the src folder
-// and import (require) them here
+/* eslint-disable max-len */
+const http = require('http');
+const { convertToCase } = require('./convertToCase/convertToCase');
+const { isError } = require('./isError');
+
+function createServer() {
+  const server = http.createServer((req, res) => {
+    res.setHeader('Content-Type', 'application/json');
+
+    const forPreparingUrl = req.url.split('?');
+
+    const textToConvert = forPreparingUrl[0].replace('/', '');
+
+    const params = new URLSearchParams(forPreparingUrl[1]);
+    const toCase = params.get('toCase');
+
+    const originalText = textToConvert;
+
+    const targetCase = toCase;
+
+    const errors = isError(originalText, toCase);
+
+    if (errors.length !== 0) {
+      res.statusCode = 400;
+      res.statusMessage = 'Bad request';
+
+      return res.end(JSON.stringify({ errors }));
+    }
+
+    const { convertedText, originalCase } = convertToCase(originalText, targetCase);
+
+    const preparedToShow = {
+      convertedText,
+      originalCase,
+      originalText,
+      targetCase,
+    };
+
+    res.statusCode = 200;
+    res.statusMessage = 'OK';
+
+    res.end(JSON.stringify(preparedToShow));
+  });
+
+  return server;
+}
+
+module.exports = { createServer };
