75e5a0ce72151db407a0876dbae3edda74341a90
Viktoriia Kravchenko <kravchenkovictoriya9@gmail.com>
2023-02-20 01:09:09 +0200

    Created a server

--- src/createServer.js
+++ src/createServer.js
@@ -1,3 +1,43 @@
-// Write code here
-// Also, you can create additional files in the src folder
-// and import (require) them here
+const http = require('http');
+const { URL } = require('url');
+
+const { convertToCase } = require('./convertToCase');
+const { validateData } = require('./validateData');
+
+const createServer = () => {
+  const server = http.createServer((req, res) => {
+    const url = new URL(req.url, `http://${req.headers.host}`);
+    const requestPath = url.pathname.slice(1); // 'UnitedStates'
+    const entries = Object.fromEntries(url.searchParams.entries());
+    // { toCase: 'UPPER'}
+    const query = entries.toCase; // 'UPPER'
+
+    res.setHeader('Content-Type', 'application/json');
+
+    const foundErrors = validateData(requestPath, query);
+
+    if (foundErrors.length > 0) {
+      res.statusCode = 400;
+      res.statusMessage = 'Bad request';
+      res.end(JSON.stringify({ errors: foundErrors }));
+
+      return;
+    }
+
+    res.statusCode = 200;
+    res.statusMessage = 'OK';
+
+    const providedData = {
+      targetCase: query,
+      originalText: requestPath,
+    };
+    const convertedPart = convertToCase(requestPath, query);
+    const okResponse = Object.assign({}, convertedPart, providedData);
+
+    res.end(JSON.stringify(okResponse));
+  });
+
+  return server;
+};
+
+module.exports = { createServer };
