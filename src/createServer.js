cec7e523b07d0a60ce0eca0f2d204547f4ea4c2f
Artem Rymarchuk <rumarchyk204@gmail.com>
2023-02-20 09:11:43 +0100

    Some refactoring

--- src/createServer.js
+++ src/createServer.js
@@ -1,49 +1,18 @@
 const http = require('http');
+const getUrlErrors = require('./getUrlErrors').getUrlErrors;
 const convertToCase
   = require('./convertToCase/convertToCase').convertToCase;
-
-function getURLErrors(orirginalText, targetCase) {
-  const arrOfMessages = [];
-  const availableCases = ['SNAKE', 'KEBAB', 'CAMEL', 'PASCAL', 'UPPER'];
-
-  if (orirginalText === '/') {
-    arrOfMessages.push({
-      message:
-        'Text to convert is required. Correct request is: '
-        + '"/<TEXT_TO_CONVERT>?toCase=<CASE_NAME>".',
-    });
-  }
-
-  if (targetCase === null) {
-    arrOfMessages.push({
-      message:
-      '"toCase" query param is required. Correct request is: '
-      + '"/<TEXT_TO_CONVERT>?toCase=<CASE_NAME>".',
-    });
-  }
-
-  if (targetCase && !availableCases.includes(targetCase)) {
-    arrOfMessages.push({
-      message:
-      'This case is not supported. Available cases: '
-      + 'SNAKE, KEBAB, CAMEL, PASCAL, UPPER.',
-    });
-  }
-
-  return arrOfMessages;
-}
 
 function createServer() {
   const server = http.createServer((req, res) => {
     const parts = req.url.split('?');
-    const textToConvert = parts[0];
-    const originalText = textToConvert.slice(1);
+    const originalText = parts[0].slice(1);
     const params = new URLSearchParams(parts[1]);
     const targetCase = params.get('toCase');
 
     res.setHeader('Content-Type', 'application/json');
 
-    const errors = getURLErrors(textToConvert, targetCase);
+    const errors = getUrlErrors(originalText, targetCase);
 
     if (errors.length !== 0) {
       res.statusCode = 400;
