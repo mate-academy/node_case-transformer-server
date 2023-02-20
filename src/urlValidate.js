e1cdefa862a70eebc9c35f92112d18bb3bb716be
TetKopot <tet.kopot@gmail.com>
2023-02-20 13:58:23 +0200

    add file

---
+++ src/urlValidate.js
@@ -0,0 +1,29 @@
+function urlValidate(query, toCase) {
+  const instances = ['SNAKE', 'KEBAB', 'CAMEL', 'PASCAL', 'UPPER'];
+  const errors = [];
+
+  if (!query) {
+    errors.push({
+      message: '"Text to convert is required'
+        + ' Correct request is: "/<TEXT_TO_CONVERT>?toCase=<CASE_NAME>".',
+    });
+  }
+
+  if (!toCase) {
+    errors.push({
+      message: '"toCase query param is required'
+        + ' Correct request is: "/<TEXT_TO_CONVERT>?toCase=<CASE_NAME>".',
+    });
+  }
+
+  if (toCase && !instances.includes(toCase)) {
+    errors.push({
+      message: 'This case is not supported'
+        + ' Available cases: SNAKE, KEBAB, CAMEL, PASCAL, UPPER',
+    });
+  }
+
+  return errors;
+};
+
+module.exports = { urlValidate };
