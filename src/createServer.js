fde2759f8daab8f8d4232f88990071071ff6fe4c
maxim2310 <m23a09x96@gmail.com>
2023-02-28 00:38:29 +0200

    server is working

--- src/createServer.js
+++ src/createServer.js
@@ -1,3 +1,36 @@
-// Write code here
-// Also, you can create additional files in the src folder
-// and import (require) them here
+
+const http = require('http');
+const { convertToCase } = require('./convertToCase/convertToCase');
+const { validator } = require('./validator');
+
+function createServer() {
+  const server = http.createServer((req, res) => {
+    res.setHeader('Content-Type', 'application/json');
+
+    const normalizeURL = new URL(req.url, `http://${req.headers.host}`);
+    const sourseText = normalizeURL.pathname.split('/').slice(-1)[0];
+    const toCase = normalizeURL.searchParams.get('toCase');
+    const errors = validator(sourseText, toCase);
+
+    if (errors.errors.length) {
+      res.statusCode = 404;
+      res.statusMessage = 'something go wrong';
+      res.end(JSON.stringify(errors));
+    } else {
+      res.statusCode = 200;
+
+      const convertedText = convertToCase(sourseText, toCase);
+
+      res.end(JSON.stringify({
+        ...convertedText,
+        targetCase: toCase,
+        originalText: sourseText,
+
+      }));
+    }
+  });
+
+  return server;
+}
+
+module.exports = { createServer };
