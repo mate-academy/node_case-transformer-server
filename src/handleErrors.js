0822cd907500d675a7d9ec38b5ed0cc3a3391c0e
Anna Inozemets <inoznetta@gmail.com>
2023-03-06 21:04:33 +0200

    Initial commit of creating server

---
+++ src/handleErrors.js
@@ -0,0 +1,28 @@
+/* eslint-disable max-len */
+function handleErrors(textToConvert, toCase) {
+  const errors = [];
+
+  if (!textToConvert) {
+    errors.push({
+      message: 'Text to convert is required. Correct request is: "/<TEXT_TO_CONVERT>?toCase=<CASE_NAME>".',
+    });
+  }
+
+  if (!toCase) {
+    errors.push({
+      message: '"toCase" query param is required. Correct request is: "/<TEXT_TO_CONVERT>?toCase=<CASE_NAME>".',
+    });
+  }
+
+  if (!(['SNAKE', 'KEBAB', 'CAMEL', 'PASCAL', 'UPPER'].includes(toCase)) && toCase) {
+    errors.push({
+      message: 'This case is not supported. Available cases: SNAKE, KEBAB, CAMEL, PASCAL, UPPER.',
+    });
+  }
+
+  return { errors };
+}
+
+module.exports = {
+  handleErrors,
+};
