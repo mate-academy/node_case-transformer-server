85c1932776a43ce7192fca70403cac9aedd768a4
PolinaSukhorukova <sukhorukovapolina03@gmail.com>
2023-03-06 21:26:50 +0200

    case transform solution

--- src/createServer.js
+++ src/createServer.js
@@ -1,3 +1,35 @@
-// Write code here
-// Also, you can create additional files in the src folder
-// and import (require) them here
+const http = require('http');
+const { convertToCase } = require('./convertToCase/convertToCase');
+const { validator } = require('./validator');
+
+const createServer = () => {
+  const server = http.createServer((req, res) => {
+    res.setHeader('Content-Type', 'application/json');
+
+    const targetURL = new URL(req.url, `http://${req.headers.host}`);
+    const originalText = targetURL.pathname.slice(1);
+    const targetCase = targetURL.searchParams.get('toCase');
+    const errors = validator(originalText, targetCase);
+    const hasError = Boolean(errors.errors.length);
+
+    if (hasError) {
+      req.statusCode = 404;
+      req.statusMessage = 'Bad request';
+      res.end(JSON.stringify(errors));
+    } else {
+      const convertedText = convertToCase(originalText, targetCase);
+
+      const requestData = JSON.stringify({
+        ...convertedText,
+        targetCase,
+        originalText,
+      });
+
+      res.end(requestData);
+    }
+  });
+
+  return server;
+};
+
+module.exports = { createServer };
