506e3fea1c5b97cfb7d64e8303d578e72948b686
Oleh Holovnykh <shankar.oleh@gmail.com>
2023-03-07 22:59:23 +0200

    implemented server with case transformer logic

---
+++ src/HandleErrors.js
@@ -0,0 +1,23 @@
+function handleErrors(textToConvert, toCase) {
+  const errors = [];
+  const supportedCases = ['SNAKE', 'KEBAB', 'CAMEL', 'PASCAL', 'UPPER'];
+
+  if (!textToConvert) {
+    errors.push({ message: 'Text to convert is required.'
+    + ' Correct request is: "/<TEXT_TO_CONVERT>?toCase=<CASE_NAME>".' });
+  }
+
+  if (!toCase) {
+    errors.push({ message: '"toCase" query param is required.'
+    + ' Correct request is: "/<TEXT_TO_CONVERT>?toCase=<CASE_NAME>".' });
+  } else {
+    if (!supportedCases.includes(toCase)) {
+      errors.push({ message: 'This case is not supported.'
+      + ' Available cases: SNAKE, KEBAB, CAMEL, PASCAL, UPPER.' });
+    }
+  }
+
+  return errors;
+}
+
+module.exports = { handleErrors };
--- src/createServer.js
+++ src/createServer.js
@@ -1,3 +1,37 @@
-// Write code here
-// Also, you can create additional files in the src folder
-// and import (require) them here
+const http = require('http');
+const { convertToCase } = require('./convertToCase/convertToCase');
+const { handleErrors } = require('./HandleErrors');
+
+function createServer() {
+  const server = http.createServer((req, res) => {
+    const [text, queryString] = req.url.split('?');
+    const params = new URLSearchParams(queryString);
+
+    const textToConvert = text.slice(1);
+    const toCase = params.get('toCase');
+    const errors = handleErrors(textToConvert, toCase);
+
+    if (errors.length > 0) {
+      res.writeHead(400, { 'Content-Type': 'application/json' });
+      res.end(JSON.stringify({ errors }));
+
+      return;
+    }
+
+    const convertedText = convertToCase(textToConvert, toCase).convertedText;
+    const originalCase = convertToCase(textToConvert, toCase).originalCase;
+
+    res.writeHead(200, { 'Content-Type': 'application/json' });
+
+    res.end(JSON.stringify({
+      originalCase,
+      targetCase: toCase,
+      originalText: textToConvert,
+      convertedText,
+    }));
+  });
+
+  return server;
+}
+
+module.exports = { createServer };
