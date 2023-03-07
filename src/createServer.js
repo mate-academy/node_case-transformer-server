14f7d8e5fb36b5baa6b42224b86ea4433c4f1d7e
Bohdan Kutasevich <obvinmusic@gmail.com>
2023-03-07 15:06:18 +0200

    Task done. Have a good time!)

--- src/createServer.js
+++ src/createServer.js
@@ -1,3 +1,44 @@
 // Write code here
 // Also, you can create additional files in the src folder
 // and import (require) them here
+const http = require('http');
+const { convertToCase } = require('./convertToCase');
+const { getErrors } = require('./getErrors');
+
+const createServer = () => {
+  const server = http.createServer((req, res) => {
+    const normilizedURL = new URL(req.url, `http://${req.headers.host}`);
+    const textToTransform = normilizedURL.pathname.slice(1);
+    const typeToTransform = normilizedURL.searchParams.get('toCase');
+    const errors = getErrors(textToTransform, typeToTransform);
+
+    if (!errors.length) {
+      const transformedData = convertToCase(textToTransform, typeToTransform);
+
+      const response = {
+        ...transformedData,
+        targetCase: typeToTransform,
+        originalText: textToTransform,
+      };
+
+      res.setHeader('Content-Type', 'application/json');
+      res.write(JSON.stringify(response));
+      res.end();
+    } else {
+      const response = {
+        errors,
+      };
+
+      res.setHeader('Content-Type', 'application/json');
+      res.statusCode = 400;
+      res.write(JSON.stringify(response));
+      res.end();
+    };
+  });
+
+  return server;
+};
+
+module.exports = {
+  createServer,
+};
