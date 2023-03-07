f6fd1c913e2bac6eebd5f8b604a6789adecdc762
Andrey Berezyanskiy <andrey.berezyanskiy@gmail.com>
2023-03-07 16:54:01 +0200

    server was created

--- src/createServer.js
+++ src/createServer.js
@@ -1,3 +1,41 @@
-// Write code here
-// Also, you can create additional files in the src folder
-// and import (require) them here
+const http = require('http');
+
+const { convertToCase } = require('./convertToCase');
+const { checkForErrors } = require('./utils/checkForErrors');
+
+function createServer() {
+  const server = http.createServer((req, res) => {
+    res.setHeader('Content-Type', 'application/json');
+
+    const normalizedURL = new URL(req.url, `http://${req.headers.host}`);
+
+    const textToReplace = normalizedURL.pathname.slice(1);
+    const caseToConvert = normalizedURL.searchParams.get('toCase');
+
+    const errors = checkForErrors(textToReplace, caseToConvert);
+    const hasError = errors.length > 0;
+
+    if (hasError) {
+      res.end(JSON.stringify({
+        errors,
+      }));
+
+      res.statusCode = 400;
+    } else {
+      const result = convertToCase(textToReplace, caseToConvert);
+
+      res.end(JSON.stringify({
+        originalCase: result.originalCase,
+        targetCase: caseToConvert,
+        originalText: textToReplace,
+        convertedText: result.convertedText,
+      }));
+
+      res.statusCode = 200;
+    }
+  });
+
+  return server;
+}
+
+module.exports = { createServer };
