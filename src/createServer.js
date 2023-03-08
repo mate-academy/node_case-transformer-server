a057d7c2efd690ad7ca1c781aa01d2da613566eb
Olha Yevstifieieva <hob.bilbo@gmail.com>
2023-03-08 15:03:13 +0200

    Add server with case-function

--- src/createServer.js
+++ src/createServer.js
@@ -1,3 +1,48 @@
-// Write code here
-// Also, you can create additional files in the src folder
-// and import (require) them here
+const http = require('http');
+const { convertToCase } = require('./convertToCase');
+const { handleErrorMessage } = require('./handleError');
+
+function createServer() {
+  const server = http.createServer((req, res) => {
+    res.setHeader('Content-Type', 'application/json');
+
+    const normalizedUrl = new URL(req.url, `http://${req.headers.host}`);
+
+    const textToConvert = normalizedUrl.pathname.slice(1);
+
+    const params = Object.fromEntries(normalizedUrl.searchParams.entries());
+
+    const toCase = params.toCase;
+
+    const errors = handleErrorMessage(textToConvert, toCase);
+
+    if (errors.length) {
+      res.statusCode = 400;
+      res.statusMessage = 'Bad request';
+
+      res.end(JSON.stringify({ errors }));
+
+      return;
+    }
+
+    const { originalCase, convertedText } = convertToCase(
+      textToConvert, toCase,
+    );
+
+    res.statusCode = 200;
+    res.statusMessage = 'OK';
+
+    res.end(JSON.stringify({
+      originalCase,
+      convertedText,
+      targetCase: toCase,
+      originalText: textToConvert,
+    }));
+  });
+
+  return server;
+}
+
+module.exports = {
+  createServer,
+};
