be6c8ac0c07742f7fdc1fe1156b6bab1275f065a
Andrii Andriushchenko <andrewandrew1604@gmail.com>
2023-03-08 14:47:06 +0200

    Fixes

--- src/convertToCase/getErrors.js
+++ src/convertToCase/getErrors.js
@@ -1,28 +1,34 @@
 'use strict';
 
+const errorCases = [
+  {
+    message: 'Text to convert is required. '
+      + 'Correct request is: "/<TEXT_TO_CONVERT>?toCase=<CASE_NAME>".',
+  },
+  {
+    message: '"toCase" query param is required. '
+      + 'Correct request is: "/<TEXT_TO_CONVERT>?toCase=<CASE_NAME>".',
+  },
+  {
+    message: 'This case is not supported. '
+      + 'Available cases: SNAKE, KEBAB, CAMEL, PASCAL, UPPER.',
+  },
+];
+
 const getErrors = (currText, toCase) => {
   const payload = [];
   const availableCases = ['SNAKE', 'KEBAB', 'CAMEL', 'PASCAL', 'UPPER'];
 
   if (!currText) {
-    payload.push({
-      message: 'Text to convert is required. '
-        + 'Correct request is: "/<TEXT_TO_CONVERT>?toCase=<CASE_NAME>".',
-    });
+    payload.push(errorCases[0]);
   }
 
   if (!toCase) {
-    payload.push({
-      message: '"toCase" query param is required. '
-        + 'Correct request is: "/<TEXT_TO_CONVERT>?toCase=<CASE_NAME>".',
-    });
+    payload.push(errorCases[1]);
   }
 
   if (toCase && !availableCases.includes(toCase)) {
-    payload.push({
-      message: 'This case is not supported. '
-        + 'Available cases: SNAKE, KEBAB, CAMEL, PASCAL, UPPER.',
-    });
+    payload.push(errorCases[2]);
   }
 
   return payload;
