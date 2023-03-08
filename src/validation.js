b6ac0432e4af1d49c50083b28f8580853f6b7298
Andrew Borys <borysandrey1988@gmail.com>
2023-03-08 23:36:29 +0200

    solution-1

---
+++ src/validation.js
@@ -0,0 +1,35 @@
+const supportedCases = ['SNAKE', 'KEBAB', 'CAMEL', 'PASCAL', 'UPPER'];
+/* eslint-disable max-len */
+const errorsMessage = {
+  emptyText: 'Text to convert is required. Correct request is: "/<TEXT_TO_CONVERT>?toCase=<CASE_NAME>".',
+  emptyCase: '"toCase" query param is required. Correct request is: "/<TEXT_TO_CONVERT>?toCase=<CASE_NAME>".',
+  notSupportedCase: 'This case is not supported. Available cases: SNAKE, KEBAB, CAMEL, PASCAL, UPPER.',
+};
+
+const validator = (text, targetCase) => {
+  const allErrors = [];
+
+  if (!text) {
+    allErrors.push({
+      message: errorsMessage.emptyText,
+    });
+  }
+
+  if (!targetCase) {
+    allErrors.push({
+      message: errorsMessage.emptyCase,
+    });
+  }
+
+  if (!supportedCases.includes(targetCase) && targetCase) {
+    allErrors.push({
+      message: errorsMessage.notSupportedCase,
+    });
+  }
+
+  return { errors: allErrors };
+};
+
+module.exports = {
+  validator,
+};
