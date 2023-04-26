ff44ecc85b16b2ad83e35a3fc84bbbbe75f2b28d
Mykola Hadupiak <mykola.hadupiak@gmail.com>
2023-04-26 20:01:26 +0300

    Case transformet server

---
+++ src/errorMessages.js
@@ -0,0 +1,29 @@
+/* eslint-disable max-len */
+const errorMessages = (text, toCase) => {
+  const cases = ['SNAKE', 'KEBAB', 'CAMEL', 'PASCAL', 'UPPER'];
+  const errors = [];
+
+  if (!text) {
+    errors.push({
+      message: 'Text to convert is required. Correct request is: "/<TEXT_TO_CONVERT>?toCase=<CASE_NAME>".',
+    });
+  }
+
+  if (!toCase) {
+    errors.push({
+      message: '"toCase" query param is required. Correct request is: "/<TEXT_TO_CONVERT>?toCase=<CASE_NAME>".',
+    });
+  }
+
+  if (!cases.includes(toCase) && toCase) {
+    errors.push({
+      message: `This case is not supported. Available cases: ${cases.join(', ')}.`,
+    });
+  }
+
+  return errors;
+};
+
+module.exports = {
+  errorMessages,
+};
