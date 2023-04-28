631c6ed23983f372915cd4c1f9e74845ad3fe4ef
Dmytro <kuharchuk.dmytro1337@gmail.com>
2023-04-29 00:10:26 +0300

    case-transformer-server was implement

--- src/createServer.js
+++ src/createServer.js
@@ -1,3 +1,36 @@
-// Write code here
-// Also, you can create additional files in the src folder
-// and import (require) them here
+const http = require('http');
+const { convertToCase } = require('./convertToCase');
+const { getErrors } = require('./getErrors');
+
+const createServer = () => {
+  return http.createServer((request, response) => {
+    const [text, searchParams] = request.url.slice(1).split('?');
+    const params = new URLSearchParams(searchParams);
+    const toCase = params.get('toCase');
+    const errors = getErrors(text, toCase);
+
+    response.setHeader('Content-Type', 'application/json');
+
+    if (errors.length > 0) {
+      response.statusCode = 400;
+      response.end(JSON.stringify({ errors }));
+
+      return;
+    }
+
+    response.statusCode = 200;
+
+    const { originalCase, convertedText } = convertToCase(text, toCase);
+
+    response.end(JSON.stringify({
+      originalCase,
+      targetCase: toCase,
+      originalText: text,
+      convertedText,
+    }));
+  });
+};
+
+module.exports = {
+  createServer,
+};
