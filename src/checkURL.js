2f8049e35f3fd800ae5b1ceb85664cff2e024ad4
Anton-Tretiak <tretiak.ap@gmail.com>
2023-05-02 15:08:52 +0300

    Implement case-transformer task

---
+++ src/checkURL.js
@@ -0,0 +1,32 @@
+const cases = ['SNAKE', 'KEBAB', 'CAMEL', 'PASCAL', 'UPPER'];
+
+const checkURL = (text, toCase) => {
+  const errors = [];
+  const correctExample = '"/<TEXT_TO_CONVERT>?toCase=<CASE_NAME>".';
+
+  if (!text) {
+    errors.push({
+      message: 'Text to convert is required. Correct request is: '
+        + correctExample,
+    });
+  }
+
+  if (!toCase) {
+    errors.push({
+      message: '"toCase" query param is required. Correct request is: '
+        + correctExample,
+    });
+  }
+
+  if (!cases.includes(toCase) && toCase) {
+    errors.push({
+      message: `This case is not supported. Available cases: ${cases.join(', ')}.`,
+    });
+  }
+
+  return errors;
+};
+
+module.exports = {
+  checkURL,
+};
--- src/createServer.js
+++ src/createServer.js
@@ -1,3 +1,41 @@
-// Write code here
-// Also, you can create additional files in the src folder
-// and import (require) them here
+const http = require('http');
+
+const { checkURL } = require('./checkURL');
+const { convertToCase } = require('./convertToCase/convertToCase');
+
+const createServer = () => {
+  return http.createServer((request, response) => {
+    response.setHeader('Content-Type', 'application/json');
+
+    const path = request.url.slice(1);
+    const [text, queryParams] = path.split('?');
+    const params = new URLSearchParams(queryParams);
+    const toCase = params.get('toCase');
+    const errors = checkURL(text, toCase);
+
+    if (errors.length) {
+      response.statusCode = 400;
+      response.end(JSON.stringify({ errors }));
+
+      return;
+    }
+
+    const { originalCase, convertedText } = convertToCase(text, toCase);
+
+    const result = JSON.stringify({
+      originalCase,
+      targetCase: toCase,
+      originalText: text,
+      convertedText,
+    });
+
+    response.statusCode = 200;
+    response.statusMessage = 'OK';
+
+    response.end(result);
+  });
+};
+
+module.exports = {
+  createServer,
+};
