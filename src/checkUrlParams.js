69825c34b058a64d88bbf7236ab86f0b361278b9
Denys Dubas <den.dubas.44@gmail.com>
2023-05-02 19:01:38 +0300

    Solution

---
+++ src/checkUrlParams.js
@@ -0,0 +1,28 @@
+/* eslint-disable max-len */
+const supportedCases = ['SNAKE', 'KEBAB', 'CAMEL', 'PASCAL', 'UPPER'];
+
+const checkUrlParams = (text, toCase) => {
+  const errors = [];
+
+  if (!text) {
+    errors.push({
+      message: 'Text to convert is required. Correct request is: "/<TEXT_TO_CONVERT>?toCase=<CASE_NAME>".',
+    });
+  }
+
+  if (!toCase) {
+    errors.push({
+      message: '"toCase" query param is required. Correct request is: "/<TEXT_TO_CONVERT>?toCase=<CASE_NAME>".',
+    });
+  }
+
+  if (toCase && !supportedCases.includes(toCase)) {
+    errors.push({
+      message: `This case is not supported. Available cases: ${supportedCases.join(', ')}.`,
+    });
+  }
+
+  return errors;
+};
+
+module.exports = { checkUrlParams };
