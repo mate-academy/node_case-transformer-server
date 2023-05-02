f25ec102846cfd78ce92d8a5c99429353cc5310a
Maksym <maksymmaliuk98@gmail.com>
2023-05-02 18:48:28 +0300

    Task done

---
+++ src/Errors.js
@@ -0,0 +1,23 @@
+function Errors(text, toCase) {
+  const typeOfCases = ['SNAKE', 'KEBAB', 'CAMEL', 'PASCAL', 'UPPER'];
+  const errors = [];
+
+  if (!text) {
+    errors.push({ message: 'Text to convert is required. '
+    + 'Correct request is: "/<TEXT_TO_CONVERT>?toCase=<CASE_NAME>".' });
+  }
+
+  if (!toCase) {
+    errors.push({ message: '"toCase" query param is required. '
+    + 'Correct request is: "/<TEXT_TO_CONVERT>?toCase=<CASE_NAME>".' });
+  }
+
+  if (!typeOfCases.includes(toCase) && toCase) {
+    errors.push({ message: 'This case is not supported. '
+    + 'Available cases: SNAKE, KEBAB, CAMEL, PASCAL, UPPER.' });
+  }
+
+  return errors;
+}
+
+module.exports = { Errors };
