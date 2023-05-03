4a4843d0a64bdad72fcc36d7badff3f36781d592
kulakostas <92936150+kulakostas@users.noreply.github.com>
2023-05-03 18:47:22 +0300

    node_case-transformer-server solution

--- src/createServer.js
+++ src/createServer.js
@@ -1,3 +1,36 @@
-// Write code here
-// Also, you can create additional files in the src folder
-// and import (require) them here
+const http = require('http');
+const { convertToCase } = require('./convertToCase/convertToCase');
+const { handleErrors } = require('./handleErrors');
+
+const createServer = () => {
+  const server = http.createServer((req, res) => {
+    const originalText = req.url.split('?').slice(0, 1).join('').slice(1);
+    const queryString = req.url.split('?').slice(1).join('');
+    const params = new URLSearchParams(queryString);
+    const targetCase = params.get('toCase');
+    const errors = handleErrors(originalText, targetCase);
+
+    if (errors.length) {
+      res.statusCode = 400;
+      res.statusMessage = 'Bad request';
+      res.setHeader('Content-Type', 'application/json');
+      res.end(JSON.stringify({ errors }));
+    } else {
+      res.statusCode = 200;
+      res.statusMessage = 'OK';
+      res.setHeader('Content-Type', 'application/json');
+
+      const convertedText = convertToCase(originalText, targetCase);
+
+      res.end(JSON.stringify({
+        ...convertedText,
+        targetCase,
+        originalText,
+      }));
+    }
+  });
+
+  return server;
+};
+
+module.exports = { createServer };
---
+++ src/handleErrors.js
@@ -0,0 +1,34 @@
+const handleErrors = (text, caseName) => {
+  const caseNames = ['SNAKE', 'KEBAB', 'CAMEL', 'PASCAL', 'UPPER'];
+  const errors = [];
+
+  if (!text) {
+    errors.push({
+      message:
+        'Text to convert is required. Correct request is: '
+        + '"/<TEXT_TO_CONVERT>?toCase=<CASE_NAME>".',
+    });
+  }
+
+  if (!caseName) {
+    errors.push({
+      message:
+        '"toCase" query param is required. Correct request is: '
+        + '"/<TEXT_TO_CONVERT>?toCase=<CASE_NAME>".',
+    });
+  }
+
+  if (caseName && !caseNames.includes(caseName)) {
+    errors.push({
+      message:
+        'This case is not supported. Available cases: '
+        + 'SNAKE, KEBAB, CAMEL, PASCAL, UPPER.',
+    });
+  }
+
+  return errors;
+};
+
+module.exports = {
+  handleErrors,
+};
