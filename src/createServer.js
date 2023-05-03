de3a646240e4e2d508d8af293c4a174437f0e860
Yevhenii Leichenko <eugeniy0536@gmail.com>
2023-05-03 16:35:49 +0300

    node case transformer - solution

--- src/createServer.js
+++ src/createServer.js
@@ -1,3 +1,66 @@
-// Write code here
-// Also, you can create additional files in the src folder
-// and import (require) them here
+const http = require('http');
+const { convertToCase } = require('./convertToCase/convertToCase');
+
+const convertParameters = ['SNAKE', 'KEBAB', 'CAMEL', 'PASCAL', 'UPPER'];
+
+function createServer() {
+  const server = http.createServer((req, res) => {
+    const paramsSplitted = req.url.split('?');
+    const messageToConvert = paramsSplitted[0].slice(1);
+    const params = new URLSearchParams(paramsSplitted[1]);
+    const paramsToConvert = params.get('toCase');
+
+    res.setHeader('Content-Type', 'application/json');
+
+    const errors = [];
+
+    if (!convertParameters.includes(paramsToConvert)
+        && paramsToConvert !== null) {
+      errors.push({
+        // eslint-disable-next-line max-len
+        message: 'This case is not supported. Available cases: SNAKE, KEBAB, CAMEL, PASCAL, UPPER.',
+      });
+    }
+
+    if (!messageToConvert) {
+      errors.push({
+        // eslint-disable-next-line max-len
+        message: 'Text to convert is required. Correct request is: "/<TEXT_TO_CONVERT>?toCase=<CASE_NAME>".',
+      });
+    }
+
+    if (paramsToConvert === null) {
+      errors.push({
+        // eslint-disable-next-line max-len
+        message: '"toCase" query param is required. Correct request is: "/<TEXT_TO_CONVERT>?toCase=<CASE_NAME>".',
+      });
+    }
+
+    if (errors.length) {
+      res.statusCode = 400;
+      res.statusMessage = 'Bad request';
+      res.end(JSON.stringify({ errors }));
+
+      return;
+    }
+
+    const {
+      originalCase,
+      convertedText,
+    } = convertToCase(messageToConvert, paramsToConvert);
+
+    const serverRespone = {
+      targetCase: paramsToConvert,
+      originalText: messageToConvert,
+      originalCase,
+      convertedText,
+    };
+
+    res.statusCode = 200;
+    res.end(JSON.stringify(serverRespone));
+  });
+
+  return server;
+}
+
+module.exports = { createServer };
