459ed9f5c688bf57dbbc27b9aeaf489e35bf275a
Sasha Dranuta <dranuta97@gmail.com>
2023-05-03 18:41:55 +0300

    Solution

---
+++ src/checkUrlParams.js
@@ -0,0 +1,28 @@
+const cases = ['SNAKE', 'KEBAB', 'CAMEL', 'PASCAL', 'UPPER'];
+
+const checkUrlParams = (text, toCase) => {
+  const errors = [];
+  const example = '"/<TEXT_TO_CONVERT>?toCase=<CASE_NAME>"';
+
+  if (!text) {
+    errors.push({
+      message: `Text to convert is required. Correct request is: ${example}.`,
+    });
+  }
+
+  if (!toCase) {
+    errors.push({
+      message: `"toCase" query param is required. Correct request is: ${example}.`,
+    });
+  }
+
+  if (!cases.includes(toCase) && toCase) {
+    errors.push({
+      message: `This case is not supported. Available cases: ${cases.join(', ')}.`,
+    });
+  }
+
+  return errors;
+};
+
+module.exports = { checkUrlParams };
