81df6c399211eceee7ca840afe142715edfb22a2
Misha Ivchenko <ivchenkomykhail@meta.ua>
2023-05-04 10:16:14 +0300

    Solution

--- src/createServer.js
+++ src/createServer.js
@@ -1,3 +1,29 @@
-// Write code here
-// Also, you can create additional files in the src folder
-// and import (require) them here
+const http = require("http");
+const { parseUrl } = require("./parseUrl/parseUrl");
+
+const createServer = () =>
+  http.createServer((req, res) => {
+    res.setHeader("Content-Type", "application/json");
+
+    const parseResult = parseUrl(req.url);
+
+    let data;
+
+    if (parseResult.isError) {
+      data = JSON.stringify(parseResult.response);
+      res.statusCode = 400;
+    } else {
+      data = JSON.stringify({
+        originalCase: parseResult.originalCase,
+        targetCase: parseResult.targetCase,
+        originalText: parseResult.originalText,
+        convertedText: parseResult.convertedText,
+      });
+
+      res.statusCode = 200;
+    }
+
+    res.end(data);
+  });
+
+module.exports = { createServer };
