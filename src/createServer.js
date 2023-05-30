196d54fdc20b1884e2bc1dec166c116b0ab931d0
Oleh Kolodii <bigknife4@gmail.com>
2023-05-30 20:31:37 +0300

    solution

--- src/createServer.js
+++ src/createServer.js
@@ -1,3 +1,50 @@
-// Write code here
-// Also, you can create additional files in the src folder
-// and import (require) them here
+const http = require('http');
+const { convertToCase } = require('./convertToCase/convertToCase');
+const errorMessages = require('./errorMessages');
+
+function createServer() {
+  const server = http.createServer((req, res) => {
+    const [path, queryString] = req.url.split('?');
+    const text = path.slice(1);
+    const params = new URLSearchParams(queryString);
+    const targetCase = params.get('toCase');
+    const responseErrors = {
+      errors: [],
+    };
+
+    res.setHeader('Content-Type', 'application/json');
+
+    if (!text) {
+      responseErrors.errors.push({ message: errorMessages.missingText });
+    }
+
+    if (!targetCase) {
+      responseErrors.errors.push({ message: errorMessages.missingTargetCase });
+    }
+
+    const response = {
+      originalText: text,
+      targetCase,
+    };
+
+    try {
+      Object.assign(response, convertToCase(text, targetCase));
+    } catch {
+      if (targetCase) {
+        responseErrors.errors.push({ message: errorMessages.wrongTargetCase });
+      }
+    }
+
+    if (!responseErrors.errors.length) {
+      res.end(JSON.stringify(response));
+    } else {
+      res.statusCode = 400;
+      res.statusMessage = 'Bad request';
+      res.end(JSON.stringify(responseErrors));
+    }
+  });
+
+  return server;
+}
+
+module.exports = { createServer };
---
+++ src/errorMessages.js
@@ -0,0 +1,6 @@
+/* eslint-disable max-len */
+module.exports = {
+  missingText: 'Text to convert is required. Correct request is: "/<TEXT_TO_CONVERT>?toCase=<CASE_NAME>".',
+  missingTargetCase: '"toCase" query param is required. Correct request is: "/<TEXT_TO_CONVERT>?toCase=<CASE_NAME>".',
+  wrongTargetCase: 'This case is not supported. Available cases: SNAKE, KEBAB, CAMEL, PASCAL, UPPER.',
+};
