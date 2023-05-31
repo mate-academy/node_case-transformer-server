c48eebc25d5c8434c951d27af202a3ea7ea19c1d
Mykyta Holtsov <nikitaholtsov@gmail.com>
2023-05-31 17:01:51 +0300

    small fixes, move object errorMessages to new file

--- src/createServer.js
+++ src/createServer.js
@@ -1,52 +1,40 @@
-/* eslint-disable max-len */
 const http = require('http');
 const { convertToCase } = require('./convertToCase');
-const errorMessages = {
-  textError: 'Text to convert is required. Correct request is: "/<TEXT_TO_CONVERT>?toCase=<CASE_NAME>".',
-  caseError: '"toCase" query param is required. Correct request is: "/<TEXT_TO_CONVERT>?toCase=<CASE_NAME>".',
-  caseInvalid: 'This case is not supported. Available cases: SNAKE, KEBAB, CAMEL, PASCAL, UPPER.',
-};
+const { errorMessages } = require('./errorMessages');
 
 function createServer() {
   const server = http.createServer((req, res) => {
     const normalizedURL = new URL(req.url, `http://${req.headers.host}`);
     const text = normalizedURL.pathname.slice(1);
     const toCase = normalizedURL.searchParams.get('toCase');
-    const responseErrors = {
-      errors: [],
-    };
 
     res.setHeader('Content-Type', 'application/json');
 
-    if (!text) {
-      responseErrors.errors.push({ message: errorMessages.textError });
-    }
+    const responseError = findError(text, toCase);
 
-    if (!toCase) {
-      responseErrors.errors.push({ message: errorMessages.caseError });
-    }
-
-    const respond = {
+    const response = {
       originalText: text,
       targetCase: toCase,
     };
 
     try {
-      Object.assign(respond, convertToCase(text, toCase));
+      Object.assign(response, convertToCase(text, toCase));
     } catch {
       if (toCase) {
-        responseErrors.errors.push({ message: errorMessages.caseInvalid });
+        responseError.errors.push({ message: errorMessages.caseInvalid });
       }
     }
 
-    if (!responseErrors.errors.length) {
+    if (!responseError.errors.length) {
       res.statusCode = 200;
-      res.end(JSON.stringify(respond));
-    } else {
-      res.statusCode = 400;
-      res.statusMessage = 'Bad request';
-      res.end(JSON.stringify(responseErrors));
+      res.end(JSON.stringify(response));
+
+      return;
     }
+
+    res.statusCode = 400;
+    res.statusMessage = 'Bad request';
+    res.end(JSON.stringify(responseError));
   });
 
   return server;
@@ -55,3 +43,19 @@ function createServer() {
 module.exports = {
   createServer,
 };
+
+function findError(text, toCase) {
+  const responseError = {
+    errors: [],
+  };
+
+  if (!text) {
+    responseError.errors.push({ message: errorMessages.textError });
+  }
+
+  if (!toCase) {
+    responseError.errors.push({ message: errorMessages.caseError });
+  }
+
+  return responseError;
+}
