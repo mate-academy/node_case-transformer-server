bbf448b855d4a91c46bb8a6a16298861c1ad7702
BondarenkoDmitriy <bondarenko.dima.kiev@gmail.com>
2023-05-31 15:30:41 +0300

    fixed all comments

--- src/createServer.js
+++ src/createServer.js
@@ -1,13 +1,21 @@
+const paramsCheck = require('./paramsCheck');
+const sendError = require('./sendError');
 const http = require('http');
 const { convertToCase } = require('./convertToCase');
+
+const extractParamsFromRequest = (request) => {
+  const { pathname, searchParams } = new URL(request.url, `http://${request.headers.host}`);
+  const text = pathname.slice(1);
+  const toCase = searchParams.get('toCase');
+  
+  return { text, toCase };
+};
 
 const createServer = () => {
   const server = http.createServer((request, response) => {
     response.setHeader('Content-Type', 'application/json');
 
-    const { pathname, searchParams } = new URL(request.url, `http://${request.headers.host}`);
-    const text = pathname.slice(1);
-    const toCase = searchParams.get('toCase');
+    const { text, toCase } = extractParamsFromRequest(request);
 
     const errorMessages = paramsCheck(text, toCase);
 
@@ -34,42 +42,6 @@ const createServer = () => {
   return server;
 };
 
-const paramsCheck = (text, toCase) => {
-  const emptyTextMessage = 'Text to convert is required.'
-    + ' Correct request is: "/<TEXT_TO_CONVERT>?toCase=<CASE_NAME>".';
-  const emptyCaseMessage = '"toCase" query param is required.'
-    + ' Correct request is: "/<TEXT_TO_CONVERT>?toCase=<CASE_NAME>".';
-  const invalidCaseMessage = 'This case is not supported.'
-    + ' Available cases: SNAKE, KEBAB, CAMEL, PASCAL, UPPER.';
-
-  const errorMessages = [];
-  const supportedCases = ['SNAKE', 'KEBAB', 'CAMEL', 'PASCAL', 'UPPER'];
-
-  if (!toCase) {
-    errorMessages.push(emptyCaseMessage);
-  }
-
-  if (!text) {
-    errorMessages.push(emptyTextMessage);
-  }
-
-  if (!!toCase && !supportedCases.includes(toCase)) {
-    errorMessages.push(invalidCaseMessage);
-  }
-
-  return errorMessages;
+module.exports = {
+  createServer
 };
-
-const sendError = (response, statusCode, errorMessages) => {
-  const errors = [];
-
-  errorMessages.forEach(error => {
-    errors.push({ message: error });
-  });
-  response.statusCode = statusCode;
-  response.end(JSON.stringify({ errors }));
-};
-
-createServer();
-
-module.exports.createServer = createServer;
