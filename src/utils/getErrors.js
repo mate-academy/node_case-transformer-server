ca26e85885ec24cbaf4ad4bffacec34becc084d3
VladyslavaT <v.titarenko@student.csn.khai.edu>
2023-05-31 20:45:54 +0300

    made a separate array for the errorsMessage

--- src/utils/getErrors.js
+++ src/utils/getErrors.js
@@ -1,25 +1,32 @@
 const cases = ['SNAKE', 'KEBAB', 'CAMEL', 'PASCAL', 'UPPER'];
+const regs = '"/<TEXT_TO_CONVERT>?toCase=<CASE_NAME>"';
+const errorMessages = [
+  {
+    message: `Text to convert is required. Correct request is: ${regs}.`,
+  },
+  {
+    message: `"toCase" query param is required. Correct request is: ${regs}.`,
+  },
+  {
+    message: `This case is not supported. Available cases: ${cases.join(
+      ', ',
+    )}.`,
+  },
+];
 
 const getErrors = (textToFormat, toCase) => {
   const errors = [];
-  const regs = '"/<TEXT_TO_CONVERT>?toCase=<CASE_NAME>"';
 
   if (!textToFormat) {
-    errors.push({
-      message: `Text to convert is required. Correct request is: ${regs}.`,
-    });
+    errors.push(errorMessages[0]);
   }
 
   if (!toCase) {
-    errors.push({
-      message: `"toCase" query param is required. Correct request is: ${regs}.`,
-    });
+    errors.push(errorMessages[1]);
   }
 
   if (toCase && !cases.includes(toCase)) {
-    errors.push({
-      message: `This case is not supported. Available cases: ${cases.join(', ')}.`,
-    });
+    errors.push(errorMessages[2]);
   }
 
   return errors;
