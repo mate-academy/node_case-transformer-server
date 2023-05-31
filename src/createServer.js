1c795b33624fc4b10927be439a36c98af48adebb
VladyslavaT <v.titarenko@student.csn.khai.edu>
2023-05-31 22:51:16 +0300

    added sendRequest function

--- src/createServer.js
+++ src/createServer.js
@@ -2,39 +2,28 @@ const http = require('http');
 const { normalizeUrl } = require('./utils/normalizeUrl');
 const { getErrors } = require('./utils/getErrors');
 const { convertToCase } = require('./convertToCase/convertToCase');
+const { sendRequest } = require('./utils/sendRequest');
 
 function createServer() {
   const server = http.createServer((req, res) => {
     const [textToFormat, caseToFormat] = normalizeUrl(req);
     const errors = getErrors(textToFormat, caseToFormat);
 
-    res.setHeader('Content-Type', 'application/json');
-
     if (errors.length > 0) {
-      res.statusCode = 400;
-
-      return res.end(
-        JSON.stringify({
-          errors,
-        }),
+      sendRequest(res, 'Bad request', 400, { errors });
+    } else {
+      const { originalCase, convertedText } = convertToCase(
+        textToFormat,
+        caseToFormat,
       );
-    }
 
-    const { originalCase, convertedText } = convertToCase(
-      textToFormat,
-      caseToFormat,
-    );
-
-    res.statusCode = 200;
-
-    res.end(
-      JSON.stringify({
+      sendRequest(res, 'OK', 200, {
         originalCase,
         convertedText,
         targetCase: caseToFormat,
         originalText: textToFormat,
-      }),
-    );
+      });
+    }
   });
 
   return server;
