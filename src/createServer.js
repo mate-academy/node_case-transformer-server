d6cd9391f392cca1c54af5f5a3e5de4650275dea
Vyacheslav <haunt.beta@gmail.com>
2023-06-01 10:47:06 +0300

    case transforming solution

--- src/createServer.js
+++ src/createServer.js
@@ -1,3 +1,38 @@
-// Write code here
-// Also, you can create additional files in the src folder
-// and import (require) them here
+const http = require('http');
+const { getData } = require('./getData');
+const { validateData } = require('./validateData');
+const { convertToCase } = require('./convertToCase/convertToCase');
+
+function createServer() {
+  const server = http.createServer((request, response) => {
+    const [textToConvert, caseName] = getData(request);
+    const errors = validateData(textToConvert, caseName);
+
+    response.setHeader('Content-Type', 'application/json');
+
+    if (errors.length) {
+      response.statusCode = 400;
+      response.statusMessage = 'Something went wrong.';
+
+      return response.end(JSON.stringify({ errors }));
+    }
+
+    const {
+      convertedText,
+      originalCase,
+    } = convertToCase(textToConvert, caseName);
+
+    return response.end(JSON.stringify({
+      originalText: textToConvert,
+      convertedText,
+      originalCase,
+      targetCase: caseName,
+    }));
+  });
+
+  return server;
+}
+
+module.exports = {
+  createServer,
+};
---
+++ src/validateData.js
@@ -0,0 +1,29 @@
+/* eslint-disable max-len */
+function validateData(text, caseName) {
+  const validCases = ['SNAKE', 'KEBAB', 'CAMEL', 'PASCAL', 'UPPER'];
+  const errors = [];
+
+  if (!text) {
+    errors.push({
+      message: 'Text to convert is required. Correct request is: "/<TEXT_TO_CONVERT>?toCase=<CASE_NAME>".',
+    });
+  }
+
+  if (!caseName) {
+    errors.push({
+      message: '"toCase" query param is required. Correct request is: "/<TEXT_TO_CONVERT>?toCase=<CASE_NAME>".',
+    });
+  }
+
+  if (caseName && !validCases.includes(caseName)) {
+    errors.push({
+      message: 'This case is not supported. Available cases: SNAKE, KEBAB, CAMEL, PASCAL, UPPER.',
+    });
+  }
+
+  return errors;
+}
+
+module.exports = {
+  validateData,
+};
