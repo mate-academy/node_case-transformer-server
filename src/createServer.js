981cc39e55dd859844178a09808fabc3becde8ed
glots-a <glots@ukr.net>
2023-06-01 13:25:41 +0300

    second commit

--- src/createServer.js
+++ src/createServer.js
@@ -2,26 +2,14 @@
 
 const http = require('http');
 const { getParamsOfUrl } = require('./getParamsOfUrl');
-
+const { urlValidation } = require('./urlValidation');
 const { convertToCase } = require('./convertToCase/convertToCase');
 
 function createServer() {
   const server = http.createServer((req, res) => {
     const [text, toCase] = getParamsOfUrl(req);
 
-    const errors = [];
-
-    /* eslint-disable */
-    if (!text) {
-      errors.push({ message: 'Text to convert is required. Correct request is: "/<TEXT_TO_CONVERT>?toCase=<CASE_NAME>".' });
-    }
-
-    if (!toCase) {
-      errors.push({ message: '"toCase" query param is required. Correct request is: "/<TEXT_TO_CONVERT>?toCase=<CASE_NAME>".' });
-    } else if (!['SNAKE', 'KEBAB', 'CAMEL', 'PASCAL', 'UPPER'].includes(toCase)) {
-      errors.push({ message: `This case is not supported. Available cases: SNAKE, KEBAB, CAMEL, PASCAL, UPPER.` });
-    }
-    /* eslint-enable */
+    const errors = urlValidation(text, toCase);
 
     if (errors.length > 0) {
       res.writeHead(400, { 'Content-Type': 'application/json' });
@@ -29,8 +17,7 @@ function createServer() {
       return res.end(JSON.stringify({ errors }));
     }
 
-    res.setHeader('Content-Type', 'application/json');
-    res.statusCode = 200;
+    res.writeHead(200, { 'Content-Type': 'application/json' });
 
     const result = convertToCase(text, toCase);
 
