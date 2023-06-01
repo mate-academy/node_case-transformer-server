3e8a5781920726062d0825d870f5806f0398ee26
VladyslavaT <v.titarenko@student.csn.khai.edu>
2023-06-01 14:38:06 +0300

    changed naming of errorMessage

--- src/utils/getErrors.js
+++ src/utils/getErrors.js
@@ -1,24 +1,24 @@
 const cases = ['SNAKE', 'KEBAB', 'CAMEL', 'PASCAL', 'UPPER'];
 const regs = '"/<TEXT_TO_CONVERT>?toCase=<CASE_NAME>"';
 const errorMessages = {
-  Error: `Text to convert is required. Correct request is: ${regs}.`,
-  Error2: `"toCase" query param is required. Correct request is: ${regs}.`,
-  Error3: `This case is not supported. Available cases: ${cases.join(', ')}.`,
+  TextRequired: `Text to convert is required. Correct request is: ${regs}.`,
+  CaseRequired: `"toCase" query param is required. Correct request is: ${regs}.`,
+  CaseSupported: `This case is not supported. Available cases: ${cases.join(', ')}.`,
 };
 
 const getErrors = (textToFormat, toCase) => {
   const errors = [];
 
   if (!textToFormat) {
-    errors.push({ message: errorMessages.Error });
+    errors.push({ message: errorMessages.TextRequired });
   }
 
   if (!toCase) {
-    errors.push({ message: errorMessages.Error2 });
+    errors.push({ message: errorMessages.CaseRequired });
   }
 
   if (toCase && !cases.includes(toCase)) {
-    errors.push({ message: errorMessages.Error3 });
+    errors.push({ message: errorMessages.CaseSupported });
   }
 
   return errors;
