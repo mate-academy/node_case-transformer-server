beaa950f6565fb06c018cb13f4cecb49c7312dcc
Maksym Didyk <didyk.maksym@gmail.com>
2023-06-01 11:55:50 +0300

    Solution v.1

--- src/createServer.js
+++ src/createServer.js
@@ -1,3 +1,29 @@
-// Write code here
-// Also, you can create additional files in the src folder
-// and import (require) them here
+const http = require('http');
+const { normalizeUrl } = require('./normalizeUrl');
+const { checkErrors } = require('./checkErrors');
+const { sendErrors } = require('./sendErrors');
+const { createResponseBody } = require('./createResponseBody');
+
+const createServer = () => {
+  const server = http.createServer((req, res) => {
+    const { text, toCase } = normalizeUrl(req);
+    const errors = checkErrors(text, toCase);
+
+    if (!!errors.length) {
+      return sendErrors(res, 400, errors);
+    }
+
+    const responseBody = createResponseBody(text, toCase);
+
+    res.setHeader('Content-Type', 'application/json');
+    res.statusCode = 200;
+    res.end(JSON.stringify(responseBody));
+  });
+
+  return server;
+}
+
+
+module.exports = {
+  createServer,
+};
