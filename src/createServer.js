fcf80ac62289f8f7d541f44c123d863632a4ae4c
Nazarii Chepil <nazzikk1998@gmail.com>
2023-06-02 12:48:36 +0300

    solution

--- src/createServer.js
+++ src/createServer.js
@@ -1,3 +1,36 @@
-// Write code here
-// Also, you can create additional files in the src folder
-// and import (require) them here
+const http = require('http');
+
+const { convertToCase } = require('./convertToCase');
+const { parseURL } = require('./parseURL');
+const { validateData } = require('./validateData');
+
+const createServer = () => {
+  const server = http.createServer((req, res) => {
+    const { stringCase, queryString } = parseURL(req.url, req.headers.host);
+    const validation = validateData(queryString, stringCase);
+
+    res.writeHead(200, { 'Content-Type': 'application/json' });
+
+    if (!validation.errors.length) {
+      const result = {
+        ...convertToCase(queryString, stringCase),
+        originalText: queryString,
+        targetCase: stringCase,
+      };
+
+      res.statusCode = 200;
+      res.end(JSON.stringify(result));
+
+      return;
+    }
+
+    res.statusCode = 400;
+    res.end(JSON.stringify(validation));
+  })
+
+  return server;
+}
+
+createServer();
+
+module.exports = { createServer };
