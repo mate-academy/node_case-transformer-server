f0168aacf1703e0716126da4f8e0cb72d07edd82
kukla1989 <kukla.1989@ukr.net>
2023-06-04 21:08:29 +0300

    improve code style

--- src/createServer.js
+++ src/createServer.js
@@ -1,41 +1,62 @@
 const http = require('http');
 const { convertToCase } = require('./convertToCase');
 
-const createServer = () => (http.createServer((req, res) => {
-  res.setHeader('Content-Type', 'application/json');
-
-  const normalizedUrl = new URL(req.url, `http://${req.headers.host}`);
+const getDataFromUrl = (url, host) => {
+  const normalizedUrl = new URL(url, `http://${host}`);
   const toCase = Object.fromEntries(normalizedUrl.searchParams.entries())
     .toCase;
   const path = normalizedUrl.pathname.slice(1);
+
+  return { toCase, path };
+};
+
+const errorTypes = {
+  missedText: 'Text to convert is required. Correct request'
+  + ' is: "/<TEXT_TO_CONVERT>?toCase=<CASE_NAME>".',
+  wrongCase: 'This case is not supported.'
+  + ' Available cases: SNAKE, KEBAB, CAMEL, PASCAL, UPPER.',
+  missingToCase: '"toCase" query param is required.'
+  + ' Correct request is: "/<TEXT_TO_CONVERT>?toCase=<CASE_NAME>".',
+};
+
+const getErrors = (path, toCase) => {
   const errors = [];
 
   if (!path) {
-    addError(errors, 'Text to convert is required. Correct request'
-      + ' is: "/<TEXT_TO_CONVERT>?toCase=<CASE_NAME>".');
+    addError(errors, errorTypes.missedText);
   }
 
   if (toCase
       && !['SNAKE', 'KEBAB', 'UPPER', 'CAMEL', 'PASCAL'].includes(toCase)) {
-    addError(errors, 'This case is not supported.'
-      + ' Available cases: SNAKE, KEBAB, CAMEL, PASCAL, UPPER.');
+    addError(errors, errorTypes.wrongCase);
   }
 
   if (!toCase) {
-    addError(errors, '"toCase" query param is required.'
-      + ' Correct request is: "/<TEXT_TO_CONVERT>?toCase=<CASE_NAME>".');
+    addError(errors, errorTypes.missingToCase);
   }
 
+  return errors;
+};
+
+const setStatus = (res, statusCode, statusMessage) => {
+  res.statusCode = statusCode;
+  res.statusMessage = statusMessage;
+};
+
+const createServer = () => (http.createServer((req, res) => {
+  res.setHeader('Content-Type', 'application/json');
+
+  const { toCase, path } = getDataFromUrl(req.url, req.headers.host);
+  const errors = getErrors(path, toCase);
+
   if (errors.length) {
-    res.statusCode = 400;
-    res.statusMessage = 'Bad request';
+    setStatus(res, 400, 'Bad request');
     res.end(JSON.stringify({ errors }));
 
     return;
   }
 
-  res.statusCode = 200;
-  res.statusMessage = 'OK';
+  setStatus(res, 200, 'OK');
 
   const { originalCase, convertedText } = convertToCase(path
     , toCase);
