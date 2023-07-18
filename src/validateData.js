5492d7a9293b3b8d2beac26e499a71cdc1516b52
Yuliya1907 <violet1907@ukr.net>
2023-07-18 22:02:02 +0300

    solution

---
+++ src/validateData.js
@@ -0,0 +1,27 @@
+/* eslint-disable max-len */
+function validateData(text, toCase) {
+  const validCases = ['SNAKE', 'KEBAB', 'CAMEL', 'PASCAL', 'UPPER'];
+  const errorMessages = {
+    emptyTextMessage: 'Text to convert is required. Correct request is: "/<TEXT_TO_CONVERT>?toCase=<CASE_NAME>".',
+    emptyCaseMessage: '"toCase" query param is required. Correct request is: "/<TEXT_TO_CONVERT>?toCase=<CASE_NAME>".',
+    invalidCaseMassage: `This case is not supported. Available cases: ${validCases.join(', ')}.`,
+  };
+
+  const errors = [];
+
+  if (!text) {
+    errors.push({ message: errorMessages.emptyTextMessage });
+  }
+
+  if (!toCase) {
+    errors.push({ message: errorMessages.emptyCaseMessage });
+  }
+
+  if (toCase && !validCases.includes(toCase)) {
+    errors.push({ message: errorMessages.invalidCaseMassage });
+  }
+
+  return errors;
+}
+
+module.exports = { validateData };
