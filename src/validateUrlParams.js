f2b86fe5d3f7894377136b62f9e85639b4a8db77
Denys Didenko <ddidenko441@gmail.com>
2023-07-18 13:27:16 +0300

    add node_case-transformer-server solution

---
+++ src/validateUrlParams.js
@@ -0,0 +1,28 @@
+/* eslint-disable max-len */
+const validCases = ['SNAKE', 'KEBAB', 'CAMEL', 'PASCAL', 'UPPER'];
+
+const isValidUrlParams = (text, toCase) => {
+  const errors = [];
+
+  if (!text) {
+    errors.push({
+      message: 'Text to convert is required. Correct request is: "/<TEXT_TO_CONVERT>?toCase=<CASE_NAME>".',
+    });
+  }
+
+  if (!toCase) {
+    errors.push({
+      message: '"toCase" query param is required. Correct request is: "/<TEXT_TO_CONVERT>?toCase=<CASE_NAME>".',
+    });
+  }
+
+  if (toCase && !validCases.includes(toCase)) {
+    errors.push({
+      message: `This case is not supported. Available cases: ${validCases.join(', ')}.`,
+    });
+  }
+
+  return errors;
+};
+
+module.exports = { isValidUrlParams };
