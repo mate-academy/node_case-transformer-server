51477ef341608a0e83cb70a79d642d4db6dde020
Vania Yarovyi <yarovyi@gmail.com>
2023-07-20 15:44:59 +0300

    had put the variable acceptableCases outside the function; created errorMessages object with error messages for each error case

--- src/helper.js
+++ src/helper.js
@@ -1,25 +1,31 @@
+const acceptableCases = ['SNAKE', 'KEBAB', 'CAMEL', 'PASCAL', 'UPPER'];
+const errorMessages = {
+  noText: 'Text to convert is required. '
+  + 'Correct request is: "/<TEXT_TO_CONVERT>?toCase=<CASE_NAME>".',
+  noCase: '"toCase" query param is required. '
+  + 'Correct request is: "/<TEXT_TO_CONVERT>?toCase=<CASE_NAME>".',
+  wrongCase: 'This case is not supported. '
+  + 'Available cases: SNAKE, KEBAB, CAMEL, PASCAL, UPPER.',
+}
+
 const getErrors = (givenText, givenCase) => {
-  const acceptableCases = ['SNAKE', 'KEBAB', 'CAMEL', 'PASCAL', 'UPPER'];
   const errors = [];
 
   if (!givenText) {
     errors.push({
-      message: 'Text to convert is required. '
-        + 'Correct request is: "/<TEXT_TO_CONVERT>?toCase=<CASE_NAME>".',
+      message: errorMessages.noText,
     });
   }
 
   if (!givenCase) {
     errors.push({
-      message: '"toCase" query param is required. '
-        + 'Correct request is: "/<TEXT_TO_CONVERT>?toCase=<CASE_NAME>".',
+      message: errorMessages.noCase,
     });
   }
 
   if (givenCase && !acceptableCases.includes(givenCase)) {
     errors.push({
-      message: 'This case is not supported. '
-      + 'Available cases: SNAKE, KEBAB, CAMEL, PASCAL, UPPER.',
+      message: errorMessages.wrongCase,
     });
   }
 
