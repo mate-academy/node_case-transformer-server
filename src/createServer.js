cfd90be0e2fd8152a64064ac455413b81fdeb559
Igor Shevtsov <contact.shevtsov@gmail.com>
2023-07-20 12:19:54 +0300

    corrected naming, refactored error detecting

--- src/createServer.js
+++ src/createServer.js
@@ -1,6 +1,6 @@
 const http = require('http');
-const { convertToCase } = require('./convertToCase')
-const { validateURL } = require('./validateURL')
+const { convertToCase } = require('./convertToCase');
+const { validateURL } = require('./validateURL');
 
 const createServer = () => {
   return http.createServer((req, res) => {
@@ -10,27 +10,35 @@ const createServer = () => {
     const toCase = normalizedURL.searchParams.get('toCase');
     const textToConvert = normalizedURL.pathname.slice(1);
 
-    const errorMessage = validateURL(toCase, textToConvert);
+    const error = validateURL(toCase, textToConvert);
 
-    if (errorMessage.errors.length) {
+    if (error.length) {
       res.statusCode = 400;
       res.statusMessage = 'Bad request';
-      res.end(JSON.stringify(errorMessage));
-    } else {
-      res.statusCode = 200;
+      res.end(JSON.stringify({ errors: error }));
 
-      const transformedText = convertToCase(textToConvert, toCase);
-      const data = {
-        ...transformedText,
-        targetCase: toCase,
-        originalText: textToConvert,
-      };
-
-      res.end(JSON.stringify(data));
+      return;
     }
+
+    res.statusCode = 200;
+    res.statusMessage = 'OK';
+
+    const {
+      originalCase,
+      convertedText,
+    } = convertToCase(textToConvert, toCase);
+
+    const data = {
+      originalCase,
+      targetCase: toCase,
+      originalText: textToConvert,
+      convertedText,
+    };
+
+    res.end(JSON.stringify(data));
   });
-}
+};
 
 module.exports = {
   createServer,
-}
+};
--- src/validateURL.js
+++ src/validateURL.js
@@ -1,31 +1,31 @@
 /* eslint-disable max-len */
 const supportedCases = ['SNAKE', 'KEBAB', 'CAMEL', 'PASCAL', 'UPPER'];
 
-const errors = {
-  noText:
+const errorMessages = {
+  missingText:
     'Text to convert is required. Correct request is: "/<TEXT_TO_CONVERT>?toCase=<CASE_NAME>".',
-  noToCase:
+  missingCase:
     '"toCase" query param is required. Correct request is: "/<TEXT_TO_CONVERT>?toCase=<CASE_NAME>".',
   notSupported:
     `This case is not supported. Available cases: ${supportedCases.join(', ')}.`,
 };
 
 function validateURL(toCase, text) {
-  return {
-    errors: [
-      ...!text
-        ? [{ message: errors.noText }]
-        : [],
+  const errors = [];
 
-      ...!toCase
-        ? [{ message: errors.noToCase }]
-        : [],
+  if (!text) {
+    errors.push({ message: errorMessages.missingText });
+  }
 
-      ...!supportedCases.includes(toCase) && toCase
-        ? [{ message: errors.notSupported }]
-        : [],
-    ],
-  };
+  if (!toCase) {
+    errors.push({ message: errorMessages.missingCase });
+  }
+
+  if (toCase && !supportedCases.includes(toCase.toUpperCase())) {
+    errors.push({ message: errorMessages.notSupported });
+  }
+
+  return errors;
 };
 
 module.exports = {
