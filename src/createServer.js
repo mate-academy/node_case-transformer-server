b4661ad2b3b3785bba084bcc372f3a8602894f9d
Bogdan Parubchenko <bogdanparubchenko1@gmail.com>
2023-08-08 14:00:58 +0300

    Created server

--- src/createServer.js
+++ src/createServer.js
@@ -1,3 +1,46 @@
-// Write code here
-// Also, you can create additional files in the src folder
-// and import (require) them here
+const http = require('http');
+const { procssingError } = require('./processingError');
+const { convertToCase } = require('./convertToCase/convertToCase.js');
+
+const createServer = () => {
+  return http.createServer((req, res) => {
+    res.setHeader('Content-type', 'application/json');
+
+    const url = new URL(req.url, `http://${req.headers.host}`);
+
+    try {
+      const originalText = url.pathname.slice(1);
+      const targetCase = url.searchParams.get('toCase');
+
+      procssingError(originalText, targetCase);
+
+      const { originalCase, convertedText } = convertToCase(
+        originalText,
+        targetCase,
+      );
+
+      res.statusCode = 200;
+
+      res.end(
+        JSON.stringify({
+          originalCase,
+          originalText,
+          targetCase,
+          convertedText,
+        }),
+      );
+    } catch (err) {
+      res.statusCode = 400;
+
+      if (Array.isArray(err)) {
+        res.end(
+          JSON.stringify({
+            errors: err.map((error) => ({ message: error.message })),
+          }),
+        );
+      }
+    }
+  });
+};
+
+module.exports = { createServer };
