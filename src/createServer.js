3dd3ab02577bade39508b7ba0cd424d10b89cbcf
Anastasiia <svintsova.nastya@gmail.com>
2023-08-11 14:12:12 +0300

    feat: implement server

--- src/createServer.js
+++ src/createServer.js
@@ -1,3 +1,43 @@
-// Write code here
-// Also, you can create additional files in the src folder
-// and import (require) them here
+const http = require("http");
+const { convertToCase } = require("./convertToCase/convertToCase.js");
+const { validator } = require("./validator.js");
+
+const createServer = () => {
+  const server = http.createServer((req, res) => {
+    const { pathname, searchParams } = new URL(
+      req.url,
+      `http://${req.headers.host}`
+    );
+
+    const originalText = pathname.slice(1);
+    const targetCase = searchParams.get("toCase");
+    const errors = validator(originalText, targetCase);
+
+    const statusCode = errors.length ? 400 : 200;
+    let response = { errors };
+
+    if (!errors.length) {
+      const { originalCase, convertedText } = convertToCase(
+        originalText,
+        targetCase
+      );
+
+      response = {
+        originalCase,
+        targetCase,
+        originalText,
+        convertedText,
+      };
+    }
+
+    res.writeHead(statusCode, { "Content-Type": "application/json" });
+    res.end(JSON.stringify(response));
+  });
+
+  return server;
+}
+
+
+module.exports = {
+  createServer,
+};
