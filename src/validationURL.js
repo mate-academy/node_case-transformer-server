192fad08c3090e9c2f0a029c92bb6af4c0af1036
pasternakv19@gmail.com <pasternakv19@gmail.com>
2023-08-11 21:07:50 +0300

    add task solution

---
+++ src/validationURL.js
@@ -0,0 +1,34 @@
+const cases = {
+  SNAKE: 'SNAKE',
+  KEBAB: 'KEBAB',
+  CAMEL: 'CAMEL',
+  PASCAL: 'PASCAL',
+  UPPER: 'UPPER',
+};
+
+function validationURL(text, toCase) {
+  const errors = [];
+
+  if (!text) {
+    errors.push({
+      message: 'Text to convert is required. Correct request is:'
+        + '"/<TEXT_TO_CONVERT>?toCase=<CASE_NAME>"',
+    });
+  }
+
+  if (!toCase) {
+    errors.push({
+      message: '"toCase" query param is required. Correct request is:'
+      + '"/<TEXT_TO_CONVERT>?toCase=<CASE_NAME>"',
+    });
+  }
+
+  if (!toCase && !cases[toCase]) {
+    errors.push({
+      message: 'This case is not supported. Available cases:'
+      + 'SNAKE, KEBAB, CAMEL, PASCAL, UPPER.',
+    });
+  }
+};
+
+module.export = { validationURL };
