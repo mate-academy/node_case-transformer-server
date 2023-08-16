da6efe55be7c742009480876e687c7e1e8c86392
Anton Baranskyi <antonbaranskij12@gmail.com>
2023-08-16 16:50:16 +0300

    Fix lint

--- src/createServer.js
+++ src/createServer.js
@@ -1,4 +1,4 @@
-/*eslint-disable*/
+/* eslint-disable max-len */
 
 const http = require("http");
 const { convertToCase } = require("./convertToCase/convertToCase");
@@ -7,11 +7,12 @@ const cases = ["SNAKE", "KEBAB", "CAMEL", "PASCAL", "UPPER"];
 
 const createServer = () => {
   const server = http.createServer((req, resp) => {
-    resp.setHeader("Content-Type", "application/json");
+    resp.setHeader('Content-Type', 'application/json');
+
     const normalizedUrl = new URL(req.url, `http://${req.headers.host}`);
 
     const textToTransform = normalizedUrl.pathname.slice(1);
-    const typeTransform = normalizedUrl.searchParams.get("toCase");
+    const typeTransform = normalizedUrl.searchParams.get('toCase');
 
     const isError =
       !textToTransform || !typeTransform || !cases.includes(typeTransform);
@@ -38,18 +39,18 @@ const createServer = () => {
       if (!cases.includes(typeTransform) && typeTransform) {
         errorMessages.errors.push({
           message:
-            "This case is not supported." +
-            " Available cases: SNAKE, KEBAB, CAMEL, PASCAL, UPPER.",
+            'This case is not supported.' +
+            ' Available cases: SNAKE, KEBAB, CAMEL, PASCAL, UPPER.',
         });
       }
 
       resp.statusCode = 400;
-      resp.statusMessage = "Bad request";
+      resp.statusMessage = 'Bad request';
       resp.end(JSON.stringify(errorMessages));
     } else {
       const { originalCase, convertedText } = convertToCase(
         textToTransform,
-        typeTransform
+        typeTransform,
       );
 
       const responce = {
@@ -58,8 +59,10 @@ const createServer = () => {
         originalText: textToTransform,
         convertedText,
       };
+
       resp.statusCode = 200;
-      resp.statusMessage = "OK";
+
+      resp.statusMessage = 'OK';
       resp.end(JSON.stringify(responce));
     }
   });
--- src/testReq.js
+++
@@ -1,9 +0,0 @@
-/*eslint-disable*/
-const axios = require("axios");
-
-const BASE = "http://localhost:5700";
-const pathName = "/createServer?toCase=SNAKE";
-
-const href = BASE + pathName;
-
-axios.get(href).catch(() => console.log("ERROR"));
