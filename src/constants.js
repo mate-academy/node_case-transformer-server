fe9071d97d89870903cf2a689317678ad91f8659
Oleh Khalanskyi <okhalanskyi@gmail.com>
2023-08-16 14:27:26 +0300

    feat: caseTransformer provided

---
+++ src/constants.js
@@ -0,0 +1,7 @@
+const ERROR_TYPES = {
+  missingText: 'Text to convert is required. Correct request is: "/<TEXT_TO_CONVERT>?toCase=<CASE_NAME>".',
+  missingCase: '"toCase" query param is required. Correct request is: "/<TEXT_TO_CONVERT>?toCase=<CASE_NAME>"',
+  caseIsNotValid: 'This case is not supported. Available cases: SNAKE, KEBAB, CAMEL, PASCAL, UPPER.',
+};
+
+module.exports = { ERROR_TYPES };
--- src/convertToCase/index.js
+++ src/convertToCase/index.js
@@ -2,6 +2,9 @@ const {
   convertToCase,
 } = require('./convertToCase');
 
+const { CASE_TYPES } = require('./case');
+
 module.exports = {
   convertToCase,
+  CASE_TYPES,
 };
