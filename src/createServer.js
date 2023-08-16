6673185aa07db67c1ed9c377fd10dfe90daf201f
Oleh Kandaskalov <olegkandaskalov@gmail.com>
2023-08-16 13:43:34 +0300

    add task solution

--- src/createServer.js
+++ src/createServer.js
@@ -1,3 +1,70 @@
-// Write code here
-// Also, you can create additional files in the src folder
-// and import (require) them here
+const http = require('http');
+const { convertToCase } = require('./convertToCase');
+
+function createServer() {
+  const SUPPORTED_CASES = ['SNAKE', 'KEBAB', 'CAMEL', 'PASCAL', 'UPPER'];
+
+  const server = http.createServer((req, res) => {
+    res.setHeader('Content-Type', 'application/json');
+
+    const [url, params] = req.url.split('?');
+    const quertParams = new URLSearchParams(params);
+    const targetCase = quertParams.get('toCase');
+    const originalText = url.slice(1);
+
+    if (
+      originalText.length === 0
+      || !SUPPORTED_CASES.includes(targetCase)
+      || !targetCase
+    ) {
+      const messages = [];
+
+      if (originalText.length === 0) {
+        messages.push({
+          // eslint-disable-next-line max-len
+          message: 'Text to convert is required. Correct request is: "/<TEXT_TO_CONVERT>?toCase=<CASE_NAME>".',
+        });
+      }
+
+      if (!targetCase) {
+        messages.push({
+          // eslint-disable-next-line max-len
+          message: '"toCase" query param is required. Correct request is: "/<TEXT_TO_CONVERT>?toCase=<CASE_NAME>".',
+        });
+      } else if (!SUPPORTED_CASES.includes(targetCase)) {
+        messages.push({
+          // eslint-disable-next-line max-len
+          message: `This case is not supported. Available cases: ${SUPPORTED_CASES.join(', ')}.`,
+        });
+      }
+
+      res.statusCode = 400;
+      res.statusMessage = 'Bad request';
+
+      res.end(JSON.stringify({
+        errors: messages,
+      }));
+
+      return;
+    }
+
+    const {
+      originalCase,
+      convertedText,
+    } = convertToCase(originalText, targetCase);
+
+    res.statusCode = 200;
+    res.statusMessage = 'OK';
+
+    res.end(JSON.stringify({
+      originalCase,
+      targetCase,
+      originalText,
+      convertedText,
+    }));
+  });
+
+  return server;
+}
+
+module.exports = { createServer };
