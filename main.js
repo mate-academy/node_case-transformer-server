d8a64f3f27038ee753938863ad30436ca4c774a6
Boiko Vlad <bojko.vladislav16@gmail.com>
2023-08-20 21:12:58 +0300

    The task has been completed

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const { createServer } = require('./src/createServer');
 
 createServer()
-  .listen(5700, () => {
+  .listen(3000, () => {
     // eslint-disable-next-line no-console
     console.log('Server started! 🚀');
   });
--- src/createServer.js
+++ src/createServer.js
@@ -1,3 +1,54 @@
-// Write code here
-// Also, you can create additional files in the src folder
-// and import (require) them here
+const http = require('http');
+const { URL } = require('url')
+const {convertToCase } = require('./convertToCase/convertToCase');
+
+const availableCases = ['SNAKE', 'CAMEL', 'PASCAL', 'KEBAB', 'UPPER'];
+
+function createServer() {
+  return http.createServer((req, res) => {
+    res.setHeader('Content-type', 'text/plain');
+
+    const newUrl = new URL(req.url, `http://${req.headers.host}`);
+
+    const targetCase = newUrl.searchParams.get('toCase');
+    const textToConvert = newUrl.pathname.slice(1);
+
+    if (!textToConvert) {
+      res.statusCode = 400;
+      res.end('Text to convert is required. Correct request is: "/<TEXT_TO_CONVERT>?toCase=<CASE_NAME>".');
+
+      return;
+    }
+
+    if (!targetCase) {
+      res.statusCode = 400;
+      res.end('"toCase" query param is required. Correct request is: "/<TEXT_TO_CONVERT>?toCase=<CASE_NAME>".');
+
+      return;
+    }
+
+    if (availableCases.every(availableCase => availableCase !== targetCase)) {
+      res.statusCode = 400;
+      res.end(`This case is not supported. Available cases: ${availableCases.join(', ')}`);
+
+      return;
+    }
+
+    try {
+      const { originalCase, convertedText } = convertToCase(textToConvert, targetCase);
+
+      res.end(JSON.stringify({
+        "originalCase": originalCase,
+        "targetCase": targetCase,
+        "originalText": textToConvert,
+        "convertedText": convertedText,
+      }));
+    } catch (error) {
+      console.error('Something bad happened', error);
+      res.statusCode = 500;
+      res.end('Internal Server Error');
+    }
+  });
+}
+
+module.exports = { createServer };
