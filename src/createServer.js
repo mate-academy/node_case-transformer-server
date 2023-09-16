6938735df60f683010b136191157fe340e2a0fc5
loralevitska <larakad94@gmail.com>
2023-09-16 21:28:39 +0300

    feat: case-transformer-server implemented

--- src/createServer.js
+++ src/createServer.js
@@ -1,3 +1,36 @@
-// Write code here
-// Also, you can create additional files in the src folder
-// and import (require) them here
+const http = require('http');
+
+const { detectCase } = require('./convertToCase/detectCase');
+const { toWords } = require('./convertToCase/toWords');
+const { wordsToCase } = require('./convertToCase/wordsToCase');
+const { validateUrl, convertUrlToParams } = require('./helper');
+
+module.exports = {
+  createServer: () => {
+    return http.createServer((req, res) => {
+      res.setHeader('Content-Type', 'application/json');
+      res.statusCode = 200;
+
+      const { textToConvert, toCase } = convertUrlToParams(req.url);
+      const validationErrors = validateUrl(textToConvert, toCase);
+
+      if (validationErrors.length > 0) {
+        res.statusCode = 400;
+        res.status = 'Bad request';
+
+        res.end(JSON.stringify({ errors: validationErrors }));
+      } else {
+        const originalCase = detectCase(textToConvert);
+        const wordsArray = toWords(textToConvert, originalCase);
+        const convertedText = wordsToCase(wordsArray, toCase);
+
+        res.end(JSON.stringify({
+          originalCase,
+          targetCase: toCase,
+          originalText: textToConvert,
+          convertedText,
+        }));
+      }
+    });
+  },
+};
