428ac28ebd5b06eae40a3debffeca40f8a0d6a92
Izzet Yusufov <17izzet@gmail.com12345Hello>
2023-10-08 23:37:41 +0200

    solution

--- src/createServer.js
+++ src/createServer.js
@@ -1,3 +1,28 @@
-// Write code here
-// Also, you can create additional files in the src folder
-// and import (require) them here
+const http = require('http');
+const PORT = process.env.PORT || 3000;
+
+function createServer() {
+  return http.createServer((req, res) => {
+    res.setHeader('Content-Type', 'application/json');
+
+    const parsedUrl = new URL(req.url, `http://localhost:${PORT}`);
+    const parts = parsedUrl.pathname.split('/').filter(Boolean);
+    const query = Object.fromEntries(parsedUrl.searchParams.entries());
+
+    const response = {
+      parts,
+      query,
+    };
+
+    res.end(JSON.stringify(response));
+  });
+}
+
+const server = createServer();
+
+server.listen(PORT, () => {
+  // eslint-disable-next-line no-console
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
+
+module.exports = { createServer };
