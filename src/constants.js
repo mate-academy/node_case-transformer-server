e29d55e291936d32e9a250c1f230ad93a85bf711
Dmytro Hrynchyi <desevoker@gmail.com>
2023-10-11 01:20:36 +0300

    improve error messages

--- src/constants.js
+++ src/constants.js
@@ -1,11 +1,12 @@
 /* eslint-disable max-len */
 
 const supportedCases = ['SNAKE', 'KEBAB', 'CAMEL', 'PASCAL', 'UPPER'];
+const correctRequest = '"/<TEXT_TO_CONVERT>?toCase=<CASE_NAME>"';
 
 const errorMessages = {
-  noText: 'Text to convert is required. Correct request is: "/<TEXT_TO_CONVERT>?toCase=<CASE_NAME>".',
-  noCase: '"toCase" query param is required. Correct request is: "/<TEXT_TO_CONVERT>?toCase=<CASE_NAME>".',
-  unsupportedCase: 'This case is not supported. Available cases: SNAKE, KEBAB, CAMEL, PASCAL, UPPER.',
+  noText: `Text to convert is required. Correct request is: ${correctRequest}.`,
+  noCase: `"toCase" query param is required. Correct request is: ${correctRequest}.`,
+  unsupportedCase: `This case is not supported. Available cases: ${supportedCases.join(', ')}.`,
 };
 
 module.exports = { supportedCases, errorMessages };
