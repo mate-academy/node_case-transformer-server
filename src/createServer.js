02858469355e7086cfd47757f4d47a86e3d6ce4a
KatOlista^^! <olya.katyushina@gmail.com>
2023-10-11 16:10:39 +0200

    create simple server

--- src/createServer.js
+++ src/createServer.js
@@ -1,3 +1,37 @@
-// Write code here
-// Also, you can create additional files in the src folder
-// and import (require) them here
+const http = require('http');
+const { convertToCase } = require('./convertToCase');
+const { checkValidation } = require('./checkValidation');
+
+const createServer = () => {
+  const server = http.createServer((request, response) => {
+    response.setHeader('Content-type', 'application/json');
+
+    const [originString, queryCase] = request.url.slice(1).split('?');
+
+    const toCase = queryCase?.split('=')[1];
+
+    const errors = checkValidation(originString, toCase);
+
+    if (errors.length) {
+      response.statusCode = 400;
+      response.statusMessage = 'Status: Bad Request';
+      response.end(JSON.stringify({ errors }));
+    } else {
+      response.statusCode = 200;
+      response.statusMessage = 'OK';
+
+      const result = convertToCase(originString, toCase);
+
+      result.targetCase = toCase;
+      result.originalText = originString;
+
+      response.end(JSON.stringify(result));
+    }
+  });
+
+  return server;
+};
+
+module.exports = {
+  createServer,
+};
