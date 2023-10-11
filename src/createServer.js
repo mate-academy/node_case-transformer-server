7eca9ee66de3ef0ef593cbe522da5598e70d432a
Izzet Yusufov <17izzet@gmail.com12345Hello>
2023-10-11 19:01:32 +0200

    solution

--- src/createServer.js
+++ src/createServer.js
@@ -2,41 +2,24 @@
 const http = require('http');
 const url = require('url');
 const { convertToCase } = require('./convertToCase/convertToCase');
-
-const CASES = ['SNAKE', 'KEBAB', 'CAMEL', 'PASCAL', 'UPPER'];
+const { checkCaseAndText } = require('./checkCaseAndText');
 
 function createServer() {
   return http.createServer((req, res) => {
     res.setHeader('Content-type', 'application/json');
 
     const normalizedUrl = new url.URL(req.url, `http://${req.headers.host}`);
-    const errors = {
-      errors: [],
-    };
 
     if (normalizedUrl.pathname !== '/favicon.ico') {
       const originalText = normalizedUrl.pathname.slice(1);
       const targetCase = normalizedUrl.searchParams.get('toCase');
 
-      if (!originalText) {
-        errors.errors.push({ message: 'Text to convert is required. Correct '
-        + 'request is: "/<TEXT_TO_CONVERT>?toCase=<CASE_NAME>".' });
-      }
+      const isErrors = checkCaseAndText(originalText, targetCase);
 
-      if (!targetCase) {
-        errors.errors.push({ message: '"toCase" query param is required. '
-        + 'Correct request is: "/<TEXT_TO_CONVERT>?toCase=<CASE_NAME>".' });
-      }
-
-      if (targetCase && !CASES.includes(targetCase)) {
-        errors.errors.push({ message: 'This case is not supported. Available '
-        + 'cases: SNAKE, KEBAB, CAMEL, PASCAL, UPPER.' });
-      }
-
-      if (errors.errors.length) {
+      if (isErrors.errors.length) {
         res.statusCode = 404;
         res.statusMessage = 'Bad request';
-        res.end(JSON.stringify(errors));
+        res.end(JSON.stringify(isErrors));
 
         return;
       }
