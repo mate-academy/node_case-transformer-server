dbd9ec487494a2b432bf8d2d45983d4c3db2b76f
Izzet Yusufov <17izzet@gmail.com12345Hello>
2023-10-11 18:19:16 +0200

    solution

--- src/createServer.js
+++ src/createServer.js
@@ -1,28 +1,61 @@
+/* eslint-disable no-console */
 const http = require('http');
-const PORT = process.env.PORT || 3000;
+const url = require('url');
+const { convertToCase } = require('./convertToCase/convertToCase');
+
+const CASES = ['SNAKE', 'KEBAB', 'CAMEL', 'PASCAL', 'UPPER'];
 
 function createServer() {
   return http.createServer((req, res) => {
-    res.setHeader('Content-Type', 'application/json');
+    res.setHeader('Content-type', 'application/json');
 
-    const parsedUrl = new URL(req.url, `http://localhost:${PORT}`);
-    const parts = parsedUrl.pathname.split('/').filter(Boolean);
-    const query = Object.fromEntries(parsedUrl.searchParams.entries());
-
-    const response = {
-      parts,
-      query,
+    const normalizedUrl = new url.URL(req.url, `http://${req.headers.host}`);
+    const errors = {
+      errors: [],
     };
 
-    res.end(JSON.stringify(response));
+    if (normalizedUrl.pathname !== '/favicon.ico') {
+      const originalText = normalizedUrl.pathname.slice(1);
+      const targetCase = normalizedUrl.searchParams.get('toCase');
+
+      if (!originalText) {
+        errors.errors.push({ message: 'Text to convert is required. Correct '
+        + 'request is: "/<TEXT_TO_CONVERT>?toCase=<CASE_NAME>".' });
+      }
+
+      if (!targetCase) {
+        errors.errors.push({ message: '"toCase" query param is required. '
+        + 'Correct request is: "/<TEXT_TO_CONVERT>?toCase=<CASE_NAME>".' });
+      }
+
+      if (targetCase && !CASES.includes(targetCase)) {
+        errors.errors.push({ message: 'This case is not supported. Available '
+        + 'cases: SNAKE, KEBAB, CAMEL, PASCAL, UPPER.' });
+      }
+
+      if (errors.errors.length) {
+        res.statusCode = 404;
+        res.statusMessage = 'Bad request';
+        res.end(JSON.stringify(errors));
+
+        return;
+      }
+
+      const { convertedText, originalCase } = convertToCase(
+        originalText, targetCase,
+      );
+
+      res.statusCode = 200;
+      res.statusMessage = 'OK';
+
+      res.end(JSON.stringify({
+        originalCase,
+        targetCase,
+        originalText,
+        convertedText,
+      }));
+    }
   });
 }
-
-const server = createServer();
-
-server.listen(PORT, () => {
-  // eslint-disable-next-line no-console
-  console.log(`Server is running on http://localhost:${PORT}`);
-});
 
 module.exports = { createServer };
