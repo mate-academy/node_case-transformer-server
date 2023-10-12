57fface888f6a2947e6fd09772c0df5918121bdd
Ivan Khutorovyi <ivanhutorovij@gmail.com>
2023-10-12 14:34:08 +0300

    add another file, split logic

--- src/createServer.js
+++ src/createServer.js
@@ -1,54 +1,22 @@
-/* eslint-disable max-len */
 const http = require('http');
 const { convertToCase } = require('./convertToCase');
+const { validateData } = require('./validateData');
 
 function createServer() {
   const server = http.createServer((req, res) => {
     res.setHeader('Content-Type', 'application/json');
 
-    const bodyError = {};
+    const { errors, targetCase, originalText } = validateData(req.url);
 
-    bodyError.errors = [];
-
-    const [originalText, query] = req.url.slice(1).split('?');
-    const params = new URLSearchParams(query);
-    const targetCase = params.get('toCase');
-
-    if (!originalText) {
-      bodyError.errors.push({
-        message: 'Text to convert is required. Correct request is: "/<TEXT_TO_CONVERT>?toCase=<CASE_NAME>".',
-      });
-    }
-
-    if (!targetCase) {
-      bodyError.errors.push({
-        message: '"toCase" query param is required. Correct request is: "/<TEXT_TO_CONVERT>?toCase=<CASE_NAME>".',
-      });
-    }
-
-    if (targetCase) {
-      try {
-        convertToCase(originalText, targetCase);
-      } catch (err) {
-        bodyError.errors.push({
-          message: 'This case is not supported. Available cases: SNAKE, KEBAB, CAMEL, PASCAL, UPPER.',
-        });
-      }
-    }
-
-    if (bodyError.errors.length) {
+    if (errors.length) {
       res.statusCode = 400;
       res.statusMessage = 'Bad request';
-      res.end(JSON.stringify(bodyError));
+      res.end(JSON.stringify({ errors }));
     } else {
-      res.statusCode = 200;
-      res.statusMessage = 'OK';
-
       const {
         originalCase,
         convertedText,
       } = convertToCase(originalText, targetCase);
-
       const response = {
         originalCase,
         targetCase,
@@ -56,6 +24,8 @@ function createServer() {
         convertedText,
       };
 
+      res.statusCode = 200;
+      res.statusMessage = 'OK';
       res.end(JSON.stringify(response));
     }
   });
