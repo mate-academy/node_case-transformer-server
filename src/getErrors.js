c10f2fa895ad6377f9a6501aeb72126e109ecf72
Nikita Zaika <zaikanikitaua@gmail.com>
2023-10-12 09:14:22 +0300

    solution of node_case-transformer-server task

---
+++ src/getErrors.js
@@ -0,0 +1,21 @@
+const { ERRORS, SUPPORTED_CASES } = require('./constants');
+
+function getErrors(requestedText, requestedCase) {
+  const errors = [];
+
+  if (!requestedText) {
+    errors.push({ message: ERRORS.NO_TEXT_ERROR });
+  }
+
+  if (!requestedCase) {
+    errors.push({ message: ERRORS.NO_CASE_ERROR });
+  }
+
+  if (requestedCase && !SUPPORTED_CASES.includes(requestedCase)) {
+    errors.push({ message: ERRORS.INVALID_CASE_ERROR });
+  }
+
+  return errors;
+}
+
+module.exports = { getErrors };
