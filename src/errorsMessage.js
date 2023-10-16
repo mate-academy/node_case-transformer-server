dcebdc8eb8f316db61c0fd9c4f104a9642070b62
Andrii Nakonechnyi <andreynakonechnuy11@mail.com>
2023-10-16 12:48:47 +0300

    corrected_name_message

--- src/errorsMessage.js
+++ src/errorsMessage.js
@@ -1,10 +1,13 @@
-const { ERROR_MESSAGE, CASE_OPTIONS } = require('./constants/constants');
+const {
+  CORRECT_REQUEST_MESSAGE,
+  CASE_OPTIONS,
+} = require('./constants/constants');
 
 const errorMessages = {
   NO_TEXT: 'Text to convert is required. '
-    + `Correct request is: ${ERROR_MESSAGE}`,
+    + `Correct request is: ${CORRECT_REQUEST_MESSAGE}`,
   NO_CASE: '"toCase" query param is required. Correct request is: '
-    + ERROR_MESSAGE,
+    + CORRECT_REQUEST_MESSAGE,
   NOT_VALID_CASE: `This case is not supported. Available cases: ${CASE_OPTIONS.join(', ')}.`,
 };
 
