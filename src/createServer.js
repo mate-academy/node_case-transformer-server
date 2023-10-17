25129c656d1499d3acbcd6720d64bcaac9268062
Hania Nassalska <hanna.nassalska@gmail.com>
2023-10-17 15:40:35 +0200

    errorCases

--- src/createServer.js
+++ src/createServer.js
@@ -4,7 +4,7 @@
 const http = require('http');
 const url = require('url');
 const { convertToCase } = require('./convertToCase/convertToCase');
-const { errorCases } = require('./errorMesseges');
+const { errorCases } = require('./errorCases');
 
 const createServer = () => {
   const server = http.createServer((req, res) => {
