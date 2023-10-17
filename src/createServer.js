c7eaed7fae9aef6741b2715766f56372e648b896
Hubert Wiktor <hubertwiktor6@gmail.com>
2023-10-17 12:32:30 +0200

    solution

--- src/createServer.js
+++ src/createServer.js
@@ -1,3 +1,17 @@
+/* eslint-disable no-console */
 // Write code here
 // Also, you can create additional files in the src folder
 // and import (require) them here
+const http = require('http');
+
+function createServer() {
+  const serwer = http.createServer((req, res) => {
+
+  });
+
+  return serwer;
+}
+
+module.exports = {
+  createServer,
+};
