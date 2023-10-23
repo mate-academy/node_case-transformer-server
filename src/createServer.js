3b6199833bde81a0e9ab9c47dff56e232a1c11d1
Matylda Lewandowska <matylda.lewandowska111@gmail.com>
2023-10-23 12:27:27 +0200

    adjustments :)

--- src/createServer.js
+++ src/createServer.js
@@ -4,10 +4,9 @@ const { sendBadRequest } = require('./sendBadRequest');
 
 function createServer() {
   const server = http.createServer((req, res) => {
-    const urlParams = new URL(req.url, `http://${req.headers.host}`);
-
-    const textToConvert = urlParams.pathname.substring(1);
-    const toCase = urlParams.searchParams.get('toCase');
+    const [textToConvert, queryToCase] = req.url.slice(1).split('?');
+    const searchParams = new URLSearchParams(queryToCase);
+    const toCase = searchParams.get('toCase');
 
     const errors = [];
 
@@ -21,15 +20,15 @@ function createServer() {
     if (!toCase) {
       // eslint-disable-next-line max-len
       errors.push('"toCase" query param is required. Correct request is: "/<TEXT_TO_CONVERT>?toCase=<CASE_NAME>".');
-    } else {
-      const supportedCases = ['SNAKE', 'KEBAB', 'CAMEL', 'PASCAL', 'UPPER'];
+    }
 
-      if (!supportedCases.includes(toCase.toUpperCase())) {
-        errors.push(
-          // eslint-disable-next-line max-len
-          'This case is not supported. Available cases: SNAKE, KEBAB, CAMEL, PASCAL, UPPER.',
-        );
-      }
+    const supportedCases = ['SNAKE', 'KEBAB', 'CAMEL', 'PASCAL', 'UPPER'];
+
+    if (toCase && !supportedCases.includes(toCase.toUpperCase())) {
+      errors.push(
+        // eslint-disable-next-line max-len
+        'This case is not supported. Available cases: SNAKE, KEBAB, CAMEL, PASCAL, UPPER.',
+      );
     }
 
     if (errors.length > 0) {
