40fcf0ed4ff9da4ba142ecb16d0d6be9cd7edcc1
Tetiana Poletaieva <ptupob@gmail.com>
2023-11-27 19:20:57 +0200

    node case transform

--- src/createServer.js
+++ src/createServer.js
@@ -1,3 +1,44 @@
-// Write code here
-// Also, you can create additional files in the src folder
-// and import (require) them here
+const http = require('http');
+const { convertToCase } = require('./convertToCase');
+const { validate } = require('./validate');
+
+const createServer = () => {
+  return http.createServer((req, res) => {
+    res.setHeader('Content-Type', 'application/json');
+
+    const normalizedURL = new URL(req.url, 'http://localhost:5700');
+
+    let textConvert = req.url.split('?')[0].slice(1);
+
+    if (!normalizedURL.hostname) {
+      textConvert = '';
+    }
+
+    const toCase = normalizedURL.searchParams.get('toCase');
+
+    const resValidation = validate(textConvert, toCase);
+
+    if (resValidation.errors.length !== 0) {
+      res.statusCode = 400;
+      res.statusMessage = 'Bad request';
+      res.end(JSON.stringify(resValidation));
+    }
+
+    if (resValidation.errors.length === 0) {
+      const { originalCase, convertedText }
+        = convertToCase(textConvert, toCase);
+
+      res.statusCode = 200;
+      res.statusMessage = 'Ok';
+
+      res.end(JSON.stringify({
+        originalCase,
+        targetCase: toCase,
+        originalText: textConvert,
+        convertedText,
+      }));
+    }
+  });
+};
+
+module.exports = { createServer };
