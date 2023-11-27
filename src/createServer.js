52539945ccbe766fabfe36e9ba9071647f44aaa1
Yuliia Bakun <bakunyuliia@gmail.com>
2023-11-27 12:53:12 +0100

    task implemetation

--- src/createServer.js
+++ src/createServer.js
@@ -1,3 +1,44 @@
-// Write code here
-// Also, you can create additional files in the src folder
-// and import (require) them here
+'use strict';
+
+const http = require('http');
+const validateRequest = require('./validateRequest');
+const normalizeUserInput = require('./normalizeUrl');
+const converter = require('./convertToCase/convertToCase');
+const caseName = require('./convertToCase/detectCase');
+const PORT = process.env.PORT || 8080;
+
+function createServer() {
+  const server = http.createServer((request, response) => {
+    const { text, method } = normalizeUserInput(request.url, PORT);
+    const validation = validateRequest(text, method);
+
+    response.setHeader('Content-Type', 'application/json');
+
+    if (validation instanceof Array) {
+      response.statusCode = 400;
+
+      const responseErrors = {
+        errors: validation,
+      };
+
+      response.end(JSON.stringify(responseErrors));
+    } else {
+      response.statusCode = 200;
+
+      const convertedText = converter.convertToCase(text, method);
+
+      const result = {
+        originalCase: caseName.detectCase(text),
+        targetCase: method,
+        originalText: text,
+        convertedText: convertedText.convertedText,
+      };
+
+      response.end(JSON.stringify(result));
+    }
+  });
+
+  return server;
+};
+
+module.exports = { createServer };
---
+++ src/validateRequest.js
@@ -0,0 +1,37 @@
+'use strict';
+
+function validateRequest(text, method) {
+  const errors = [];
+  const toCaseValues = [
+    'SNAKE',
+    'KEBAB',
+    'CAMEL',
+    'PASCAL',
+    'UPPER',
+  ];
+
+  if (!text) {
+    errors.push({
+      message: 'Text to convert is required. '
+        + 'Correct request is: "/<TEXT_TO_CONVERT>?toCase=<CASE_NAME>".',
+    });
+  }
+
+  if (!method) {
+    errors.push({
+      message: '"toCase" query param is required. '
+        + 'Correct request is: "/<TEXT_TO_CONVERT>?toCase=<CASE_NAME>".',
+    });
+  }
+
+  if (method && !toCaseValues.includes(method)) {
+    errors.push({
+      message: 'This case is not supported. '
+        + `Available cases: ${toCaseValues.join(', ')}.`,
+    });
+  }
+
+  return errors.length ? errors : true;
+}
+
+module.exports = validateRequest;
