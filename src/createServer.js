b53a8ed7bc04a03e5bce5f60057e1867bda289c0
Vladyslav Atanov <gibepromt@gmail.com>
2023-11-29 17:13:50 +0200

    made own server

--- src/createServer.js
+++ src/createServer.js
@@ -1,3 +1,62 @@
-// Write code here
-// Also, you can create additional files in the src folder
-// and import (require) them here
+const http = require('http');
+const { convertToCase } = require('./convertToCase/convertToCase');
+
+const caseTypes = ['SNAKE', 'KEBAB', 'CAMEL', 'PASCAL', 'UPPER'];
+
+function createServer() {
+  return http.createServer((req, res) => {
+    const errorMessages = {
+      errors: [
+      ],
+    };
+
+    const [originalText, queryString] = req.url.slice(1).split('?');
+
+    const params = new URLSearchParams(queryString);
+    const targetCase = params.get('toCase');
+
+    res.setHeader('Content-Type', 'application/json');
+
+    if (!originalText) {
+      res.statusCode = 400;
+
+      errorMessages.errors.push(
+        { message: 'Text to convert is required.'
+        + ' Correct request is: "/<TEXT_TO_CONVERT>?toCase=<CASE_NAME>".' },
+      );
+    }
+
+    if (!targetCase) {
+      res.statusCode = 400;
+
+      errorMessages.errors.push(
+        { message: '"toCase" query param is required.'
+          + ' Correct request is: "/<TEXT_TO_CONVERT>?toCase=<CASE_NAME>".' },
+      );
+    }
+
+    if (!caseTypes.includes(targetCase) && targetCase) {
+      res.statusCode = 400;
+
+      errorMessages.errors.push(
+        { message: 'This case is not supported. '
+          + 'Available cases: SNAKE, KEBAB, CAMEL, PASCAL, UPPER.' },
+      );
+    }
+
+    if (errorMessages.errors.length) {
+      res.end(JSON.stringify(errorMessages));
+    } else {
+      const {
+        originalCase,
+        convertedText,
+      } = convertToCase(originalText, targetCase);
+
+      res.end(JSON.stringify({
+        originalCase, targetCase, originalText, convertedText,
+      }));
+    }
+  });
+}
+
+module.exports.createServer = createServer;
