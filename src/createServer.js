f2f6e120ea318ca466df084d67d7f743a90dab7e
BetterSol <94358102+BetterSol@users.noreply.github.com>
2023-11-29 14:03:14 +0200

    initial code started

--- src/createServer.js
+++ src/createServer.js
@@ -1,3 +1,17 @@
-// Write code here
-// Also, you can create additional files in the src folder
-// and import (require) them here
+const http = require('http');
+
+const createServer = () => {
+  const server = http.createServer((req, res) => {
+    res.setHeader('Content-Type', 'text/html');
+    res.statusCode = 200;
+
+    res.write('<h1>Hello, world!</h1>');
+    res.end('the last portion of data');
+  });
+
+  return server;
+};
+
+module.exports = {
+  createServer,
+};
