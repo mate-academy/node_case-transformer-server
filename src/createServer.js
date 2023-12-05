82fd1fe193e4a7d582c1bdb2be61b22230aea4d3
Dmytro <dmytro2105@gmail.com>
2023-12-05 22:32:26 +0200

    case transformer

--- src/createServer.js
+++ src/createServer.js
@@ -1,3 +1,64 @@
-// Write code here
-// Also, you can create additional files in the src folder
-// and import (require) them here
+/* eslint-disable max-len */
+/* eslint-disable no-console */
+function createServer() {
+  const http = require('http');
+  const convertToCase = require('src/convertToCase');
+
+  const server = http.createServer((req, res) => {
+    res.setHeader('Content-Type', 'application/json');
+
+    const myUrl = new URL(req.url, 'http://localhost:5700');
+    const originalText = myUrl.pathname.slice(1);
+    const targetCase = myUrl.searchParams.get('toCase');
+
+    const errors = [];
+
+    if (!originalText) {
+      errors.push({
+        message: 'Text to convert is required. Correct request is: "/<TEXT_TO_CONVERT>?toCase=<CASE_NAME>".',
+      });
+    }
+
+    if (!targetCase) {
+      errors.push({
+        message: '"toCase" query param is required. Correct request is: "/<TEXT_TO_CONVERT>?toCase=<CASE_NAME>".',
+      });
+    } else {
+      const supportedCases = ['SNAKE', 'KEBAB', 'CAMEL', 'PASCAL', 'UPPER'];
+
+      if (!supportedCases.includes(targetCase.toUpperCase())) {
+        errors.push({
+          message: 'This case is not supported. Available cases: SNAKE, KEBAB, CAMEL, PASCAL, UPPER.',
+        });
+      }
+    }
+
+    if (errors.length) {
+      res.writeHead(400, { 'Content-Type': 'application/json' });
+      res.end(JSON.stringify({ errors }));
+    }
+
+    const convertedResult = convertToCase(targetCase, originalText);
+
+    const { originalCase, convertedText } = convertedResult;
+
+    const data = {
+      originalCase,
+      targetCase: 'CASE_NAME',
+      originalText,
+      convertedText,
+    };
+
+    res.writeHead(200, { 'Content-Type': 'application/json' });
+    res.end(JSON.stringify({ success: true, resData: data }));
+
+    console.log(myUrl);
+    console.log(myUrl.searchParams.get('toCase'));
+  });
+
+  return server;
+}
+
+module.exports = {
+  createServer,
+};
