1bdc0faf02d596b7f5df91497c41f8096ca7088a
Orest Kozak <orestkozak17@gmail.com>
2023-12-14 17:48:02 +0200

    errors and const were updated

--- src/createServer.js
+++ src/createServer.js
@@ -1,9 +1,16 @@
 /* eslint-disable quote-props */
 /* eslint-disable max-len */
 const { convertToCase } = require('./convertToCase');
+const { supportedCases } = require('./supportedCases');
 
 const createServer = () => {
   const http = require('http');
+
+  const errorMessages = {
+    missingText: 'Text to convert is required. Correct request is: "/<TEXT_TO_CONVERT>?toCase=<CASE_NAME>"',
+    missingToCaseQueryParam: '"toCase" query param is required. Correct request is: "/<TEXT_TO_CONVERT>?toCase=<CASE_NAME>"',
+    unsupportedCase: 'This case is not supported. Available cases: SNAKE, KEBAB, CAMEL, PASCAL, UPPER',
+  };
 
   const server = http.createServer((req, res) => {
     res.setHeader('Content-Type', 'application/json');
@@ -14,30 +21,28 @@ const createServer = () => {
     const targetCase = url.searchParams.get('toCase');
 
     if (!originalText) {
-      errors.push({
-        message: 'Text to convert is required. Correct request is: "/<TEXT_TO_CONVERT>?toCase=<CASE_NAME>".',
-      });
+      errors.push({ message: errorMessages.missingText });
     }
 
     if (!targetCase) {
-      errors.push({
-        message: '"toCase" query param is required. Correct request is: "/<TEXT_TO_CONVERT>?toCase=<CASE_NAME>".',
-      });
+      errors.push({ message: errorMessages.missingToCaseQueryParam });
     } else {
-      const supportedCases = ['SNAKE', 'KEBAB', 'CAMEL', 'PASCAL', 'UPPER'];
-
       if (!supportedCases.includes(targetCase)) {
-        errors.push({
-          message: 'This case is not supported. Available cases: SNAKE, KEBAB, CAMEL, PASCAL, UPPER.',
-        });
+        errors.push({ message: errorMessages.unsupportedCase });
       }
+    }
+
+    // Check for both missing text and invalid toCase
+    if (!originalText && !targetCase) {
+      errors.push({ message: errorMessages.missingText });
+      errors.push({ message: errorMessages.unsupportedCase });
     }
 
     if (errors.length) {
       res.statusCode = 400;
 
       return res.end(JSON.stringify({
-        errors,
+        errors: errors.map(error => ({ message: error.message + '.' })),
       }));
     }
 
