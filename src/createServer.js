485d01bf0b4e2bfb043adb91165a9a58b0b2ae34
Mykola Hadupiak <mykola.hadupiak@gmail.com>
2024-01-05 22:48:18 +0200

    case transform

--- src/createServer.js
+++ src/createServer.js
@@ -1,3 +1,42 @@
-// Write code here
-// Also, you can create additional files in the src folder
-// and import (require) them here
+/* eslint-disable no-console */
+const http = require('http');
+const { convertToCase } = require('./convertToCase/convertToCase');
+const { errorMessages } = require('./errorMessages');
+
+function createServer() {
+  const server = http.createServer((req, res) => {
+    res.setHeader('Content-Type', 'application/json');
+
+    const { pathname, searchParams } = new URL(req.url, `http://${req.headers.host}`);
+
+    const toCase = searchParams.get('toCase');
+    const mormalizedPath = pathname.slice(1);
+    const errors = errorMessages(mormalizedPath, toCase);
+
+    if (errors.length) {
+      res.statusCode = 400;
+      res.statusMessage = 'Bad request';
+
+      res.end(JSON.stringify({ errors }));
+
+      return;
+    }
+
+    res.statusCode = 200;
+    res.statusMessage = 'OK';
+
+    const convertedText = convertToCase(mormalizedPath, toCase);
+
+    res.end(JSON.stringify({
+      ...convertedText,
+      targetCase: toCase,
+      originalText: mormalizedPath,
+    }));
+  });
+
+  return server;
+};
+
+module.exports = {
+  createServer,
+};
