3a7b364ff6fc0096d41357b2bc2b0a4967d030fe
«Maria» <nedymai@gmail.com>
2024-01-09 21:19:08 +0200

    solution

--- src/createServer.js
+++ src/createServer.js
@@ -1,3 +1,44 @@
-// Write code here
-// Also, you can create additional files in the src folder
-// and import (require) them here
+/* eslint-disable max-len */
+/* eslint-disable no-console */
+const http = require('http');
+const { convertToCase } = require('./convertToCase');
+const { detectErrors } = require('./detectErrors');
+
+function createServer() {
+  const server = http.createServer((req, res) => {
+    res.setHeader('Content-Type', 'application/json');
+
+    const normalize = new URL(req.url, 'http:localhost:5700');
+    const originalText = normalize.pathname.slice(1) || '';
+    const targetCase = normalize.searchParams.get('toCase') || '';
+
+    const errors = detectErrors(originalText, targetCase);
+
+    console.log(errors);
+
+    if (errors) {
+      res.statusCode = 400;
+      res.statusMessage = 'Bad request';
+
+      res.end(JSON.stringify({ errors }));
+    }
+
+    if (!errors) {
+      const { originalCase, convertedText } = convertToCase(originalText, targetCase);
+
+      res.statusCode = 200;
+      res.statusMessage = 'OK';
+
+      res.end(JSON.stringify({
+        originalCase,
+        targetCase,
+        originalText,
+        convertedText,
+      }));
+    }
+  });
+
+  return server;
+};
+
+module.exports = { createServer };
