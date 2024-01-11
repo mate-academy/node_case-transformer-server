4b2f4262b218ad2aa449eeb72fd5b086a0ae958d
Ruslana Gnatenko <rgnatenko5@gmail.com>
2024-01-11 15:27:08 +0300

    create server

--- src/createServer.js
+++ src/createServer.js
@@ -1,3 +1,13 @@
 // Write code here
 // Also, you can create additional files in the src folder
 // and import (require) them here
+const http = require('node:http');
+const { makeRequest } = require('./convertToCase/makeRequest');
+
+const server = http.createServer(makeRequest);
+
+const createServer = () => server;
+
+module.exports = {
+  createServer,
+};
