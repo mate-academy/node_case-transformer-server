57f223acf94f318052cb684783f7bbbfc081e692
Anton Kuchmasov <antek.ukraine@gmail.com>
2024-01-15 08:56:10 +0200

    fixed vars names, separated error logic in ordinary file

--- src/createServer.js
+++ src/createServer.js
@@ -1,13 +1,9 @@
 const http = require('http');
 const { convertToCase } = require('./convertToCase');
 const { wordsToCase } = require('./convertToCase/wordsToCase');
+const { errorHandler } = require('./ErrorHandler');
 
-const REQUIRE_TEXT_MESSAGE = 'Text to convert is required. Correct request is: "/<TEXT_TO_CONVERT>?toCase=<CASE_NAME>".';
-const TOCASE_MISSING = '"toCase" query param is required. Correct request is: "/<TEXT_TO_CONVERT>?toCase=<CASE_NAME>".';
-const TOCASE_NOT_ALLOWED = 'This case is not supported. Available cases: SNAKE, KEBAB, CAMEL, PASCAL, UPPER.';
 const BAD_REQUEST = 'Bad request';
-
-const CaseName = ['SNAKE', 'KEBAB', 'CAMEL', 'PASCAL', 'UPPER'];
 
 const PORT = process.env.PORT || 5700;
 
@@ -26,19 +22,7 @@ function createServer() {
     const originalText = normalizedURL.pathname.slice(1);
     const targetCase = normalizedURL.searchParams.get('toCase');
 
-    const errorsObj = { 'errors': [] };
-
-    if (!originalText) {
-      errorsObj.errors.push({ 'message': REQUIRE_TEXT_MESSAGE });
-    }
-
-    if (!targetCase) {
-      errorsObj.errors.push({ 'message': TOCASE_MISSING });
-    }
-
-    if (targetCase && (!CaseName.includes(targetCase))) {
-      errorsObj.errors.push({ 'message': TOCASE_NOT_ALLOWED });
-    }
+    const errorsObj = errorHandler(originalText, targetCase);
 
     if (errorsObj.errors.length) {
       res.statusCode = 400;
