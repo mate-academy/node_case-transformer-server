99cd88e9c27d029dce3b66a0e73fade90871fc0f
Nazarii <kostrubitskiiwork@gmail.com>
2024-03-07 20:50:59 +0200

    solution

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
+/* eslint-disable no-console */
 const { createServer } = require('./src/createServer');
 
 createServer()
   .listen(5700, () => {
-    // eslint-disable-next-line no-console
     console.log('Server started! 🚀');
   });
