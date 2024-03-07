971e0f8bb9329b25053faaa455d4913a742aaeb5
Vlad Gerula <vlad.gerula@gmail.com>
2024-03-07 22:28:59 +0200

    changed according to comments :)

--- src/createServer.js
+++ src/createServer.js
@@ -4,41 +4,49 @@
 // and import (require) them here
 const http = require('http');
 const { convertToCase } = require('./convertToCase');
+const VALIDCASES = ['SNAKE', 'KEBAB', 'CAMEL', 'PASCAL', 'UPPER'];
+
+function handleErrors(textToConvert, toCase) {
+  const errors = [];
+
+  if (!textToConvert) {
+    errors.push('Text to convert is required. Correct request is: "/<TEXT_TO_CONVERT>?toCase=<CASE_NAME>".');
+  }
+
+  if (!toCase) {
+    errors.push('"toCase" query param is required. Correct request is: "/<TEXT_TO_CONVERT>?toCase=<CASE_NAME>".');
+  } else if (!VALIDCASES.includes(toCase.toUpperCase())) {
+    errors.push('This case is not supported. Available cases: SNAKE, KEBAB, CAMEL, PASCAL, UPPER.');
+  }
+
+  return errors;
+}
 
 function createServer() {
   const server = http.createServer((req, res) => {
+    const normalizedURL = new URL(req.url, `http://${req.headers.host}`);
+    const textToConvert = normalizedURL.pathname.slice(1);
+    const toCase = normalizedURL.searchParams.get('toCase');
+
     try {
-      const normalizedURL = new URL(req.url, `http://${req.headers.host}`);
-      const textToConvert = normalizedURL.pathname.slice(1);
-      const toCase = normalizedURL.searchParams.get('toCase');
-      const validCases = ['SNAKE', 'KEBAB', 'CAMEL', 'PASCAL', 'UPPER'];
-
-      const errors = [];
-
-      if (!textToConvert) {
-        errors.push('Text to convert is required. Correct request is: "/<TEXT_TO_CONVERT>?toCase=<CASE_NAME>".');
-      }
-
-      if (!toCase) {
-        errors.push('"toCase" query param is required. Correct request is: "/<TEXT_TO_CONVERT>?toCase=<CASE_NAME>".');
-      } else if (!validCases.includes(toCase.toUpperCase())) {
-        errors.push('This case is not supported. Available cases: SNAKE, KEBAB, CAMEL, PASCAL, UPPER.');
-      }
+      const errors = handleErrors(textToConvert, toCase);
 
       if (errors.length > 0) {
         throw errors;
       }
 
-      const result = convertToCase(textToConvert, toCase.toUpperCase());
+      const { originalCase, convertedText } = convertToCase(textToConvert, toCase.toUpperCase());
 
       res.writeHead(200, { 'Content-Type': 'application/json' });
 
-      res.end(JSON.stringify({
-        originalCase: result.originalCase,
+      const response = {
+        originalCase,
         targetCase: toCase.toUpperCase(),
         originalText: textToConvert,
-        convertedText: result.convertedText,
-      }));
+        convertedText,
+      };
+
+      res.end(JSON.stringify(response));
     } catch (errors) {
       res.writeHead(400, 'Bad Request', { 'Content-Type': 'application/json' });
 
