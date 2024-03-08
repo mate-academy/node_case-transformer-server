e7cbd0ea94126cec3ff4cd38aeb56c705ee533ca
avakiel <79792776+avakiel@users.noreply.github.com>
2024-03-08 20:50:53 +0200

    solution

--- src/createServer.js
+++ src/createServer.js
@@ -1,3 +1,60 @@
-// Write code here
-// Also, you can create additional files in the src folder
-// and import (require) them here
+/* eslint-disable no-console */
+/* eslint-disable max-len */
+const http = require('http');
+const { convertToCase } = require('./convertToCase');
+
+const handleErrors = (res, errors) => {
+  res.end(JSON.stringify({ errors }));
+};
+
+const handleConversion = (res, originalText, targetCase) => {
+  const { originalCase, convertedText } = convertToCase(originalText, targetCase);
+
+  res.end(JSON.stringify({
+    originalCase, targetCase, originalText, convertedText,
+  }));
+};
+
+const createServer = () => {
+  const cases = ['SNAKE', 'KEBAB', 'CAMEL', 'PASCAL', 'UPPER'];
+  const server = http.createServer((req, res) => {
+    res.setHeader('Content-Type', 'application/json');
+
+    const path = new URL(req.url, 'http://localhost');
+    const originalText = path.pathname.slice(1);
+    const targetCase = path.searchParams.get('toCase');
+
+    const errorMessages = {
+      errors: [],
+    };
+
+    if (!originalText) {
+      errorMessages.errors.push({
+        message: 'Text to convert is required. Correct request is: "/<TEXT_TO_CONVERT>?toCase=<CASE_NAME>".',
+      });
+    }
+
+    if (!targetCase) {
+      errorMessages.errors.push({
+        message: '"toCase" query param is required. Correct request is: "/<TEXT_TO_CONVERT>?toCase=<CASE_NAME>".',
+      });
+    }
+
+    if (!cases.includes(targetCase) && targetCase) {
+      errorMessages.errors.push({
+        message: 'This case is not supported. Available cases: SNAKE, KEBAB, CAMEL, PASCAL, UPPER.',
+      });
+    }
+
+    if (!errorMessages.errors.length) {
+      handleConversion(res, originalText, targetCase);
+    } else {
+      res.statusCode = 400;
+      handleErrors(res, errorMessages.errors);
+    }
+  });
+
+  return server;
+};
+
+module.exports = { createServer };
