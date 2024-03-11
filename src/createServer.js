3559262e34064dae891fba8afe9d9a98c63c1ddc
Dmytro Pivkopa <alien_dima@yahoo.com>
2024-03-11 16:16:43 +0200

    add edits of the mentor

--- src/createServer.js
+++ src/createServer.js
@@ -2,6 +2,14 @@
 const http = require('http');
 const { convertToCase } = require('./convertToCase/convertToCase');
 
+const AVALIABE_CASES = ['SNAKE', 'KEBAB', 'CAMEL', 'PASCAL', 'UPPER'];
+const ERR_CONVERT_TEXT = 'Text to convert is required. Correct request is:'
++ ' "/<TEXT_TO_CONVERT>?toCase=<CASE_NAME>".';
+const ERR_TO_CASE = '"toCase" query param is required.'
++ ' Correct request is: "/<TEXT_TO_CONVERT>?toCase=<CASE_NAME>".';
+const ERR_SUPORTED_CASE = 'This case is not supported.'
++ ' Available cases: SNAKE, KEBAB, CAMEL, PASCAL, UPPER.';
+
 const createServer = () => {
   const sever = http.createServer((req, res) => {
     const { pathname, searchParams } = new URL(req.url, `http://${req.headers.host}`);
@@ -11,17 +19,13 @@ const createServer = () => {
     const errors = [];
 
     if (!textToConvert) {
-      errors.push({ message: 'Text to convert is required. Correct request is:'
-      + ' "/<TEXT_TO_CONVERT>?toCase=<CASE_NAME>".' });
+      errors.push({ message: ERR_CONVERT_TEXT });
     }
 
     if (!toCase) {
-      errors.push({ message: '"toCase" query param is required.'
-      + ' Correct request is: "/<TEXT_TO_CONVERT>?toCase=<CASE_NAME>".' });
-    } else if (!['SNAKE', 'KEBAB', 'CAMEL', 'PASCAL', 'UPPER']
-      .includes(toCase)) {
-      errors.push({ message: 'This case is not supported.'
-      + ' Available cases: SNAKE, KEBAB, CAMEL, PASCAL, UPPER.' });
+      errors.push({ message: ERR_TO_CASE });
+    } else if (!AVALIABE_CASES.includes(toCase)) {
+      errors.push({ message: ERR_SUPORTED_CASE });
     }
 
     if (errors.length > 0) {
