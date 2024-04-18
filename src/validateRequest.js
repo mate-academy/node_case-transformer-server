f2d878a34ba7988d9fbdc61ef22c12b74f6804ff
Yehor-f2006 <151622806+Yehor-f2006@users.noreply.github.com>
2024-04-18 14:15:42 +0300

    added fixes

--- src/validateRequest.js
+++ src/validateRequest.js
@@ -1,17 +1,24 @@
+/* eslint-disable max-len */
 const cases = ['SNAKE', 'KEBAB', 'UPPER', 'PASCAL', 'CAMEL'];
+const errorMessages = [
+  'Text to convert is required. Correct request is: "/<TEXT_TO_CONVERT>?toCase=<CASE_NAME>".',
+  '"toCase" query param is required. Correct request is: "/<TEXT_TO_CONVERT>?toCase=<CASE_NAME>".',
+  'This case is not supported. Available cases: SNAKE, KEBAB, CAMEL, PASCAL, UPPER.',
+];
 
 const validateCase = (originalText, targetCase) => {
   const errors = [];
 
-  if (!originalText || !targetCase) {
-    errors.push('Both paramaters have to be present');
+  if (!originalText) {
+    errors.push({ message: errorMessages[0] });
+  }
+
+  if (!targetCase) {
+    errors.push({ message: errorMessages[1] });
   }
 
   if (!cases.includes(targetCase)) {
-    /* eslint-disable max-len */
-    errors.push(
-      "Enter a valid target case. Consider one of the following: ['SNAKE', 'KEBAB', 'UPPER', 'PASCAL', 'CAMEL']",
-    );
+    errors.push({ message: errorMessages[2] });
   }
 
   return errors;
