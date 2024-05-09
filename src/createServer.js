1ea412e838edf41ab899aec8854477d0ab18fda4
Vladyslav Zakharov <vladyslavzakharovdk@gmail.com>
2024-05-09 13:17:10 +0300

    solution

--- src/createServer.js
+++ src/createServer.js
@@ -6,49 +6,56 @@ const { convertToCase } = require('./convertToCase/convertToCase');
 const getErrorMessages = (text, caseName) => {
   const errorsList = [];
   const supportedCases = ['SNAKE', 'KEBAB', 'CAMEL', 'PASCAL', 'UPPER'];
-  const correctRequest = `Correct request is: "/<TEXT_TO_CONVERT>?toCase=<CASE_NAME>"`;
+  const correctRequest = `Correct request is: "/<TEXT_TO_CONVERT>?toCase=<CASE_NAME>".`;
 
   if (!text) {
-    errorsList.push(`Text to convert is required. ${correctRequest}`);
+    errorsList.push({
+      message: `Text to convert is required. ${correctRequest}`,
+    });
   }
 
   if (!caseName) {
-    errorsList.push(`toCase" query param is required. ${correctRequest}`);
-  }
+    errorsList.push({
+      message: `"toCase" query param is required. ${correctRequest}`,
+    });
 
-  if (!supportedCases.includes(supportedCases)) {
-    errorsList.push(
-      `This case is not supported. Available cases: SNAKE, KEBAB, CAMEL, PASCAL, UPPER.`,
-    );
-  }
-
-  if (!errorsList.length) {
     return errorsList;
   }
+
+  if (!supportedCases.includes(caseName)) {
+    errorsList.push({
+      message: `This case is not supported. Available cases: SNAKE, KEBAB, CAMEL, PASCAL, UPPER.`,
+    });
+  }
+
+  return errorsList;
 };
 
 const createServer = () => {
   const server = http.createServer((req, res) => {
+    res.setHeader('Content-Type', 'application/json');
+
     const normalizedUrl = new url.URL(req.url, `http://${req.headers.host}`);
     const normalizedPathname = normalizedUrl.pathname.slice(1);
     const normalizedParams = normalizedUrl.searchParams.get('toCase');
     const errors = getErrorMessages(normalizedPathname, normalizedParams);
-    const result = convertToCase(normalizedPathname, normalizedParams);
-
-    global.console.log(result);
 
     if (errors.length) {
       res.statusCode = 400;
       res.end(JSON.stringify({ errors }));
+
+      return;
     }
+
+    const result = convertToCase(normalizedPathname, normalizedParams);
 
     res.statusCode = 200;
     res.statusMessage = 'OK';
 
     res.end(
       JSON.stringify({
-        originalCase: normalizedParams,
-        targetCase: result.originalCase,
+        originalCase: result.originalCase,
+        targetCase: normalizedParams,
         originalText: normalizedPathname,
         convertedText: result.convertedText,
       }),
