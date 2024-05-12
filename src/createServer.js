4b048efcea6d2435d6267e134ad3e4f014b315f2
Aleksandr <kartanovaleks@gmail.com>
2024-05-13 00:15:05 +0300

    solution

--- src/createServer.js
+++ src/createServer.js
@@ -17,17 +17,15 @@ function createServer() {
         errors.push({
           message: `"toCase" query param is required. Correct request is: "/<TEXT_TO_CONVERT>?toCase=<CASE_NAME>".`,
         });
+      } else if (!availableCases.includes(caseName)) {
+        errors.push({
+          message: `This case is not supported. Available cases: SNAKE, KEBAB, CAMEL, PASCAL, UPPER.`,
+        });
       }
 
       if (!originalText) {
         errors.push({
           message: `Text to convert is required. Correct request is: "/<TEXT_TO_CONVERT>?toCase=<CASE_NAME>".`,
-        });
-      }
-
-      if (!availableCases.includes(caseName)) {
-        errors.push({
-          message: `This case is not supported. Available cases: SNAKE, KEBAB, CAMEL, PASCAL, UPPER.`,
         });
       }
 
