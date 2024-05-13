63b0d288a373c94fb7bf3de745fb6ccdd50b2aa7
Khrystyna Hryts <hi4lan@gmail.com>
2024-05-13 10:04:17 +0300

    solution

--- src/createServer.js
+++ src/createServer.js
@@ -1,3 +1,4 @@
+/* eslint-disable max-len */
 const http = require('http');
 const { convertToCase } = require('./convertToCase');
 
@@ -8,34 +9,40 @@ const STATUS_CODE = {
   ERROR: 400,
 };
 
+const ERROR_MESSAGES = {
+  TO_CASE_REQUIRED: `"toCase" query param is required. Correct request is: "/<TEXT_TO_CONVERT>?toCase=<CASE_NAME>".`,
+  TEXT_REQUIRED: `Text to convert is required. Correct request is: "/<TEXT_TO_CONVERT>?toCase=<CASE_NAME>".`,
+  CASE_NOT_SUPPORTED: `This case is not supported. Available cases: SNAKE, KEBAB, CAMEL, PASCAL, UPPER.`,
+};
+
+function validateRequest(targetCase, originalText) {
+  const errors = [];
+
+  if (!targetCase) {
+    errors.push({ message: ERROR_MESSAGES.TO_CASE_REQUIRED });
+  }
+
+  if (!originalText) {
+    errors.push({ message: ERROR_MESSAGES.TEXT_REQUIRED });
+  }
+
+  if (targetCase && !CASES.includes(targetCase.toUpperCase())) {
+    errors.push({ message: ERROR_MESSAGES.CASE_NOT_SUPPORTED });
+  }
+
+  return errors;
+}
+
 function createServer() {
   const server = http.createServer((req, res) => {
     const urlParts = req.url.slice(1).split('?');
-    const pathToConvert = urlParts[0];
-    const toCase = new URLSearchParams(urlParts[1]).get('toCase');
+    const originalText = urlParts[0];
+    const targetCase = new URLSearchParams(urlParts[1]).get('toCase');
+    const errors = validateRequest(targetCase, originalText);
 
-    const errors = [];
-
-    if (!toCase) {
-      errors.push({
-        message: `"toCase" query param is required. Correct request is: "/<TEXT_TO_CONVERT>?toCase=<CASE_NAME>".`,
-      });
-    }
-
-    if (!pathToConvert) {
-      errors.push({
-        message: `Text to convert is required. Correct request is: "/<TEXT_TO_CONVERT>?toCase=<CASE_NAME>".`,
-      });
-    }
-
-    if (toCase && !CASES.includes(toCase.toUpperCase())) {
-      errors.push({
-        message: `This case is not supported. Available cases: SNAKE, KEBAB, CAMEL, PASCAL, UPPER.`,
-      });
-    }
+    res.setHeader('Content-Type', 'application/json');
 
     if (errors.length) {
-      res.setHeader('Content-Type', 'application/json');
       res.statusCode = STATUS_CODE.ERROR;
       res.end(JSON.stringify({ errors }));
 
@@ -43,18 +50,17 @@ function createServer() {
     }
 
     const { originalCase, convertedText } = convertToCase(
-      pathToConvert,
-      toCase,
+      originalText,
+      targetCase,
     );
 
-    res.setHeader('Content-Type', 'application/json');
     res.statusCode = STATUS_CODE.OK;
 
     res.end(
       JSON.stringify({
         originalCase,
-        targetCase: toCase,
-        originalText: pathToConvert,
+        targetCase: targetCase,
+        originalText,
         convertedText,
       }),
     );
