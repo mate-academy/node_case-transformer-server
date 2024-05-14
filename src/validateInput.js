d51a4b186659d915f7f9d9523159a801a8e9a557
Olena Shapkun <shapkunkh@gmail.com>
2024-05-14 18:16:16 +0300

    solution

---
+++ src/validateInput.js
@@ -0,0 +1,36 @@
+function validateInput(text, caseName) {
+  const errors = [];
+
+  if (!text) {
+    errors.push({
+      message:
+        'Text to convert is required. Correct request is:' +
+        '"/<TEXT_TO_CONVERT>?toCase=<CASE_NAME>".',
+    });
+  }
+
+  if (!caseName) {
+    errors.push({
+      message:
+        '"toCase" query param is required. Correct request is:' +
+        '"/<TEXT_TO_CONVERT>?toCase=<CASE_NAME>".',
+    });
+  }
+
+  if (
+    caseName &&
+    !['SNAKE', 'KEBAB', 'CAMEL', 'PASCAL', 'UPPER'].includes(
+      caseName.toUpperCase(),
+    )
+  ) {
+    errors.push({
+      message:
+        'This case is not supported. Available cases:' +
+        'SNAKE, KEBAB, CAMEL, PASCAL, UPPER.',
+    });
+  }
+
+  return errors;
+}
+
+module.exports = { validateInput };
