855d4d5a7b037dcdf7aca237acf5ab5f5125702f
Dmytro Haidash <tipocto.ckujiji@gmail.com>
2024-05-17 18:08:46 +0300

    add task solution

--- src/createServer.js
+++ src/createServer.js
@@ -1,3 +1,68 @@
-// Write code here
-// Also, you can create additional files in the src folder
-// and import (require) them here
+const http = require('http');
+const { convertToCase } = require('./convertToCase');
+const { detectCase } = require('./convertToCase/detectCase');
+
+const PORT = process.env.PORT || 1595;
+
+const createServer = () => {
+  return http.createServer((req, res) => {
+    const normalizedUrl = new URL(req.url, `http://localhost:${PORT}`);
+    const originalText = decodeURIComponent(normalizedUrl.pathname.slice(1));
+    const originalCase = detectCase(originalText);
+    const toCase = normalizedUrl.searchParams.get('toCase');
+
+    res.setHeader('Content-Type', 'application/json');
+
+    const errors = [];
+    const cases = ['SNAKE', 'KEBAB', 'CAMEL', 'PASCAL', 'UPPER'];
+
+    const handleError = (message) => {
+      const error = { message };
+
+      errors.push(error);
+    };
+
+    if (!originalText) {
+      const errorMessage =
+        'Text to convert is required. Correct request is: "/<TEXT_TO_CONVERT>?toCase=<CASE_NAME>".';
+
+      handleError(errorMessage);
+    }
+
+    if (toCase && !cases.includes(toCase)) {
+      const errorMessage =
+        'This case is not supported. Available cases: SNAKE, KEBAB, CAMEL, PASCAL, UPPER.';
+
+      handleError(errorMessage);
+    }
+
+    if (!toCase) {
+      const errorMessage =
+        '"toCase" query param is required. Correct request is: "/<TEXT_TO_CONVERT>?toCase=<CASE_NAME>".';
+
+      handleError(errorMessage);
+    }
+
+    if (errors.length) {
+      res.end(JSON.stringify({ errors }));
+
+      return;
+    }
+
+    const convertedText = convertToCase(originalText, toCase);
+
+    const result = {
+      originalCase,
+      targetCase: toCase,
+      originalText: originalText,
+    };
+
+    const ress = Object.assign(result, convertedText);
+
+    res.end(JSON.stringify(ress));
+  });
+};
+
+module.exports = {
+  createServer,
+};
