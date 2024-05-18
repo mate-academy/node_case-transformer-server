2c0da3b49076f4393e68dd876fbf0229b8bbd6fa
Ihor Prodan <prodanigor0@gmail.com>
2024-05-18 21:36:14 +0200

    create server

--- src/createServer.js
+++ src/createServer.js
@@ -1,3 +1,65 @@
-// Write code here
-// Also, you can create additional files in the src folder
-// and import (require) them here
+const http = require('http');
+const { convertToCase } = require('./convertToCase');
+
+function createServer() {
+  return http.createServer((req, res) => {
+    try {
+      const myURL = new URL(req.url, `http://${req.headers.host}`);
+      const pathname = myURL.pathname.slice(1);
+      const toCase = myURL.searchParams.get('toCase');
+
+      const errors = [];
+
+      if (!pathname) {
+        errors.push({
+          message:
+            // eslint-disable-next-line max-len
+            'Text to convert is required. Correct request is: "/<TEXT_TO_CONVERT>?toCase=<CASE_NAME>".',
+        });
+      }
+
+      if (!toCase) {
+        errors.push({
+          message:
+            // eslint-disable-next-line max-len
+            '"toCase" query param is required. Correct request is: "/<TEXT_TO_CONVERT>?toCase=<CASE_NAME>".',
+        });
+      }
+
+      const supportedCases = ['SNAKE', 'KEBAB', 'CAMEL', 'PASCAL', 'UPPER'];
+
+      if (toCase && !supportedCases.includes(toCase)) {
+        errors.push({
+          message:
+            // eslint-disable-next-line max-len
+            'This case is not supported. Available cases: SNAKE, KEBAB, CAMEL, PASCAL, UPPER.',
+        });
+      }
+
+      if (errors.length > 0) {
+        res.writeHead(400, { 'Content-Type': 'application/json' });
+        res.end(JSON.stringify({ errors }));
+
+        return;
+      }
+
+      const result = convertToCase(pathname, toCase);
+
+      res.writeHead(200, { 'Content-Type': 'application/json' });
+
+      res.end(
+        JSON.stringify({
+          originalCase: result.originalCase,
+          targetCase: toCase,
+          originalText: pathname,
+          convertedText: result.convertedText,
+        }),
+      );
+    } catch (err) {
+      res.writeHead(500, { 'Content-Type': 'application/json' });
+      res.end(JSON.stringify({ errors: [{ message: err.message }] }));
+    }
+  });
+}
+
+module.exports = { createServer };
