14535d2c4927d90e1d830e288e0c86fe9e150a91
ArtemZakhar <artem.zakharchuk@ukr.net>
2024-05-20 14:33:53 +0300

    improvements

--- src/urlValidation/urlValidation.js
+++ src/urlValidation/urlValidation.js
@@ -1,23 +1,26 @@
+const cases = ['SNAKE', 'KEBAB', 'CAMEL', 'PASCAL', 'UPPER'];
+
+const noTextError = {
+  message: `Text to convert is required. Correct request is: "/<TEXT_TO_CONVERT>?toCase=<CASE_NAME>".`,
+};
+const inappropriateCaseError = {
+  message: `This case is not supported. Available cases: SNAKE, KEBAB, CAMEL, PASCAL, UPPER.`,
+};
+
+const noQueryParamsError = {
+  message: `"toCase" query param is required. Correct request is: "/<TEXT_TO_CONVERT>?toCase=<CASE_NAME>".`,
+};
+
+const MIN_ALLOWED_LENGTH = 2;
+
 function urlValidation(url) {
-  const cases = ['SNAKE', 'KEBAB', 'CAMEL', 'PASCAL', 'UPPER'];
   const [textToConvert, searchParams] = url.split('?');
 
   const errors = [];
   const params = new URLSearchParams(searchParams);
   const toCase = params.get('toCase');
-  const normilizedText = textToConvert.slice(1);
-  const noTextWasProvided = textToConvert.length < 2;
-
-  const noTextError = {
-    message: `Text to convert is required. Correct request is: "/<TEXT_TO_CONVERT>?toCase=<CASE_NAME>".`,
-  };
-  const inappropriateCaseError = {
-    message: `This case is not supported. Available cases: SNAKE, KEBAB, CAMEL, PASCAL, UPPER.`,
-  };
-
-  const noQueryParamsError = {
-    message: `"toCase" query param is required. Correct request is: "/<TEXT_TO_CONVERT>?toCase=<CASE_NAME>".`,
-  };
+  const normalizedText = textToConvert.slice(1);
+  const noTextWasProvided = textToConvert.length < MIN_ALLOWED_LENGTH;
 
   if (noTextWasProvided) {
     errors.push(noTextError);
@@ -31,7 +34,7 @@ function urlValidation(url) {
     errors.push(noQueryParamsError);
   }
 
-  return { originalText: normilizedText, targetCase: toCase, errors };
+  return { originalText: normalizedText, targetCase: toCase, errors };
 }
 
 module.exports = { urlValidation };
