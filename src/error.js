e4b9bccf6922c528482adfe4a03395d4b703f32a
Kacper Grosicki <kacpergrosicki2002@gmail.com>
2024-06-11 16:02:32 +0200

    task solution

---
+++ src/error.js
@@ -0,0 +1,15 @@
+/* eslint-disable max-len */
+const supportedCases = ['SNAKE', 'KEBAB', 'CAMEL', 'PASCAL', 'UPPER'];
+
+const invalidText =
+  'Text to convert is required. Correct request is: "/<TEXT_TO_CONVERT>?toCase=<CASE_NAME>".';
+const invalidToCase =
+  '"toCase" query param is required. Correct request is: "/<TEXT_TO_CONVERT>?toCase=<CASE_NAME>".';
+
+const invalidToCaseValue = `This case is not supported. Available cases: ${supportedCases.join(', ')}.`;
+
+module.exports = {
+  invalidText,
+  invalidToCase,
+  invalidToCaseValue,
+};
---
+++ src/validator.js
@@ -0,0 +1,27 @@
+const { invalidText, invalidToCase, invalidToCaseValue } = require('./error');
+
+const supportedCases = ['SNAKE', 'KEBAB', 'CAMEL', 'PASCAL', 'UPPER'];
+
+function validateInput(word, query) {
+  const errors = [];
+
+  if (!word) {
+    errors.push({
+      message: invalidText,
+    });
+  }
+
+  if (!query) {
+    errors.push({
+      message: invalidToCase,
+    });
+  } else if (!supportedCases.includes(query)) {
+    errors.push({
+      message: invalidToCaseValue,
+    });
+  }
+
+  return errors;
+}
+
+module.exports = { validateInput };
