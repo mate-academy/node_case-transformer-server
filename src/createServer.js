def0d6ac438786fb303bb8a538b8bf9411a10d87
KatsuboDmytro <dmitriyofficial03@gmaik.com>
2024-06-25 11:53:42 +0300

    created Server

--- src/createServer.js
+++ src/createServer.js
@@ -1,3 +1,65 @@
-// Write code here
-// Also, you can create additional files in the src folder
-// and import (require) them here
+const { convertToCase } = require('./convertToCase');
+const http = require('http');
+
+const createServer = () => {
+  return http.createServer(async (req, res) => {
+    try {
+      const url = new URL(req.url, `http://${req.headers.host}`);
+      const wordToCase = decodeURIComponent(url.pathname.slice(1));
+      const toCase = url.searchParams.get('toCase');
+      const error =
+        'Correct request is: "/<TEXT_TO_CONVERT>?toCase=<CASE_NAME>".';
+      const cases = ['SNAKE', 'KEBAB', 'CAMEL', 'PASCAL', 'UPPER'];
+      const casesError = `Available cases: ${cases.join(', ')}.`;
+
+      const errors = [];
+
+      if (!wordToCase) {
+        errors.push({
+          message: `Text to convert is required. ${error}`,
+        });
+      }
+
+      if (!toCase) {
+        errors.push({
+          message: `"toCase" query param is required. ${error}`,
+        });
+      }
+
+      if (toCase && !cases.includes(toCase)) {
+        errors.push({
+          message: `This case is not supported. ${casesError}`,
+        });
+      }
+
+      if (errors.length > 0) {
+        res.writeHead(400, { 'Content-Type': 'application/json' });
+        res.end(JSON.stringify({ errors }));
+
+        return;
+      }
+
+      const { originalCase, convertedText } = convertToCase(wordToCase, toCase);
+
+      res.statusCode = 200;
+      res.setHeader('Content-Type', 'application/json');
+
+      res.end(
+        JSON.stringify({
+          originalCase,
+          targetCase: toCase,
+          convertedText,
+          originalText: wordToCase,
+        }),
+      );
+    } catch (error) {
+      res.writeHead(500, { 'Content-Type': 'application/json' });
+
+      res.end(
+        JSON.stringify({ errors: [{ message: 'Internal server error' }] }),
+      );
+    }
+  });
+};
+
+module.exports = { createServer };
