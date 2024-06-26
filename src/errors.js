4807a36d4365b529a8a578a4230905e73d639e92
Allanpt <jatallantavares@gmail.com>
2024-06-26 17:06:31 -0300

    solution

---
+++ src/errors.js
@@ -0,0 +1,34 @@
+function checkError(originalText, targetCase) {
+  const validCases = ['SNAKE', 'KEBAB', 'CAMEL', 'PASCAL', 'UPPER'];
+  const errors = [];
+
+  if (!originalText) {
+    errors.push({
+      message:
+        'Text to convert is required. ' +
+        'Correct request is: "/<TEXT_TO_CONVERT>?toCase=<CASE_NAME>".',
+    });
+  }
+
+  if (!targetCase) {
+    errors.push({
+      message:
+        '"toCase" query param is required.' +
+        ' Correct request is: "/<TEXT_TO_CONVERT>?toCase=<CASE_NAME>".',
+    });
+  }
+
+  if (targetCase && !validCases.some((cas) => cas === targetCase)) {
+    errors.push({
+      message:
+        'This case is not supported. ' +
+        'Available cases: SNAKE, KEBAB, CAMEL, PASCAL, UPPER.',
+    });
+  }
+
+  return errors;
+}
+
+module.exports = {
+  checkError,
+};
