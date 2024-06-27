603591c784d62d7a7ad83e2d764c53db688da55d
katya havriuk <0303gavkat@gmail.com>
2024-06-27 13:43:34 -0600

    separate validation

--- src/createServer.js
+++ src/createServer.js
@@ -1,8 +1,6 @@
 const http = require('http');
-
 const { convertToCase } = require('./convertToCase');
-
-const CASES = ['SNAKE', 'KEBAB', 'CAMEL', 'PASCAL', 'UPPER'];
+const { validate } = require('./validation');
 
 const createServer = () => {
   return http.createServer((req, res) => {
@@ -10,31 +8,7 @@ const createServer = () => {
     const textToConvert = normalizedURL.pathname.slice(1);
     const toCase = normalizedURL.searchParams.get('toCase');
 
-    // console.log(textToConvert, toCase, 'here');
-
-    const errors = [];
-
-    if (!textToConvert) {
-      errors.push({
-        message:
-          'Text to convert is required. Correct request is: ' +
-          '"/<TEXT_TO_CONVERT>?toCase=<CASE_NAME>".',
-      });
-    }
-
-    if (!toCase) {
-      errors.push({
-        message:
-          '"toCase" query param is required. Correct request is: ' +
-          '"/<TEXT_TO_CONVERT>?toCase=<CASE_NAME>".',
-      });
-    } else if (!CASES.includes(toCase.toUpperCase())) {
-      errors.push({
-        message:
-          'This case is not supported. Available cases: ' +
-          'SNAKE, KEBAB, CAMEL, PASCAL, UPPER.',
-      });
-    }
+    const errors = validate(textToConvert, toCase);
 
     if (errors.length > 0) {
       res.writeHead(400, { 'Content-Type': 'application/json' });
