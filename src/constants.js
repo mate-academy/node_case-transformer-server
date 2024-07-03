6735583987847f6567c22b144875a95a9a4d7265
Tetiana Veremchuk <tetianaverem@gmail.com>
2024-07-03 15:22:10 +0300

    fix

---
+++ src/constants.js
@@ -0,0 +1,16 @@
+const VALID_CASES = ['SNAKE', 'KEBAB', 'CAMEL', 'PASCAL', 'UPPER'];
+const STATUS_CODES = {
+  OK: 200,
+  NOT_FOUND: 404,
+};
+const ERROR_MESSAGES = {
+  MISSING_TEXT: 'Text to convert is required. Correct request is: "/<TEXT_TO_CONVERT>?toCase=<CASE_NAME>".',
+  MISSING_TO_CASE: '"toCase" query param is required. Correct request is: "/<TEXT_TO_CONVERT>?toCase=<CASE_NAME>".',
+  INVALID_TO_CASE: `This case is not supported. Available cases: ${VALID_CASES.join(', ')}.`,
+};
+
+module.exports = {
+  VALID_CASES,
+  STATUS_CODES,
+  ERROR_MESSAGES,
+};
--- src/createServer.js
+++ src/createServer.js
@@ -1,5 +1,6 @@
 const http = require('http');
 const { convertToCase } = require('./convertToCase/convertToCase');
+const { VALID_CASES, STATUS_CODES, ERROR_MESSAGES } = require('./constants');
 
 function createServer() {
   return http.createServer((request, response) => {
@@ -9,49 +10,34 @@ function createServer() {
     const text = url.pathname.slice(1);
     const toCase = url.searchParams.get('toCase');
 
-    const validCases = ['SNAKE', 'KEBAB', 'CAMEL', 'PASCAL', 'UPPER'];
     const errors = [];
 
     if (!text) {
-      errors.push({
-        message:
-          'Text to convert is required. Correct request is: ' +
-          '"/<TEXT_TO_CONVERT>?toCase=<CASE_NAME>".',
-      });
+      errors.push({ message: ERROR_MESSAGES.MISSING_TEXT });
     }
 
     if (!toCase) {
-      errors.push({
-        message:
-          '"toCase" query param is required. Correct request is: ' +
-          '"/<TEXT_TO_CONVERT>?toCase=<CASE_NAME>".',
-      });
-    } else if (!validCases.includes(toCase)) {
-      errors.push({
-        message: `This case is not supported. Available cases: ${validCases.join(', ')}.`,
-      });
+      errors.push({ message: ERROR_MESSAGES.MISSING_TO_CASE });
+    } else if (!VALID_CASES.includes(toCase)) {
+      errors.push({ message: ERROR_MESSAGES.INVALID_TO_CASE });
     }
 
     if (errors.length > 0) {
-      response.statusCode = 404;
+      response.statusCode = STATUS_CODES.NOT_FOUND;
       response.write(JSON.stringify({ errors }));
       response.end();
-
       return;
     }
 
     const result = convertToCase(text, toCase);
 
-    response.statusCode = 200;
-
-    response.write(
-      JSON.stringify({
-        originalCase: result.originalCase,
-        targetCase: toCase,
-        originalText: text,
-        convertedText: result.convertedText,
-      }),
-    );
+    response.statusCode = STATUS_CODES.OK;
+    response.write(JSON.stringify({
+      originalCase: result.originalCase,
+      targetCase: toCase,
+      originalText: text,
+      convertedText: result.convertedText,
+    }));
     response.end();
   });
 }
