030ce5427281770cdcb2fbc4fbdfcb9044e0ec9f
Olaf <olaf.chuszno@gmail.com>
2024-07-03 19:21:53 +0200

    Fix condition nesting by declaring errors in beginning

--- src/createServer.js
+++ src/createServer.js
@@ -1,9 +1,6 @@
-/* eslint-disable no-console */
-// Write code here
-// Also, you can create additional files in the src folder
-// and import (require) them here
 const http = require('http');
 const url = require('url');
+
 const { detectCase } = require('./convertToCase/detectCase');
 const { convertToCase } = require('./convertToCase/convertToCase');
 
@@ -14,6 +11,8 @@ const createServer = () => {
     response.setHeader('Content-Type', 'application/json');
 
     const responseData = {};
+
+    responseData.errors = [];
 
     const normalizedURL = new url.URL(
       request.url,
@@ -27,37 +26,29 @@ const createServer = () => {
 
     // If no 'text to convert', set error in errors array
     if (!textToConvert.length) {
-      responseData.errors = [
-        {
-          message: `Text to convert is required. Correct request is: "/<TEXT_TO_CONVERT>?toCase=<CASE_NAME>".`,
-        },
-      ];
+      responseData.errors.push({
+        message: `Text to convert is required. Correct request is: "/<TEXT_TO_CONVERT>?toCase=<CASE_NAME>".`,
+      });
     }
 
     // If no toCase query param
     if (!searchParams.has('toCase')) {
-      // If there's no errors array, declare it
-      if (!Array.isArray(responseData.errors)) {
-        responseData.errors = [];
-      }
-
       responseData.errors.push({
         message: `"toCase" query param is required. Correct request is: "/<TEXT_TO_CONVERT>?toCase=<CASE_NAME>".`,
       });
 
       // If Case not supported -> Append error
-    } else if (!supportedCases.includes(targetCase)) {
-      if (!Array.isArray(responseData.errors)) {
-        responseData.errors = [];
-      }
-
+    } else if (targetCase === null || !supportedCases.includes(targetCase)) {
       responseData.errors.push({
         message: `This case is not supported. Available cases: SNAKE, KEBAB, CAMEL, PASCAL, UPPER.`,
       });
     }
 
     // If no errors -> Convert the text
-    if (!('errors' in responseData)) {
+    if (!responseData.errors.length) {
+      // Remove the empty errors array
+      delete responseData.errors;
+
       responseData.originalCase = detectCase(textToConvert);
 
       responseData.targetCase = targetCase;
@@ -76,19 +67,11 @@ const createServer = () => {
       response.statusMessage = 'Bad request';
     }
 
-    // {
-    //   "originalCase": "CASE_NAME",
-    //   "targetCase": "CASE_NAME",
-    //   "originalText": "ORIGINAL_TEXT",
-    //   "convertedText": "CONVERTED_TEXT"
-    // }
-
-    // response.write(`<h1>Siema</h1> \n\n\n`);
-
     response.end(JSON.stringify(responseData));
   });
 
   server.on('error', (error) => {
+    // eslint-disable-next-line no-console
     console.log(error.message);
   });
 
