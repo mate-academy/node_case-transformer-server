6207f07f9ae957ae08480586694e1a1215ad0346
Yaroslav Nazarenko <yinazarenkoo@gmail.com>
2024-07-04 21:27:21 +0200

    update solution

--- src/createServer.js
+++ src/createServer.js
@@ -1,50 +1,35 @@
 const { createServer } = require('http');
 
 const { convertToCase } = require('./convertToCase');
-const { getErrorCollector } = require('./getErrorCollector');
-const { ERROR_MESSAGE, TO_CASE_QUERY } = require('./variables');
+const { TO_CASE_QUERY } = require('./variables');
+const { validateUserData } = require('./validateUserData');
 
 module.exports.createServer = () =>
   createServer((request, response) => {
     response.setHeader('Content-Type', 'application/json');
 
-    const errorCollector = getErrorCollector();
     const url = new URL(request.url, `http://${request.headers.host}`);
     const originalText = url.pathname.slice(1);
     const targetCase = url.searchParams.get(TO_CASE_QUERY);
 
-    if (!originalText) {
-      errorCollector.catchIfThrown(() => {
-        throw new Error(ERROR_MESSAGE.TEXT_REQUIRED);
-      });
-    }
+    const errors = validateUserData(originalText, targetCase);
 
-    const convertedText = errorCollector.catchIfThrown(() => {
-      if (!targetCase) {
-        throw new Error(ERROR_MESSAGE.TO_CASE_REQUIRED);
-      }
-
-      try {
-        return convertToCase(originalText, targetCase);
-      } catch {
-        throw new Error(ERROR_MESSAGE.INVALID_TO_CASE);
-      }
-    });
-
-    const errors = errorCollector.getAll();
-
-    if (errors) {
+    if (errors.length) {
       response.writeHead(400);
       response.end(JSON.stringify({ errors }));
-    } else {
-      response.writeHead(200);
 
-      response.end(
-        JSON.stringify({
-          targetCase,
-          originalText,
-          ...convertedText,
-        }),
-      );
+      return;
     }
+
+    const convertedText = convertToCase(originalText, targetCase);
+
+    response.writeHead(200);
+
+    response.end(
+      JSON.stringify({
+        targetCase,
+        originalText,
+        ...convertedText,
+      }),
+    );
   });
--- src/variables.js
+++ src/variables.js
@@ -2,11 +2,13 @@
 
 module.exports.TO_CASE_QUERY = 'toCase';
 
+module.exports.POSSIBLE_CASES = ['SNAKE', 'KEBAB', 'CAMEL', 'PASCAL', 'UPPER'];
+
 module.exports.ERROR_MESSAGE = {
   TEXT_REQUIRED:
     'Text to convert is required. Correct request is: "/<TEXT_TO_CONVERT>?toCase=<CASE_NAME>".',
-  TO_CASE_REQUIRED:
+  CASE_REQUIRED:
     '"toCase" query param is required. Correct request is: "/<TEXT_TO_CONVERT>?toCase=<CASE_NAME>".',
-  INVALID_TO_CASE:
+  INVALID_CASE:
     'This case is not supported. Available cases: SNAKE, KEBAB, CAMEL, PASCAL, UPPER.',
 };
