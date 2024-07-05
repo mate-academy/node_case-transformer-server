768ba1d5ee5be4095c9607070ce9ee446b1d4667
volodymyr <volodiayurovych@gmail.com>
2024-07-05 21:46:49 +0200

    іщдгешщт

--- src/createServer.js
+++ src/createServer.js
@@ -31,69 +31,34 @@ function createServer() {
     const splitedReq = req.url.split('?');
     const textToConvert = splitedReq[0].slice(1);
 
-    if (!textToConvert) {
-      res.statusCode = 400;
-
-      const errorResponse = {
-        errors: [
-          {
-            message: `Text to convert is required. Correct request is: "/<TEXT_TO_CONVERT>?toCase=<CASE_NAME>".`,
-          },
-        ],
-      };
-
-      res.end(JSON.stringify(errorResponse));
-
-      return;
-    }
-
     const params = new URLSearchParams(splitedReq[1]);
     const toCase = params.get('toCase');
 
+    const errors = [];
+
+    if (!textToConvert) {
+      errors.push({
+        message: `Text to convert is required. Correct request is: "/<TEXT_TO_CONVERT>?toCase=<CASE_NAME>".`,
+      });
+    }
+
     if (!toCase) {
-      res.statusCode = 400;
-
-      const errorResponse = {
-        errors: [
-          {
-            message: `"toCase" query param is required. Correct request is: "/<TEXT_TO_CONVERT>?toCase=<CASE_NAME>".`,
-          },
-        ],
-      };
-
-      res.end(JSON.stringify(errorResponse));
-
-      return;
+      errors.push({
+        message: `"toCase" query param is required. Correct request is: "/<TEXT_TO_CONVERT>?toCase=<CASE_NAME>".`,
+      });
     }
 
-    if (!types.includes(toCase)) {
-      res.statusCode = 400;
-
-      const errorResponse = {
-        errors: [
-          {
-            message: `This case is not supported. Available cases: SNAKE, KEBAB, CAMEL, PASCAL, UPPER.`,
-          },
-        ],
-      };
-
-      res.end(JSON.stringify(errorResponse));
-
-      return;
+    if (toCase && !types.includes(toCase)) {
+      errors.push({
+        message: `This case is not supported. Available cases: SNAKE, KEBAB, CAMEL, PASCAL, UPPER.`,
+      });
     }
 
-    if (!textToConvert && !types.includes(toCase)) {
+    if (errors.length > 0) {
       res.statusCode = 400;
 
       const errorResponse = {
-        errors: [
-          {
-            message: `Text to convert is required. Correct request is: "/<TEXT_TO_CONVERT>?toCase=<CASE_NAME>".`,
-          },
-          {
-            message: `This case is not supported. Available cases: SNAKE, KEBAB, CAMEL, PASCAL, UPPER.`,
-          },
-        ],
+        errors,
       };
 
       res.end(JSON.stringify(errorResponse));
