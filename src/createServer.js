d3cac256411fd4a3017a070dd84589c41fc30af7
MacBook Pro 13 <macbookpro13@MacBook-Pro-MacBook.local>
2024-07-05 17:49:29 +0300

    add server

--- src/createServer.js
+++ src/createServer.js
@@ -1,3 +1,63 @@
-// Write code here
-// Also, you can create additional files in the src folder
-// and import (require) them here
+/* eslint-disable max-len */
+const http = require('http');
+const CASE = ['SNAKE', 'KEBAB', 'CAMEL', 'PASCAL', 'UPPER'];
+const { convertToCase } = require('./convertToCase/convertToCase');
+
+const createServer = () => {
+  const server = http.createServer((req, res) => {
+    const normalizedUrl = new URL(req.url, `http://localhost:5700`);
+    const textToConvert = normalizedUrl.pathname.slice(1);
+    const caseType = normalizedUrl.searchParams.get('toCase');
+
+    let statusCode = 200;
+    let resolve = '';
+
+    if (!textToConvert) {
+      statusCode = 400;
+
+      resolve = JSON.stringify({
+        error: 'Bad Request',
+        message:
+          'Text to convert is required. Correct request is: /<TEXT_TO_CONVERT>?toCase=<CASE_NAME>',
+      });
+    } else if (!caseType) {
+      statusCode = 400;
+
+      resolve = JSON.stringify({
+        error: 'Bad Request',
+        message:
+          'toCase query param is required. Correct request is: /<TEXT_TO_CONVERT>?toCase=<CASE_NAME>',
+      });
+    } else if (!CASE.includes(caseType)) {
+      statusCode = 400;
+
+      resolve = JSON.stringify({
+        error: 'Bad Request',
+        message:
+          'This case is not supported. Available cases: SNAKE, KEBAB, CAMEL, PASCAL, UPPER.',
+      });
+    } else {
+      const { originalCase, convertedText } = convertToCase(
+        textToConvert,
+        caseType,
+      );
+
+      resolve = JSON.stringify({
+        originalCase: originalCase,
+        targetCase: caseType,
+        originalText: textToConvert,
+        convertedText: convertedText,
+      });
+    }
+
+    res.writeHead(statusCode, { 'Content-Type': 'application/json' });
+
+    res.end(resolve);
+  });
+
+  return server;
+};
+
+module.exports = {
+  createServer,
+};
