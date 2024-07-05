ab9b5ff1301d93c6c59b891815328e0dede96f39
MacBook Pro 13 <macbookpro13@MacBook-Pro-MacBook.local>
2024-07-05 21:44:55 +0300

    add fix

--- src/createServer.js
+++ src/createServer.js
@@ -2,57 +2,54 @@
 const http = require('http');
 const CASE = ['SNAKE', 'KEBAB', 'CAMEL', 'PASCAL', 'UPPER'];
 const { convertToCase } = require('./convertToCase/convertToCase');
-
 const createServer = () => {
   const server = http.createServer((req, res) => {
     const normalizedUrl = new URL(req.url, `http://localhost:5700`);
     const textToConvert = normalizedUrl.pathname.slice(1);
     const caseType = normalizedUrl.searchParams.get('toCase');
-
-    let statusCode = 200;
-    let resolve = '';
+    const errors = [];
 
     if (!textToConvert) {
-      statusCode = 400;
-
-      resolve = JSON.stringify({
-        error: 'Bad Request',
+      errors.push({
         message:
-          'Text to convert is required. Correct request is: /<TEXT_TO_CONVERT>?toCase=<CASE_NAME>',
+          'Text to convert is required. Correct request is: "/<TEXT_TO_CONVERT>?toCase=<CASE_NAME>".',
       });
-    } else if (!caseType) {
-      statusCode = 400;
+    }
 
-      resolve = JSON.stringify({
-        error: 'Bad Request',
+    if (!caseType) {
+      errors.push({
         message:
-          'toCase query param is required. Correct request is: /<TEXT_TO_CONVERT>?toCase=<CASE_NAME>',
+          '"toCase" query param is required. Correct request is: "/<TEXT_TO_CONVERT>?toCase=<CASE_NAME>".',
       });
     } else if (!CASE.includes(caseType)) {
-      statusCode = 400;
-
-      resolve = JSON.stringify({
-        error: 'Bad Request',
+      errors.push({
         message:
           'This case is not supported. Available cases: SNAKE, KEBAB, CAMEL, PASCAL, UPPER.',
       });
-    } else {
-      const { originalCase, convertedText } = convertToCase(
-        textToConvert,
-        caseType,
-      );
+    }
 
-      resolve = JSON.stringify({
+    if (errors.length) {
+      res.writeHead(400, { 'Content-Type': 'application/json' });
+      res.end(JSON.stringify({ errors }));
+
+      return;
+    }
+
+    const { originalCase, convertedText } = convertToCase(
+      textToConvert,
+      caseType,
+    );
+
+    res.writeHead(200, { 'Content-Type': 'application/json' });
+
+    res.end(
+      JSON.stringify({
         originalCase: originalCase,
         targetCase: caseType,
         originalText: textToConvert,
         convertedText: convertedText,
-      });
-    }
-
-    res.writeHead(statusCode, { 'Content-Type': 'application/json' });
-
-    res.end(resolve);
+      }),
+    );
   });
 
   return server;
