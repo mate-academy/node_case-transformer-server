a3a96c748f7f9484c16e06ad18e947e149fca4f7
SerjDSA <serjdsa7@gmail.com>
2024-07-05 06:48:36 +0300

    add task solution

--- src/createServer.js
+++ src/createServer.js
@@ -1,3 +1,39 @@
-// Write code here
-// Also, you can create additional files in the src folder
-// and import (require) them here
+const http = require('http');
+const { convertToCase } = require('./convertToCase');
+const { parseQueryParams } = require('./parseQueryParams');
+const { validateInput } = require('./validateInput');
+
+const createServer = () => {
+  return http.createServer((req, res) => {
+    const { path, params } = parseQueryParams(req.url);
+    const text = path.slice(1);
+    const caseName = params.get('toCase');
+
+    const errors = validateInput(text, caseName);
+
+    if (errors.length > 0) {
+      res.writeHead(400, { 'Content-Type': 'application/json' });
+      res.end(JSON.stringify({ errors }));
+
+      return;
+    }
+
+    const { originalCase, convertedText } = convertToCase(
+      text,
+      caseName.toUpperCase(),
+    );
+
+    res.writeHead(200, { 'Content-Type': 'application/json' });
+
+    res.end(
+      JSON.stringify({
+        originalCase,
+        targetCase: caseName.toUpperCase(),
+        originalText: text,
+        convertedText,
+      }),
+    );
+  });
+};
+
+module.exports = { createServer };
