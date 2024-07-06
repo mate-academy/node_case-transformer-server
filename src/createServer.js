f5a3b33dec2ed2ff4943d196f7fa7db53e29e0b2
metrakon <metrakon@proton.me>
2024-07-06 15:53:49 +0300

    add_solution

--- src/createServer.js
+++ src/createServer.js
@@ -1,3 +1,65 @@
-// Write code here
-// Also, you can create additional files in the src folder
-// and import (require) them here
+const http = require('http');
+const { convertToCase } = require('./convertToCase/convertToCase');
+
+const createServer = () => {
+  const server = http.createServer((req, res) => {
+    const urlArr = req.url.split('?');
+    const queryObject = new URLSearchParams(urlArr[1]);
+    const textToConvert = urlArr[0].slice(1);
+    const caseName = queryObject.get('toCase');
+    const errors = [];
+
+    if (!textToConvert) {
+      errors.push({
+        message:
+          'Text to convert is required. ' +
+          'Correct request is: "/<TEXT_TO_CONVERT>?toCase=<CASE_NAME>".',
+      });
+    }
+
+    if (!caseName) {
+      errors.push({
+        message:
+          '"toCase" query param is required. ' +
+          'Correct request is: "/<TEXT_TO_CONVERT>?toCase=<CASE_NAME>".',
+      });
+    } else if (
+      !['SNAKE', 'KEBAB', 'CAMEL', 'PASCAL', 'UPPER'].includes(caseName)
+    ) {
+      errors.push({
+        message:
+          'This case is not supported. Available cases: ' +
+          'SNAKE, KEBAB, CAMEL, PASCAL, UPPER.',
+      });
+    }
+
+    if (errors.length > 0) {
+      res.writeHead(400, { 'Content-Type': 'application/json' });
+      res.end(JSON.stringify({ errors }));
+
+      return;
+    }
+
+    try {
+      const convertedText = convertToCase(textToConvert, caseName);
+
+      res.writeHead(200, { 'Content-Type': 'application/json' });
+
+      res.end(
+        JSON.stringify({
+          originalCase: convertedText.originalCase,
+          targetCase: caseName,
+          originalText: textToConvert,
+          convertedText: convertedText.convertedText,
+        }),
+      );
+    } catch (error) {
+      res.writeHead(400, { 'Content-Type': 'application/json' });
+      res.end(JSON.stringify({ errors: [{ message: error.message }] }));
+    }
+  });
+
+  return server;
+};
+
+module.exports = { createServer };
