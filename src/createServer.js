94001e5e43dcf809a75a6625497d5d8d421e6511
metrakon <metrakon@proton.me>
2024-07-08 12:03:01 +0300

    fix

--- src/createServer.js
+++ src/createServer.js
@@ -7,34 +7,41 @@ const createServer = () => {
     const queryObject = new URLSearchParams(urlArr[1]);
     const textToConvert = urlArr[0].slice(1);
     const caseName = queryObject.get('toCase');
+
+    const HTTP_OK = 200;
+    const HTTP_BAD_REQUEST = 400;
+
+    const SUPPORTED_CASES = ['SNAKE', 'KEBAB', 'CAMEL', 'PASCAL', 'UPPER'];
+    const REQUIRED_TEXT_MESSAGE =
+      'Text to convert is required. Correct request is: ' +
+      '"/<TEXT_TO_CONVERT>?toCase=<CASE_NAME>".';
+    const REQUIRED_CASE_MESSAGE =
+      '"toCase" query param is required. Correct request is: ' +
+      '"/<TEXT_TO_CONVERT>?toCase=<CASE_NAME>".';
+    const UNSUPPORTED_CASE_MESSAGE =
+      'This case is not supported. Available cases: ' +
+      'SNAKE, KEBAB, CAMEL, PASCAL, UPPER.';
+
     const errors = [];
 
     if (!textToConvert) {
       errors.push({
-        message:
-          'Text to convert is required. ' +
-          'Correct request is: "/<TEXT_TO_CONVERT>?toCase=<CASE_NAME>".',
+        message: REQUIRED_TEXT_MESSAGE,
       });
     }
 
     if (!caseName) {
       errors.push({
-        message:
-          '"toCase" query param is required. ' +
-          'Correct request is: "/<TEXT_TO_CONVERT>?toCase=<CASE_NAME>".',
+        message: REQUIRED_CASE_MESSAGE,
       });
-    } else if (
-      !['SNAKE', 'KEBAB', 'CAMEL', 'PASCAL', 'UPPER'].includes(caseName)
-    ) {
+    } else if (!SUPPORTED_CASES.includes(caseName)) {
       errors.push({
-        message:
-          'This case is not supported. Available cases: ' +
-          'SNAKE, KEBAB, CAMEL, PASCAL, UPPER.',
+        message: UNSUPPORTED_CASE_MESSAGE,
       });
     }
 
     if (errors.length > 0) {
-      res.writeHead(400, { 'Content-Type': 'application/json' });
+      res.writeHead(HTTP_BAD_REQUEST, { 'Content-Type': 'application/json' });
       res.end(JSON.stringify({ errors }));
 
       return;
@@ -43,7 +50,7 @@ const createServer = () => {
     try {
       const convertedText = convertToCase(textToConvert, caseName);
 
-      res.writeHead(200, { 'Content-Type': 'application/json' });
+      res.writeHead(HTTP_OK, { 'Content-Type': 'application/json' });
 
       res.end(
         JSON.stringify({
@@ -54,7 +61,7 @@ const createServer = () => {
         }),
       );
     } catch (error) {
-      res.writeHead(400, { 'Content-Type': 'application/json' });
+      res.writeHead(HTTP_BAD_REQUEST, { 'Content-Type': 'application/json' });
       res.end(JSON.stringify({ errors: [{ message: error.message }] }));
     }
   });
