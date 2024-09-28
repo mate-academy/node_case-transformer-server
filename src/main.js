9431c167e5769b0b80e84a3ae8def231766c9e4f
Teollan <serhiikofficial@gmail.com>
2024-09-28 20:27:55 +0300

    removed comments

--- src/main.js
+++ src/main.js
@@ -1,32 +1,6 @@
-// const http = require('http');
 const { createServer } = require('./createServer');
 
 createServer().listen(5700, () => {
   // eslint-disable-next-line no-console
   console.log('Server started! 🚀');
 });
-
-// const options = {
-//   hostname: 'localhost',
-//   port: 5700,
-//   path: '/',
-//   method: 'GET',
-// };
-
-// const req = http.request(options, (res) => {
-//   let data = '';
-
-//   res.on('data', (chunk) => {
-//     data += chunk;
-//   });
-
-//   res.on('end', () => {
-//     console.log('Response:', data);
-//   });
-// });
-
-// req.on('error', (error) => {
-//   console.error('Error:', error);
-// });
-
-// req.end();
