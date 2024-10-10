de5a9f4381369090dd582040f46938ec28406113
Pawel Nowicki <pawel.nowicki87@tlen.pl>
2024-10-10 11:43:07 +0200

    add task

--- src/createServer.js
+++ src/createServer.js
@@ -1,64 +1,109 @@
+/* eslint-disable max-len */
 const http = require('http');
 const { convertToCase } = require('./convertToCase');
 
-function createServer() {
-  const server = http.createServer((req, res) => {
-    const url = new URL(req.url, `http://${req.headers.host}`);
-    const originalText = url.pathname.slice(1);
-    const targetCase = url.searchParams.get('toCase');
-    const errors = [];
+function validateRequest(originalText, targetCase) {
+  const errors = [];
 
-    res.setHeader('Content-Type', 'application/json');
+  if (!originalText) {
+    errors.push({
+      message:
+        'Text to convert is required. Correct request is: "/<TEXT_TO_CONVERT>?toCase=<CASE_NAME>".',
+    });
+  }
 
-    if (!originalText) {
-      errors.push({
-        message:
-          // eslint-disable-next-line max-len
-          'Text to convert is required. Correct request is: "/<TEXT_TO_CONVERT>?toCase=<CASE_NAME>".',
-      });
-    }
+  if (!targetCase) {
+    errors.push({
+      message:
+        '"toCase" query param is required. Correct request is: "/<TEXT_TO_CONVERT>?toCase=<CASE_NAME>".',
+    });
+  } else if (
+    !['SNAKE', 'KEBAB', 'CAMEL', 'PASCAL', 'UPPER'].includes(targetCase)
+  ) {
+    errors.push({
+      message:
+        'This case is not supported. Available cases: SNAKE, KEBAB, CAMEL, PASCAL, UPPER.',
+    });
+  }
 
-    if (!targetCase) {
-      errors.push({
-        message:
-          // eslint-disable-next-line max-len
-          '"toCase" query param is required. Correct request is: "/<TEXT_TO_CONVERT>?toCase=<CASE_NAME>".',
-      });
-    } else if (
-      !['SNAKE', 'KEBAB', 'CAMEL', 'PASCAL', 'UPPER'].includes(targetCase)
-    ) {
-      errors.push({
-        message:
-          // eslint-disable-next-line max-len
-          'This case is not supported. Available cases: SNAKE, KEBAB, CAMEL, PASCAL, UPPER.',
-      });
-    }
+  return errors;
+}
 
-    if (errors.length) {
-      res.statusCode = 400;
-      res.statusMessage = 'Bad request';
-      res.end(JSON.stringify({ errors }));
+function parseUrl(req) {
+  const url = new URL(req.url, `http://${req.headers.host}`);
+  const originalText = url.pathname.slice(1);
+  const targetCase = url.searchParams.get('toCase');
 
-      return;
-    }
+  return { originalText, targetCase };
+}
 
+function sendErrorResponse(res, errors) {
+  res.statusCode = 400;
+  res.statusMessage = 'Bad request';
+  res.end(JSON.stringify({ errors }));
+}
+
+function sendSuccessResponse(
+  res,
+  originalText,
+  originalCase,
+  targetCase,
+  convertedText,
+) {
+  res.statusCode = 200;
+  res.statusMessage = 'OK';
+
+  res.end(
+    JSON.stringify({
+      originalCase,
+      targetCase,
+      originalText,
+      convertedText,
+    }),
+  );
+}
+
+function handleRequest(req, res) {
+  res.setHeader('Content-Type', 'application/json');
+
+  const { originalText, targetCase } = parseUrl(req);
+  const errors = validateRequest(originalText, targetCase);
+
+  if (errors.length) {
+    sendErrorResponse(res, errors);
+
+    return;
+  }
+
+  try {
     const { originalCase, convertedText } = convertToCase(
       originalText,
       targetCase,
     );
 
-    res.statusCode = 200;
-    res.statusMessage = 'OK';
+    sendSuccessResponse(
+      res,
+      originalText,
+      originalCase,
+      targetCase,
+      convertedText,
+    );
+  } catch (error) {
+    res.statusCode = 500;
+    res.statusMessage = 'Internal Server Error';
 
     res.end(
       JSON.stringify({
-        originalCase,
-        targetCase,
-        originalText,
-        convertedText,
+        message: 'An error occurred during text conversion.',
+        error: error.message,
       }),
     );
-  });
+  }
+}
+
+// Funkcja tworząca serwer
+function createServer() {
+  const server = http.createServer(handleRequest);
 
   return server;
 }
