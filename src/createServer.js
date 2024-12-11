85fc99e4c2a1ec1fad3ae0d13dc29aeaca0452e1
Dmytro <fridrifd3554@gmail.com>
2024-12-11 22:16:57 +0200

    few updates

--- src/createServer.js
+++ src/createServer.js
@@ -1,39 +1,61 @@
 const http = require('http');
-const { convertToCase } = require('./convertToCase/convertToCase');
-const { errorHandler } = require('./errorHandler');
+const { convertToCase } = require('./convertToCase');
+const cases = ['SNAKE', 'KEBAB', 'CAMEL', 'PASCAL', 'UPPER'];
 
 function createServer() {
-  const server = http.createServer(async (req, res) => {
-    res.setHeader('Content-type', 'application/json');
+  const server = http.createServer((req, res) => {
+    res.setHeader('Content-Type', 'application/json');
 
-    const normalizedURL = new URL(req.url, 'http://localhost:8080');
+    const [str, queryString] = req.url.split('?');
+    const toCase = new URLSearchParams(queryString).get('toCase');
 
-    const originalText = normalizedURL.pathname.slice(1);
-    const targetCase = normalizedURL.searchParams.get('toCase');
+    const validation = {
+      errors: [],
+    };
 
     try {
-      if (!originalText) {
-        throw new Error('Text should not be null');
+      const text = str.slice(1);
+
+      if (!text) {
+        validation.errors.push({
+          message:
+            'Text to convert is required.' +
+            ' Correct request is: "/<TEXT_TO_CONVERT>?toCase=<CASE_NAME>".',
+        });
       }
 
-      const convertedText = await convertToCase(originalText, targetCase);
+      if (!toCase) {
+        validation.errors.push({
+          message:
+            '"toCase" query param is required.' +
+            ' Correct request is: "/<TEXT_TO_CONVERT>?toCase=<CASE_NAME>".',
+        });
+      } else if (!cases.includes(toCase)) {
+        validation.errors.push({
+          message:
+            'This case is not supported.' +
+            ' Available cases: SNAKE, KEBAB, ' +
+            'CAMEL, PASCAL, UPPER.',
+        });
+      }
+
+      if (validation.errors.length > 0) {
+        res.statusCode = 400;
+        res.end(JSON.stringify(validation));
+
+        return;
+      }
+
+      const result = convertToCase(text, toCase);
+
+      res.statusCode = 200;
 
       res.end(
-        JSON.stringify({
-          originalCase: convertedText.originalCase,
-          targetCase,
-          originalText,
-          convertedText: convertedText.convertedText,
-        }),
+        JSON.stringify({ ...result, originalText: text, targetCase: toCase }),
       );
     } catch (error) {
-      res.writeHead(400, { 'Content-Type': 'application/json' });
-
-      res.end(
-        JSON.stringify({
-          errors: errorHandler(originalText, targetCase),
-        }),
-      );
+      res.statusCode = 500;
+      res.end(JSON.stringify({ error: 'Invalid JSON' }));
     }
   });
 
