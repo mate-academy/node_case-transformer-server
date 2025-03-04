f301d18e99d148038ec8141407f023f32f930875
Andrii Rykal <andreuka79@gmail.com>
2025-03-04 17:20:06 +0200

    Solution

--- src/createServer.js
+++ src/createServer.js
@@ -1,3 +1,64 @@
 // Write code here
 // Also, you can create additional files in the src folder
 // and import (require) them here
+
+const http = require('http');
+const { convertToCase } = require('./convertToCase');
+
+const SUPPORTED_CASES = ['SNAKE', 'KEBAB', 'CAMEL', 'PASCAL', 'UPPER'];
+
+function createServer() {
+  return http.createServer((req, res) => {
+    const baseUrl = `http://${req.headers.host}`;
+    const parsedUrl = new URL(req.url, baseUrl);
+    const pathParts = parsedUrl.pathname.split('/').filter(Boolean);
+    const toCase = parsedUrl.searchParams.get('toCase');
+    const errors = [];
+
+    if (pathParts.length === 0) {
+      errors.push({
+        message:
+          'Text to convert is required. Correct request is: "/<TEXT_TO_CONVERT>?toCase=<CASE_NAME>".',
+      });
+    }
+
+    if (!toCase) {
+      errors.push({
+        message:
+          '"toCase" query param is required. Correct request is: "/<TEXT_TO_CONVERT>?toCase=<CASE_NAME>".',
+      });
+    } else if (!SUPPORTED_CASES.includes(toCase)) {
+      errors.push({
+        message:
+          'This case is not supported. Available cases: SNAKE, KEBAB, CAMEL, PASCAL, UPPER.',
+      });
+    }
+
+    if (errors.length > 0) {
+      res.writeHead(400, {
+        'Content-Type': 'application/json',
+        Status: '400 Bad Request',
+      });
+      res.end(JSON.stringify({ errors }));
+      return;
+    }
+
+    const text = pathParts.join('/');
+    const result = convertToCase(text, toCase);
+
+    const responseBody = {
+      originalCase: result.originalCase,
+      targetCase: toCase,
+      originalText: text,
+      convertedText: result.convertedText,
+    };
+
+    res.writeHead(200, {
+      'Content-Type': 'application/json',
+      Status: '200 OK',
+    });
+    res.end(JSON.stringify(responseBody));
+  });
+}
+
+module.exports = { createServer };
