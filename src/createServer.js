2bf321c8e72779bf73c7c6e3951bd7b70772bbe8
Andrii Rykal <andreuka79@gmail.com>
2025-03-04 17:24:54 +0200

    Solution

--- src/createServer.js
+++ src/createServer.js
@@ -18,19 +18,22 @@ function createServer() {
     if (pathParts.length === 0) {
       errors.push({
         message:
-          'Text to convert is required. Correct request is: "/<TEXT_TO_CONVERT>?toCase=<CASE_NAME>".',
+          'Text to convert is required. Correct request is: '
+          + '"/<TEXT_TO_CONVERT>?toCase=<CASE_NAME>".',
       });
     }
 
     if (!toCase) {
       errors.push({
         message:
-          '"toCase" query param is required. Correct request is: "/<TEXT_TO_CONVERT>?toCase=<CASE_NAME>".',
+          '"toCase" query param is required. Correct request is: '
+          + '"/<TEXT_TO_CONVERT>?toCase=<CASE_NAME>".',
       });
     } else if (!SUPPORTED_CASES.includes(toCase)) {
       errors.push({
         message:
-          'This case is not supported. Available cases: SNAKE, KEBAB, CAMEL, PASCAL, UPPER.',
+          'This case is not supported. Available cases: '
+          + 'SNAKE, KEBAB, CAMEL, PASCAL, UPPER.',
       });
     }
 
