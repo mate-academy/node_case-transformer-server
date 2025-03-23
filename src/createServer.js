bc3969db83f8ef2ad4d917ed90368a9eb10da750
Yuliia <shevchuuchkaaa@gmail.com>
2025-03-23 11:31:19 +0100

    to check

--- src/createServer.js
+++ src/createServer.js
@@ -1,3 +1,53 @@
-// Write code here
-// Also, you can create additional files in the src folder
-// and import (require) them here
+/* eslint-disable no-console */
+
+const http = require('node:http');
+
+const { convertToCase } = require('./convertToCase/convertToCase');
+const { checkErrors } = require('./checkErrors');
+
+function newServer() {
+  const server = http.createServer((req, res) => {
+    const url = req.url;
+
+    if (url === '/favicon.ico') {
+      return res.end();
+    }
+
+    const normalisedUrl = new URL(url, `http://${req.headers.host}`);
+
+    const text = normalisedUrl.pathname.slice(1);
+    const toCase = normalisedUrl.searchParams.get('toCase');
+
+    console.log(text, toCase);
+
+    const errors = checkErrors(text, toCase);
+
+    if (errors.errors.length > 0) {
+      res.statusCode = 400;
+      res.statusMessage = 'Bad request';
+
+      console.log(res.statusCode);
+      res.end(JSON.stringify(errors));
+    } else {
+      const result = convertToCase(text, toCase);
+
+      const responseBody = {
+        originalCase: result.originalCase,
+        targetCase: toCase,
+        originalText: text,
+        convertedText: result.convertedText,
+      };
+
+      res.statusCode = 200;
+      res.end(JSON.stringify(responseBody));
+    }
+  });
+
+  return server;
+}
+
+// newServer();
+
+module.exports = {
+  newServer,
+};
--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
-const { createServer } = require('./createServer');
+const { newServer } = require('./createServer');
+// const { createServer } = require('./createServer');
 
-createServer().listen(5700, () => {
+newServer().listen(5700, () => {
   // eslint-disable-next-line no-console
   console.log('Server started! 🚀');
 });
