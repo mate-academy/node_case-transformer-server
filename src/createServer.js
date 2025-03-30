40acdbd87d8b5774238e01f1c122cadc0d78f73b
Bohdan Ivankov <bodanich600@gmail.com>
2025-03-30 12:42:33 +0200

    Solution

--- src/createServer.js
+++ src/createServer.js
@@ -1,3 +1,81 @@
-// Write code here
-// Also, you can create additional files in the src folder
-// and import (require) them here
+const http = require('node:http');
+const { convertToCase } = require('./convertToCase');
+
+const PORT = process.env.PORT || 3000;
+const baseUrl = `http://localhost:${PORT}`;
+
+const availableCases = ['SNAKE', 'KEBAB', 'CAMEL', 'PASCAL', 'UPPER'];
+
+function createServer() {
+  return http.createServer((req, res) => {
+    const normilizedUrl = new URL(req.url, baseUrl);
+    const textToConvert = normilizedUrl.pathname.slice(1);
+    const caseName = normilizedUrl.searchParams.get('toCase');
+
+    res.setHeader('Content-Type', 'application/json');
+
+    const errors = [];
+
+    if (!textToConvert) {
+      errors.push({
+        message:
+          // eslint-disable-next-line max-len
+          'Text to convert is required. Correct request is: "/<TEXT_TO_CONVERT>?toCase=<CASE_NAME>".',
+      });
+    }
+
+    if (!caseName) {
+      errors.push({
+        message:
+          // eslint-disable-next-line max-len
+          '"toCase" query param is required. Correct request is: "/<TEXT_TO_CONVERT>?toCase=<CASE_NAME>".',
+      });
+    }
+
+    if (caseName && !availableCases.includes(caseName.toUpperCase())) {
+      errors.push({
+        message:
+          // eslint-disable-next-line max-len
+          'This case is not supported. Available cases: SNAKE, KEBAB, CAMEL, PASCAL, UPPER.',
+      });
+    }
+
+    if (errors.length > 0) {
+      res.writeHead(400, {
+        'Content-Type': 'application/json',
+        statusText: 'Bad request',
+      });
+      res.end(JSON.stringify({ errors }));
+
+      return;
+    }
+
+    try {
+      const result = convertToCase(textToConvert, caseName);
+
+      res.writeHead(200, {
+        'Content-Type': 'application/json',
+        statusText: 'OK',
+      });
+
+      res.end(
+        JSON.stringify({
+          originalCase: result.originalCase,
+          targetCase: caseName.toUpperCase(),
+          originalText: textToConvert,
+          convertedText: result.convertedText,
+        }),
+      );
+    } catch (error) {
+      // eslint-disable-next-line no-console
+      console.log(error);
+      res.statusCode = 500;
+
+      res.end(
+        JSON.stringify({ errors: [{ message: 'Internal Server Error' }] }),
+      );
+    }
+  });
+}
+
+module.exports = { createServer };
