fb41eb5f4363921ff5aa19bbb0d15b196a8f1b77
Maria <m.gosp1704@gmail.com>
2025-04-09 16:10:26 +0300

    solution2

--- src/validateData.js
+++ src/validateData.js
@@ -2,24 +2,25 @@ function validateData(textToChange, toCase) {
   const errors = [];
 
   if (!textToChange) {
-    errors.push(
-      'Text to convert is required.' +
-      'Correct request is: "/<TEXT_TO_CONVERT>?toCase=<CASE_NAME>".',
-    );
+    errors.push({
+      message:
+        // eslint-disable-next-line max-len
+        'Text to convert is required. Correct request is: "/<TEXT_TO_CONVERT>?toCase=<CASE_NAME>".',
+    });
   }
 
   if (!toCase) {
-    errors.push(
-      '"toCase" query param is required.' +
-      'Correct request is: "/<TEXT_TO_CONVERT>?toCase=<CASE_NAME>".',
-    );
-  }
-
-  if (!['SNAKE', 'KEBAB', 'CAMEL', 'PASCAL', 'UPPER'].includes(toCase)) {
-    errors.push(
-      'This case is not supported.' +
-      'Available cases: SNAKE, KEBAB, CAMEL, PASCAL, UPPER.',
-    );
+    errors.push({
+      message:
+        // eslint-disable-next-line max-len
+        '"toCase" query param is required. Correct request is: "/<TEXT_TO_CONVERT>?toCase=<CASE_NAME>".',
+    });
+  } else if (!['SNAKE', 'KEBAB', 'CAMEL', 'PASCAL', 'UPPER'].includes(toCase)) {
+    errors.push({
+      message:
+        // eslint-disable-next-line max-len
+        'This case is not supported. Available cases: SNAKE, KEBAB, CAMEL, PASCAL, UPPER.',
+    });
   }
 
   return errors;
