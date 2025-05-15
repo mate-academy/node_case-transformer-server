cc88680fd79823fc99a9a79ae09a556a502fe806
Csillag61 <csillagok@gmail.com>
2025-05-15 13:16:09 -0400

    server

--- src/createServer.js
+++ src/createServer.js
@@ -1,3 +1,62 @@
-// Write code here
-// Also, you can create additional files in the src folder
-// and import (require) them here
+/* eslint-disable max-len */
+/* eslint-disable no-unused-vars */
+
+const http = require('http');
+const url = require('url');
+const { convertToCase } = require('./convertToCase');
+const { parse } = require('path');
+
+const SUPPORTED_CASES = new Set(['SNAKE', 'KEBAB', 'CAMEL', 'PASCAL', 'UPPER']);
+
+function createServer() {
+  return http.createServer((req, res) => {
+    res.setHeader('Content-Type', 'application/json');
+
+    const parsedUrl = url.URL(req.url, true);
+    const pathname = parsedUrl.pathname.slice(1);
+    const targetCase = parsedUrl.query.toCase;
+
+    const errors = [];
+
+    if (!pathname) {
+      errors.push({
+        message:
+          'Text to convert is required. Correct request is: "/<TEXT_TO_CONVERT>?toCase=<CASE_NAME>".',
+      });
+    }
+
+    if (!targetCase) {
+      errors.push({
+        message:
+          '"toCase" query param is required. Correct request is: "/<TEXT_TO_CONVERT>?toCase=<CASE_NAME>".',
+      });
+    } else if (!SUPPORTED_CASES.has(targetCase)) {
+      errors.push({
+        message:
+          'This case is not supported. Available cases: SNAKE, KEBAB, CAMEL, PASCAL, UPPER.',
+      });
+    }
+
+    if (errors.length > 0) {
+      res.statusCode = 400;
+
+      return res.end(JSON.stringify({ errors }));
+    }
+
+    // Convert text
+    const result = convertToCase(targetCase, pathname);
+
+    res.statusCode = 200;
+
+    res.end(
+      JSON.stringify({
+        originalCase: result.originalCase,
+        targetCase: targetCase,
+        originalText: pathname,
+        convertedText: result.convertedText,
+      }),
+    );
+  });
+}
+
+module.exports = { createServer };
