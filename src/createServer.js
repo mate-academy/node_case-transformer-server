48a65652263517de2ee296f24795aa64d8635368
AVyhaa <artemvyhovanec1@gmail.com>
2025-05-22 12:31:16 +0300

    solution

--- src/createServer.js
+++ src/createServer.js
@@ -1,3 +1,23 @@
-// Write code here
-// Also, you can create additional files in the src folder
-// and import (require) them here
+const http = require('http');
+
+const { determineResponse } = require('./determineResponse');
+
+const ORIGIN = 'http://localhost:5700';
+
+function createServer() {
+  return http.createServer((req, res) => {
+    const parsedUrl = new URL(ORIGIN + req.url);
+
+    const textToConvert = parsedUrl.pathname.slice(1);
+    const convertToFormat = parsedUrl.searchParams.get('toCase');
+
+    res.setHeader('Content-Type', 'application/json');
+
+    const responseArgs = determineResponse(textToConvert, convertToFormat);
+
+    res.statusCode = responseArgs.statusCode;
+    res.end(JSON.stringify(responseArgs.response));
+  });
+}
+
+module.exports = { createServer };
