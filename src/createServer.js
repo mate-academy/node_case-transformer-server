ec2191b6f32c92d49e535b4717ec6bdf9068181c
anastasia22422 <anastasia.skripka.sao@gmail.com>
2025-08-04 21:22:37 +0200

    Update createServer.js

--- src/createServer.js
+++ src/createServer.js
@@ -1,3 +1,57 @@
-// Write code here
-// Also, you can create additional files in the src folder
-// and import (require) them here
+import http from 'http';
+import convertToCase from './convertToCase';
+
+function createServer() {
+  const server = http.createServer((req, res) => {
+    const normalUrl = new URL(req.url, 'http://localhost');
+    const toCaseType = normalUrl.searchParams.get('toCase');
+    const textToConvert = normalUrl.pathname.slice(1);
+    const allTypes = ['SNAKE', 'KEBAB', 'CAMEL', 'PASCAL', 'UPPER'];
+    const errors = [];
+
+    if (!textToConvert) {
+      errors.push({
+        message:
+          // eslint-disable-next-line max-len
+          'Text to convert is required. Correct request is: "/<TEXT_TO_CONVERT>?toCase=<CASE_NAME>".',
+      });
+    } else if (!toCaseType) {
+      errors.push({
+        message:
+          // eslint-disable-next-line max-len
+          '"toCase" query param is required. Correct request is: "/<TEXT_TO_CONVERT>?toCase=<CASE_NAME>".',
+      });
+    } else if (!allTypes.includes(toCaseType)) {
+      errors.push({
+        message:
+          // eslint-disable-next-line max-len
+          'This case is not supported. Available cases: SNAKE, KEBAB, CAMEL, PASCAL, UPPER.',
+      });
+    }
+
+    if (errors.length > 0) {
+      res.writeHead(400, { 'Content-Type': 'application/json' });
+      res.end(JSON.stringify({ errors }));
+
+      return;
+    }
+
+    const result = convertToCase(toCaseType, textToConvert);
+
+    res.writeHead(200, { 'Content-Type': 'application/json' });
+
+    res.end(
+      JSON.stringify({
+        ...result,
+        targetCase: toCaseType,
+        originalText: textToConvert,
+      }),
+    );
+  });
+
+  return server;
+}
+
+module.exports = {
+  createServer,
+};
