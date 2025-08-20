76e69f0afbd26d21e1d6f54bf7c819119f607e36
Gregory Korsun <lvlup9119@gmail.com>
2025-08-20 13:42:44 +0300

    Cut long lines

--- src/createServer.js
+++ src/createServer.js
@@ -1,3 +1,73 @@
-// Write code here
-// Also, you can create additional files in the src folder
-// and import (require) them here
+const http = require('http');
+const { convertToCase } = require('./convertToCase/convertToCase.js');
+
+const createServer = () => {
+  const server = http.createServer((req, res) => {
+    if (req.url === '/favicon.ico') {
+      res.writeHead(204, { 'Content-Type': 'image/x-icon' });
+      res.end();
+
+      return;
+    }
+
+    res.setHeader('Content-Type', 'application/json');
+    res.statusCode = 200;
+    res.statusMessage = 'OK';
+
+    const errors = [];
+    const VALID_TARGET_CASES = ['SNAKE', 'KEBAB', 'CAMEL', 'PASCAL', 'UPPER'];
+
+    const url = new URL(req.url || '', `http://${req.headers.host}`);
+    const originalText = url.pathname.slice(1);
+    const targetCase = url.searchParams.get('toCase');
+
+    if (!originalText) {
+      errors.push({
+        message:
+          'Text to convert is required. Correct request' +
+          'is: "/<TEXT_TO_CONVERT>?toCase=<CASE_NAME>".',
+      });
+    }
+
+    if (!targetCase) {
+      errors.push({
+        message:
+          '"toCase" query param is required. Correct request' +
+          'is: "/<TEXT_TO_CONVERT>?toCase=<CASE_NAME>".',
+      });
+    }
+
+    if (!VALID_TARGET_CASES.includes(targetCase)) {
+      errors.push({
+        message:
+          'This case is not supported. Available cases:' +
+          ' SNAKE, KEBAB, CAMEL, PASCAL, UPPER.',
+      });
+    }
+
+    if (errors.length > 0) {
+      res.statusCode = 400;
+      res.statusMessage = 'Bad Request';
+      res.end(JSON.stringify({ errors }));
+
+      return;
+    }
+
+    const transformText = convertToCase(originalText, targetCase);
+
+    res.end(
+      JSON.stringify({
+        originalCase: transformText.originalCase,
+        targetCase: targetCase,
+        originalText: originalText,
+        convertedText: transformText.convertedText,
+      }),
+    );
+  });
+
+  return server;
+};
+
+module.exports = {
+  createServer,
+};
--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-const { createServer } = require('./createServer');
+const { createServer } = require('./createServer.js');
 
 createServer().listen(5700, () => {
   // eslint-disable-next-line no-console
