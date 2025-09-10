63d676e57ac000b141706e311c0111c8081ad650
Vitalii <dumavb777@gmail.com>
2025-09-10 22:12:55 +0300

    solution

--- src/createServer.js
+++ src/createServer.js
@@ -1,3 +1,52 @@
-// Write code here
-// Also, you can create additional files in the src folder
-// and import (require) them here
+const http = require('http');
+const convertToCase = require('./convertToCase').convertToCase;
+const errorHandler = require('./convertToCase/errorHandler').errorHandler;
+
+function createServer() {
+  const PORT = process.env.PORT || 3000;
+
+  const server = http.createServer((req, res) => {
+    if (req.method !== 'GET') {
+      res.statusCode = 404;
+      res.end('Not Found');
+
+      return;
+    }
+
+    res.setHeader('Content-Type', 'application/json');
+
+    const url = new URL(req.url || '', `http://${req.headers.host}`);
+    const requestedPath = url.pathname?.slice(1);
+    const toCase = url.searchParams.get('toCase');
+
+    const errors = errorHandler(requestedPath, toCase);
+
+    if (errors) {
+      res.statusCode = 400;
+      res.end(JSON.stringify({ errors }));
+
+      return;
+    }
+
+    const result = convertToCase(requestedPath, toCase);
+
+    res.statusCode = 200;
+
+    res.end(
+      JSON.stringify({
+        convertedText: result.convertedText,
+        originalCase: result.originalCase,
+        originalText: requestedPath,
+        targetCase: toCase,
+      }),
+    );
+  });
+
+  server.listen(PORT, () => {});
+
+  return server;
+}
+
+module.exports = {
+  createServer,
+};
