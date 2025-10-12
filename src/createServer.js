9710d2d74f63f023d0c8afac22df26797b34b476
keygen_exe <matkovskiyf3vlad@gmail.com>
2025-10-13 00:19:38 +0300

    sol

--- src/createServer.js
+++ src/createServer.js
@@ -1,3 +1,61 @@
-// Write code here
-// Also, you can create additional files in the src folder
-// and import (require) them here
+const http = require('node:http');
+const { convertToCase } = require('./convertToCase/index.js');
+
+const possibleTypes = ['SNAKE', 'KEBAB', 'CAMEL', 'PASCAL', 'UPPER'];
+
+const createServer = () =>
+  http.createServer((req, res) => {
+    const normalizedURL = new URL(req.url, `http://${req.headers.host}`);
+
+    const text = (normalizedURL.pathname ?? '').slice(1);
+    const toCase = normalizedURL.searchParams.get('toCase');
+
+    const errors = [];
+
+    if (text === '') {
+      errors.push({
+        message:
+          'Text to convert is required. Correct request is: ' +
+          '"/<TEXT_TO_CONVERT>?toCase=<CASE_NAME>".',
+      });
+    }
+
+    if (!toCase) {
+      errors.push({
+        message:
+          '"toCase" query param is required. ' +
+          'Correct request is: "/<TEXT_TO_CONVERT>?toCase=<CASE_NAME>".',
+      });
+    }
+
+    if (toCase && !possibleTypes.includes(toCase)) {
+      errors.push({
+        message:
+          'This case is not supported. ' +
+          'Available cases: SNAKE, KEBAB, CAMEL, PASCAL, UPPER.',
+      });
+    }
+
+    if (errors.length > 0) {
+      res.writeHead(400, 'Bad request', { 'Content-Type': 'application/json' });
+      res.end(JSON.stringify({ errors }));
+
+      return;
+    }
+
+    const result = convertToCase(text, toCase);
+
+    const response = {
+      originalCase: result.originalCase,
+      targetCase: toCase,
+      originalText: text,
+      convertedText: result.convertedText,
+    };
+
+    res.writeHead(200, 'OK', { 'Content-Type': 'application/json' });
+    res.end(JSON.stringify(response));
+  });
+
+module.exports = {
+  createServer,
+};
