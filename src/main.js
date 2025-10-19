f634d9feddeeb9aecec2f7cba409b545524b546e
ArtemHryb <artem.mushroom.gh@gmail.com>
2025-10-19 14:59:29 +0300

    add solution

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-const { createServer } = require('./createServer');
+const { createServer } = require('../src/createServer');
 
 createServer().listen(5700, () => {
   // eslint-disable-next-line no-console
