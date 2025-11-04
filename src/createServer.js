1415c66814ab7fe15974ca51cf91cbbc5fe14a9e
Isaque Pereira dos Santos <isaquevidaltube@gmail.com>
2025-11-04 13:54:17 -0300

    task node_case-transformer-server

--- src/createServer.js
+++ src/createServer.js
@@ -1,8 +1,8 @@
 // Write code here
 // Also, you can create additional files in the src folder
 // and import (require) them here
-import http from 'http';
-import { convertToCase } from './convertToCase/convertToCase';
+const http = require('http');
+const { convertToCase } = require('./convertToCase/convertToCase');
 
 function createServer() {
   const server = http.createServer((req, res) => {
@@ -11,25 +11,47 @@ function createServer() {
     const text = url.pathname.slice(1);
     const caseName = url.searchParams.get('toCase');
 
-    if (!text || !caseName) {
+    const errors = [];
+
+    if (!text) {
+      errors.push({
+        message: 'Text to convert is required. Correct request is: "/<TEXT_TO_CONVERT>?toCase=<CASE_NAME>".', // eslint-disable-line max-len
+      });
+    }
+
+    if (!caseName) {
+      errors.push({
+        message: '"toCase" query param is required. Correct request is: "/<TEXT_TO_CONVERT>?toCase=<CASE_NAME>".', // eslint-disable-line max-len
+      });
+    }
+
+    if (!['SNAKE', 'KEBAB', 'CAMEL', 'PASCAL', 'UPPER'].includes(caseName)) {
+      errors.push({
+        message: 'This case is not supported. Available cases: SNAKE, KEBAB, CAMEL, PASCAL, UPPER.', // eslint-disable-line max-len
+      });
+    }
+
+    if (errors.length > 0) {
       res.statusCode = 400;
       res.setHeader('Content-Type', 'application/json');
-
-      res.end(
-        JSON.stringify({
-          error: 'Uso: /<text>?toCase=<SNAKE|KEBAB|CAMEL|PASCAL|UPPER>',
-        }),
-      );
+      res.end(JSON.stringify({ errors }));
 
       return;
     }
 
     try {
-      const result = convertToCase(text, caseName.toUpperCase());
+      const result = convertToCase(text, caseName);
+
+      const responseBody = {
+        originalText: text,
+        originalCase: result.originalCase,
+        targetCase: caseName,
+        convertedText: result.convertedText,
+      };
 
       res.statusCode = 200;
       res.setHeader('Content-Type', 'application/json');
-      res.end(JSON.stringify(result));
+      res.end(JSON.stringify(responseBody));
     } catch (err) {
       res.statusCode = 500;
       res.setHeader('Content-Type', 'application/json');
