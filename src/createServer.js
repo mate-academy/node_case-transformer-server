e222636505c366c1622129c62572e703464a4b06
Olivio Neves <olivioservio@gmail.com>
2025-11-30 00:02:07 -0300

    Removed // eslint-disable-next-line max-len

--- src/createServer.js
+++ src/createServer.js
@@ -16,24 +16,24 @@ function createServer() {
     if (!text) {
       errors.push({
         message:
-          // eslint-disable-next-line max-len
-          'Text to convert is required. Correct request is: "/<TEXT_TO_CONVERT>?toCase=<CASE_NAME>".',
+          'Text to convert is required. Correct request is: ' +
+          '"/<TEXT_TO_CONVERT>?toCase=<CASE_NAME>".',
       });
     }
 
     if (!toCase) {
       errors.push({
         message:
-          // eslint-disable-next-line max-len
-          '"toCase" query param is required. Correct request is: "/<TEXT_TO_CONVERT>?toCase=<CASE_NAME>".',
+          '"toCase" query param is required. Correct request is: ' +
+          '"/<TEXT_TO_CONVERT>?toCase=<CASE_NAME>".',
       });
     }
 
     if (toCase && !ALLOWED_CASES.includes(toCase)) {
       errors.push({
         message:
-          // eslint-disable-next-line max-len
-          'This case is not supported. Available cases: SNAKE, KEBAB, CAMEL, PASCAL, UPPER.',
+          'This case is not supported. Available cases: ' +
+          'SNAKE, KEBAB, CAMEL, PASCAL, UPPER.',
       });
     }
 
