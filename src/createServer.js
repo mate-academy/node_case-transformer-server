c70258b2e431572ff6b024f35de9d848381c8319
tvk777 <klimenko.tv7@gmail.com>
2025-12-19 17:46:08 +0200

    add solution

--- src/createServer.js
+++ src/createServer.js
@@ -1,3 +1,49 @@
-// Write code here
-// Also, you can create additional files in the src folder
-// and import (require) them here
+/* eslint no-console: [,{ allow: ["warn", "log", "error"] }] */
+const http = require('http');
+const { convertToCase } = require('./convertToCase');
+const { validateRequest } = require('./validateRequest');
+
+const createServer = () => {
+  return http.createServer((req, res) => {
+    res.setHeader('Content-Type', 'application/json');
+
+    const url = new URL(req.url, `http:\\${req.headers.host}`);
+
+    if (url.pathname === '/favicon.ico') {
+      res.end();
+
+      return;
+    }
+
+    const originalText = url.pathname.slice(1);
+    const targetCase = url.searchParams.get('toCase');
+
+    const errors = validateRequest(originalText, targetCase);
+
+    if (errors.length > 0) {
+      res.statusCode = 400;
+      res.statusMessage = 'Bad request';
+      res.end(JSON.stringify({ errors }));
+
+      return;
+    }
+
+    const { originalCase, convertedText } = convertToCase(
+      originalText,
+      targetCase,
+    );
+
+    const result = {
+      originalCase,
+      targetCase,
+      originalText,
+      convertedText,
+    };
+
+    console.log(result);
+
+    res.end(JSON.stringify(result));
+  });
+};
+
+module.exports = { createServer };
