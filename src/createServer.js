51886c1b5054b07d222bd4da1826ba460242a478
RomanHasiuk <romanhasiuk1@gmail.com>
2026-02-03 11:00:58 +0200

    update status messages per mentor's review

--- src/createServer.js
+++ src/createServer.js
@@ -36,7 +36,7 @@ const createServer = () => {
     }
 
     if (errors.length > 0) {
-      res.writeHead(400, { 'Content-Type': 'application/json' });
+      res.writeHead(400, 'Bad request', { 'Content-Type': 'application/json' });
       res.end(JSON.stringify({ errors }));
 
       return;
@@ -44,7 +44,7 @@ const createServer = () => {
 
     const result = convertToCase(textToConvert, toCase);
 
-    res.writeHead(200, { 'Content-Type': 'application/json' });
+    res.writeHead(200, 'OK', { 'Content-Type': 'application/json' });
 
     res.end(
       JSON.stringify({
