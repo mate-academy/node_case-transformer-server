2a9918541eb59c8ca5d8f7225be985abe2fa6233
Grzegorz <grze.pra@gmail.com>
2026-02-24 16:48:13 +0100

    Solution

--- src/createServer.js
+++ src/createServer.js
@@ -1,3 +1,67 @@
-// Write code here
-// Also, you can create additional files in the src folder
-// and import (require) them here
+/* eslint-disable max-len */
+const http = require('http');
+const { convertToCase } = require('./convertToCase/convertToCase');
+
+function createServer() {
+  const server = http.createServer((req, res) => {
+    res.setHeader('Content-Type', 'application/json');
+
+    const urlParts = req.url.split('?');
+
+    const textToConvert = urlParts[0].slice(1);
+    const queryString = urlParts[1];
+
+    const params = new URLSearchParams(queryString);
+    const toCase = params.get('toCase');
+
+    const errors = [];
+
+    if (!textToConvert) {
+      errors.push({
+        message:
+          'Text to convert is required. Correct request is: "/<TEXT_TO_CONVERT>?toCase=<CASE_NAME>".',
+      });
+    }
+
+    if (!toCase) {
+      errors.push({
+        message:
+          '"toCase" query param is required. Correct request is: "/<TEXT_TO_CONVERT>?toCase=<CASE_NAME>".',
+      });
+    }
+
+    const allowedCases = ['SNAKE', 'KEBAB', 'CAMEL', 'PASCAL', 'UPPER'];
+
+    if (toCase && !allowedCases.includes(toCase)) {
+      errors.push({
+        message:
+          'This case is not supported. Available cases: SNAKE, KEBAB, CAMEL, PASCAL, UPPER.',
+      });
+    }
+
+    if (errors.length > 0) {
+      res.statusCode = 400;
+      res.statusMessage = 'Bad request';
+      res.end(JSON.stringify({ errors: errors }));
+
+      return;
+    }
+
+    const result = convertToCase(textToConvert, toCase);
+    const endData = {
+      originalCase: result.originalCase,
+      targetCase: toCase,
+      originalText: textToConvert,
+      convertedText: result.convertedText,
+    };
+
+    res.statusCode = 200;
+    res.statusMessage = 'OK';
+
+    res.end(JSON.stringify(endData));
+  });
+
+  return server;
+}
+
+module.exports = { createServer };
