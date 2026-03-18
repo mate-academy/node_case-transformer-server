8337970299cf5d5532fbdd05a3061615efb45f69
wzfxc <maksohrimenko1@gmail.com>
2026-03-18 18:06:22 +0200

    problems

--- src/createServer.js
+++ src/createServer.js
@@ -1,3 +1,71 @@
-// Write code here
-// Also, you can create additional files in the src folder
-// and import (require) them here
+import http from 'http';
+
+const convertToCase = require('./convertToCase/convertToCase.js');
+
+function createServer() {
+  const server = http.createServer((req, res) => {
+    const url = req.url.split('?');
+    const textToConvert = url[0].slice(1) || '';
+    const queryString = url[1];
+
+    const params = new URLSearchParams(queryString);
+    const toCase = params.get('toCase');
+
+    const errors = [];
+
+    res.setHeader('Content-Type', 'application/json; charset=utf-8');
+
+    if (!textToConvert) {
+      errors.push({
+        message:
+          // eslint-disable-next-line
+          'Text to convert is required. Correct request is: "/<TEXT_TO_CONVERT>?toCase=<CASE_NAME>".',
+      });
+    }
+
+    if (!toCase) {
+      errors.push({
+        message:
+          // eslint-disable-next-line
+          '"toCase" query param is required. Correct request is: "/<TEXT_TO_CONVERT>?toCase=<CASE_NAME>".',
+      });
+    }
+
+    if (
+      toCase &&
+      !['SNAKE', 'KEBAB', 'CAMEL', 'PASCAL', 'UPPER'].includes(toCase)
+    ) {
+      errors.push({
+        message:
+          // eslint-disable-next-line
+          'This case is not supported. Available cases: SNAKE, KEBAB, CAMEL, PASCAL, UPPER.',
+      });
+    }
+
+    if (errors.length > 0) {
+      res.statusCode = 400;
+      res.statusMessage = 'Bad request';
+      res.end(JSON.stringify({ errors }));
+
+      return;
+    }
+
+    const result = convertToCase(textToConvert, toCase);
+
+    res.statusCode = 200;
+    res.statusMessage = 'OK';
+
+    res.end(
+      JSON.stringify({
+        originalCase: result.originalCase,
+        targetCase: toCase,
+        originalText: textToConvert,
+        convertedText: result.convertedText,
+      }),
+    );
+  });
+
+  return server;
+}
+
+module.exports = { createServer };
