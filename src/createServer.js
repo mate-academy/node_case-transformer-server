f1cada12828103a2a9d296cc91c5b68e36596c8c
vexsmart <programador.vitor.ribeiro@hotmail.com>
2026-03-18 16:14:50 -0300

    task solution

--- src/createServer.js
+++ src/createServer.js
@@ -13,22 +13,22 @@ const createServer = () => {
     if (!textToConvert) {
       errors.push({
         message:
-          `Text to convert is required. Correct request is: 
-          "/<TEXT_TO_CONVERT>?toCase=<CASE_NAME>".`,
+          'Text to convert is required. ' +
+          'Correct request is: "/<TEXT_TO_CONVERT>?toCase=<CASE_NAME>".',
       });
     }
 
     if (!toCase) {
       errors.push({
         message:
-          `"toCase" query param is required. Correct request is: 
-          "/<TEXT_TO_CONVERT>?toCase=<CASE_NAME>".`,
+          '"toCase" query param is required. ' +
+          'Correct request is: "/<TEXT_TO_CONVERT>?toCase=<CASE_NAME>".',
       });
     } else if (!supportedCases.includes(toCase)) {
       errors.push({
         message:
-          `This case is not supported. Available cases: 
-          SNAKE, KEBAB, CAMEL, PASCAL, UPPER.`,
+          'This case is not supported. ' +
+          'Available cases: SNAKE, KEBAB, CAMEL, PASCAL, UPPER.',
       });
     }
 
@@ -41,13 +41,9 @@ const createServer = () => {
       return;
     }
 
-    const { originalCase, convertedText } = convertToCase(
-      textToConvert,
-      toCase,
-    );
+    const { originalCase, convertedText } = convertToCase(textToConvert, toCase);
 
     res.statusCode = 200;
-
     res.end(
       JSON.stringify({
         originalCase,
