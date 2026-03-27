3f529f4e0d9c0848d3b4f79d825c9cceaa0fe991
adonch <donch.a080@gmail.com>
2026-03-27 20:16:30 +0200

    feat: implement createServer function for case conversion API
    - Add HTTP server setup to handle text case conversion requests.
    - Validate input parameters and return appropriate error messages.
    - Support multiple case formats: SNAKE, KEBAB, CAMEL, PASCAL, UPPER.

--- src/createServer.js
+++ src/createServer.js
@@ -1,3 +1,61 @@
-// Write code here
-// Also, you can create additional files in the src folder
-// and import (require) them here
+const http = require('node:http');
+const { convertToCase } = require('./convertToCase');
+
+function createServer() {
+  return http.createServer((req, res) => {
+    const url = new URL(req.url, `http://${req.headers.host}`);
+    const normilizedPath = url.pathname.slice(1);
+    const targetCase = url.searchParams.get('toCase');
+    const errors = [];
+
+    if (!normilizedPath) {
+      errors.push({
+        message:
+          // eslint-disable-next-line max-len
+          'Text to convert is required. Correct request is: "/<TEXT_TO_CONVERT>?toCase=<CASE_NAME>".',
+      });
+    }
+
+    if (!targetCase) {
+      errors.push({
+        message:
+        // eslint-disable-next-line max-len
+          '"toCase" query param is required. Correct request is: "/<TEXT_TO_CONVERT>?toCase=<CASE_NAME>".',
+      });
+    }
+
+    if (
+      targetCase &&
+      !['SNAKE', 'KEBAB', 'CAMEL', 'PASCAL', 'UPPER'].includes(targetCase)
+    ) {
+      errors.push({
+        message:
+        // eslint-disable-next-line max-len
+          'This case is not supported. Available cases: SNAKE, KEBAB, CAMEL, PASCAL, UPPER.',
+      });
+    }
+
+    if (errors.length > 0) {
+      res.writeHead(400, 'Bad request', { 'Content-Type': 'application/json' });
+      res.end(JSON.stringify({ errors }));
+
+      return;
+    }
+
+    const convertedText = convertToCase(normilizedPath, targetCase);
+
+    res.writeHead(200, { 'Content-Type': 'application/json' });
+
+    res.end(
+      JSON.stringify({
+        ...convertedText,
+        targetCase: targetCase,
+        originalText: normilizedPath,
+      }),
+    );
+  });
+}
+
+module.exports = {
+  createServer,
+};
