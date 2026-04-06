fbbd2118f534e52c62d4466b9350e29230929202
Artem Yurin <artem.urin121@gmail.com>
2026-04-06 14:36:30 +0300

    Solution

--- src/createServer.js
+++ src/createServer.js
@@ -1,3 +1,72 @@
 // Write code here
 // Also, you can create additional files in the src folder
 // and import (require) them here
+const http = require('http');
+const { convertToCase } = require('./convertToCase');
+const { detectCase } = require('./convertToCase/detectCase');
+
+function createServer() {
+  const server = http.createServer((req, res) => {
+    const url = new URL(req.url, `http://${req.headers.host}`);
+    const text = url.pathname.slice(1);
+    const toCase = url.searchParams.get('toCase');
+    const errors = [];
+
+    if (!text) {
+      errors.push({
+        message:
+          // eslint-disable-next-line max-len
+          'Text to convert is required. Correct request is: "/<TEXT_TO_CONVERT>?toCase=<CASE_NAME>".',
+      });
+    }
+
+    if (!toCase) {
+      errors.push({
+        message:
+          // eslint-disable-next-line max-len
+          '"toCase" query param is required. Correct request is: "/<TEXT_TO_CONVERT>?toCase=<CASE_NAME>".',
+      });
+    }
+
+    const validCases = ['SNAKE', 'KEBAB', 'CAMEL', 'PASCAL', 'UPPER'];
+
+    if (toCase && !validCases.includes(toCase)) {
+      errors.push({
+        message:
+          // eslint-disable-next-line max-len
+          'This case is not supported. Available cases: SNAKE, KEBAB, CAMEL, PASCAL, UPPER.',
+      });
+    }
+
+    res.setHeader('Content-Type', 'application/json');
+
+    if (errors.length > 0) {
+      res.statusCode = 400;
+      res.statusMessage = 'Bad request';
+
+      res.end(JSON.stringify({ errors }));
+
+      return;
+    }
+
+    const convertedData = convertToCase(text, toCase);
+    const originalCase = detectCase(text);
+
+    const result = {
+      originalCase: originalCase,
+      targetCase: toCase,
+      originalText: text,
+      convertedText: convertedData.convertedText,
+    };
+
+    res.statusCode = 200;
+    res.statusMessage = 'OK';
+    res.end(JSON.stringify(result));
+  });
+
+  return server;
+}
+
+module.exports = {
+  createServer,
+};
