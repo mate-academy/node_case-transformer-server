796cf06fc28d067d168e567ba10a68f82b430fcc
Marina Kramarchuk <kramarchukmm20182018@gmail.com>
2026-04-27 15:57:14 +0300

    made

--- src/createServer.js
+++ src/createServer.js
@@ -15,19 +15,22 @@ const createServer = () => {
     if (!textToConvert) {
       errors.push({
         message:
-          'Text to convert is required.',
+          'Text to convert is required. ' +
+          'Correct request is: "/<TEXT_TO_CONVERT>?toCase=<CASE_NAME>".',
       });
     }
 
     if (!toCase) {
       errors.push({
         message:
-          '"toCase" query param is required.',
+          '"toCase" query param is required. ' +
+          'Correct request is: "/<TEXT_TO_CONVERT>?toCase=<CASE_NAME>".',
       });
     } else if (!supportedCases.includes(toCase)) {
       errors.push({
         message:
-          'This case is not supported.',
+          'This case is not supported. ' +
+          'Available cases: SNAKE, KEBAB, CAMEL, PASCAL, UPPER.',
       });
     }
 
