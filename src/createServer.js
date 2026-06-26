4f628be8d2633a649bf81490b626e0cb58b0a4ba
artembaranovsky1 <baranovskyiartem2006@gmail.com>
2026-06-26 15:11:39 +0300

    ашч

--- src/createServer.js
+++ src/createServer.js
@@ -4,15 +4,13 @@ const { convertToCase } = require('./convertToCase/convertToCase');
 const { detectCase } = require('./convertToCase/detectCase');
 const { getRequestData } = require('./convertToCase/getRequestData');
 
-function createServer(PORT) {
+function createServer() {
   const server = http.createServer((req, res) => {
     const { targetCase, originalText } = getRequestData(req.url);
     const errors = validateRequest(originalText, targetCase);
 
     if (errors.length > 0) {
-      res.statusCode = 400;
-
-      res.setHeader('Content-Type', 'application/json');
+      res.writeHead(400, 'Bad request', { 'Content-Type': 'application/json' });
 
       return res.end(JSON.stringify({ errors }));
     }
@@ -20,7 +18,7 @@ function createServer(PORT) {
     const { convertedText } = convertToCase(originalText, targetCase);
     const originalCase = detectCase(originalText);
 
-    res.writeHead(200, { 'Content-Type': 'application/json' });
+    res.writeHead(200, 'OK', { 'Content-Type': 'application/json' });
 
     res.end(
       JSON.stringify({
