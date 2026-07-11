f7ca8a76d8a8162d87752380ca0334135cc1cc17
Anatolii Lutai <toliklutai@gmail.com>
2026-07-11 15:12:12 +0300

    feat: implement createServer logic

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 const { createServer } = require('./createServer');
+const PORT = process.env.PORT || 3000;
 
-createServer().listen(5700, () => {
+createServer().listen(PORT, () => {
   // eslint-disable-next-line no-console
-  console.log('Server started! 🚀');
+  console.log(`Server started at http://localhost:${PORT}/ 🚀`);
 });
