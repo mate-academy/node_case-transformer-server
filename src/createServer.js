b75779ff3c0c4aefc88eca15bfae79b70c016cab
Bohdan Leonets <leonetsbogdan@gmail.com>
2026-07-24 19:12:45 +0300

    Add HTTP server implementation for text case conversion
    - Implemented a server in createServer.js that handles requests for converting text to various cases (SNAKE, KEBAB, CAMEL, PASCAL, UPPER).
    - Added error handling for missing text and unsupported case types.
    - Updated main.js to log the server's URL on startup.

--- src/createServer.js
+++ src/createServer.js
@@ -1,3 +1,64 @@
-// Write code here
-// Also, you can create additional files in the src folder
-// and import (require) them here
+const http = require('http');
+const { convertToCase } = require('./convertToCase');
+
+const SUPPORTED_CASES = ['SNAKE', 'KEBAB', 'CAMEL', 'PASCAL', 'UPPER'];
+
+function createServer() {
+  return http.createServer((req, res) => {
+    const [pathPart, queryString = ''] = req.url.split('?');
+    const text = pathPart.startsWith('/') ? pathPart.slice(1) : pathPart;
+
+    const params = new URLSearchParams(queryString);
+    const toCase = params.get('toCase');
+
+    const errors = [];
+
+    if (!text) {
+      errors.push({
+        message:
+          'Text to convert is required. Correct request is: ' +
+          '"/<TEXT_TO_CONVERT>?toCase=<CASE_NAME>".',
+      });
+    }
+
+    if (!toCase) {
+      errors.push({
+        message:
+          '"toCase" query param is required. Correct request is: ' +
+          '"/<TEXT_TO_CONVERT>?toCase=<CASE_NAME>".',
+      });
+    } else if (!SUPPORTED_CASES.includes(toCase)) {
+      errors.push({
+        message:
+          'This case is not supported. Available cases: ' +
+          'SNAKE, KEBAB, CAMEL, PASCAL, UPPER.',
+      });
+    }
+
+    if (errors.length > 0) {
+      res.writeHead(400, 'Bad request', {
+        'Content-Type': 'application/json',
+      });
+      res.end(JSON.stringify({ errors }));
+
+      return;
+    }
+
+    const { originalCase, convertedText } = convertToCase(text, toCase);
+
+    res.writeHead(200, 'OK', { 'Content-Type': 'application/json' });
+
+    res.end(
+      JSON.stringify({
+        originalCase,
+        targetCase: toCase,
+        originalText: text,
+        convertedText,
+      }),
+    );
+  });
+}
+
+module.exports = {
+  createServer,
+};
