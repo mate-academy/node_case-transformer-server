40b25164228bf1b360a9f30d2743ae802ee6ef82
linska <linskatanya@gmail.com>
2026-08-15 17:48:36 +0300

    fear: implement a server for case transformer

--- src/createServer.js
+++ src/createServer.js
@@ -1,3 +1,37 @@
-// Write code here
-// Also, you can create additional files in the src folder
-// and import (require) them here
+/* eslint-disable no-console */
+
+const http = require('node:http');
+const { parseRequest } = require('./parseRequest');
+const { checkRequest } = require('./checkRequest');
+const { convertToCase } = require('./convertToCase');
+
+function createServer() {
+  const server = http.createServer((req, res) => {
+    const { text, toCase } = parseRequest(req);
+    const errorMessages = checkRequest(text, toCase);
+
+    if (errorMessages.length > 0) {
+      res.writeHead(400, 'Bad request', { 'Content-Type': 'application/json' });
+
+      return res.end(JSON.stringify({ errors: errorMessages }));
+    }
+
+    const result = convertToCase(text, toCase);
+
+    const response = {
+      originalCase: result.originalCase,
+      targetCase: toCase,
+      originalText: text,
+      convertedText: result.convertedText,
+    };
+
+    res.writeHead(200, 'OK', { 'Content-Type': 'application/json' });
+    res.end(JSON.stringify(response));
+  });
+
+  return server;
+}
+
+module.exports = {
+  createServer,
+};
